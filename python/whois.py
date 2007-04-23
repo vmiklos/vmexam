7945a85578c8a55cb8580f97f01dc6f39ba0f443
VMiklos <vmiklos@frugalware.org>
2007-04-23 13:05:42 +0000

    python/whois.py: output cleanup

--- python/whois.py
+++ python/whois.py
@@ -3,5 +3,5 @@ import urllib, sys
 sock = urllib.urlopen("http://www.completewhois.com/cgi2/whois.cgi?query=%s" % sys.argv[1])
 for i in sock.readlines():
 	if i.startswith("descr: "):
-		print i
+		print i.strip()
 		break
