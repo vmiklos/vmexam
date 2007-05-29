8fde9f241f2701425447f08a08b08c768ee4052d
VMiklos <vmiklos@frugalware.org>
2007-05-29 17:29:35 +0000

    pipe: more error handling

--- pipe/pipe.c
+++ pipe/pipe.c
@@ -8,20 +8,28 @@
 
 int popen2(char **args, FILE **fpin, FILE **fpout)
 {
-	/* first, create the pipe, fds[0] is the read end of pipe, fds[1]
-	 * is the write end of pipe. 
-	 */
 	int pin[2], pout[2];
-	char *ptr;
-	int i;
-	pipe(pin);
-	pipe(pout);
+	pid_t pid;
 
-	/* fork two children. remember that a return value of 0 means that
-	 * we are "in" the child. the parent is ignoring the return value
-	 * of both forks. 
-	 */
-	if (fork() == 0) {
+	if(pipe(pin) == -1)
+	{
+		perror("pipe");
+		return(-1);
+	}
+	if(pipe(pout) == -1)
+	{
+		perror("pipe");
+		return(-1);
+	}
+
+	pid = fork();
+	if(pid == -1)
+	{
+		perror("fork");
+		return(-1);
+	}
+	if (pid == 0) {
+		/* we are "in" the child */
 		dup2(pin[0], STDIN_FILENO);   /* this process' stdin should
 						 be the read end of the pin
 						 pipe. dup2 closes original
@@ -32,24 +40,18 @@ int popen2(char **args, FILE **fpin, FILE **fpout)
 						 be the write end of the pout
 						 pipe. dup2 closes original
 						 stdout */
-		close(pout[1]);                /* don't need this after dup2 */
+		close(pout[1]);
 		close(pout[0]);
 
 		execv(args[0], args);
-		_exit(EXIT_FAILURE);  /* on sucess, execv never returns */
+		return(-1); /* on sucess, execv never returns */
 	}
 
-	/* the parent process only needed the pipe to give to its
-	 * children, it should NOT keep these open (can you figure out
-	 * why? what happens if the parent does not close them?). 
-	 */
 	close(pin[0]);
 	close(pout[1]);
 	*fpin = fdopen(pin[1], "w");
 	*fpout = fdopen(pout[0], "r");
-	/* do you think it matters what order the children were forked? */
-
-	return 0;
+	return(0);
 }
 
 int main(int argc, char **argv)
@@ -58,7 +60,8 @@ int main(int argc, char **argv)
 	char buf[256];
 	char *args[] = { "/usr/bin/bc", NULL };
 
-	popen2(args, &pin, &pout);
+	if(popen2(args, &pin, &pout) == -1)
+		return(1);
 
 	fprintf(pin, "2+2\n");
 	fclose(pin);
@@ -66,5 +69,6 @@ int main(int argc, char **argv)
 	fclose(pout);
 
 	printf("res: '%s'\n", buf);
+	return(0);
 }
 
