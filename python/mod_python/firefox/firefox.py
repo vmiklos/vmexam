7631495029b9ccd0e6edf3df58eb102c3d1aecdd
VMiklos <vmiklos@frugalware.org>
2007-06-12 16:25:12 +0000

    mod_python/firefox: new feed

--- python/mod_python/firefox/firefox.py
+++ python/mod_python/firefox/firefox.py
@@ -70,6 +70,7 @@ if __name__ == "firefox":
 		out.append(dumpfeed("http://feeds.feedburner.com/HUP"))
 		out.append(dumpfeed("http://frugalware.org/~vmiklos/rss/irc/irc.py"))
 		out.append(dumpfeed("http://rss.slashdot.org/Slashdot/slashdot"))
+		out.append(dumpfeed("http://frugalware.org/~vmiklos/stuff/legalja/legalja.py"))
 		out.append("""
 		</div>
 		</body>
