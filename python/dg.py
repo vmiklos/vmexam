393f94b1613d199405094816266f6b17aabc2fad
VMiklos <vmiklos@frugalware.org>
2007-06-13 17:12:54 +0000

    dg: added options to rec

--- python/dg.py
+++ python/dg.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys, tty, termios, os, re
+import sys, tty, termios, os, re, getopt
 
 class File:
 	def __init__(self, filename):
@@ -52,7 +52,7 @@ def bug(s=None):
 
 def scan_dir():
 	ret = []
-	sock = os.popen("git status -v -a")
+	sock = os.popen("git diff HEAD")
 	lines = sock.readlines()
 	sock.close()
 
@@ -102,10 +102,9 @@ def scan_dir():
 		ret.append(file)
 	return Files(ret)
 
-def askhunks(hunks):
+def askhunks(hunks, preans=None):
 	total = len(hunks)
 	hunknum = 0
-	preans = None
 	commit = False
 	for i in hunks:
 		if preans == None:
@@ -149,19 +148,55 @@ h or ?: show this help"""
 		return hunks
 
 
-def record():
+def record(argv):
+	def usage(ret):
+		print """Usage: darcs-git record [OPTION]... [FILE or DIRECTORY]...
+Save changes in the unstaged index to the current branch as a commit.
+
+Options:
+  -m PATCHNAME  --commit-name=PATCHNAME  name of commit
+  -a            --all                    answer yes to all hunks
+  -e            --edit-long-comment      backward compatibility
+  -h            --help                   shows brief description of command and its arguments"""
+		sys.exit(ret)
+
+	class Options:
+		def __init__(self):
+			self.name = None
+			self.all = False
+			self.help = False
+	options = Options()
+
+	try:
+		opts, args = getopt.getopt(argv, "m:ah", ["commit-name=", "all", "help"])
+	except getopt.GetoptError:
+		usage(1)
+	for opt, arg in opts:
+		if opt in ("-m", "--commit-name"):
+			options.name = arg
+		elif opt in ("-a", "--all"):
+			options.all = True
+		elif opt in ("-h", "--help"):
+			options.help = True
+	if options.help:
+		usage(0)
 	status = scan_dir()
-	status.hunks = askhunks(status.hunks)
+	if options.all:
+		status.hunks = askhunks(status.hunks, True)
+	else:
+		status.hunks = askhunks(status.hunks)
+	if status.hunks:
+		if options.name:
+			msg = options.name
+		else:
+			msg = ask("What is the patch name?", str)
+	else:
+		print "Ok, if you don't want to record anything, that's fine!"
+		sys.exit(0)
 	flist = []
 	for i in status.hunks:
 		if i.picked:
 			flist.append(re.sub(r".* a/([^ ]+) .*", r"\1", i.text.split("\n")[0]))
-	if status.hunks:
-		pass
-		msg = ask("What is the patch name?", str)
-	else:
-		print "Ok, if you don't want to record anything, that's fine!"
-		sys.exit(0)
 	while True:
 		ret = ask("Do you want to add a long comment? [ynq]")
 		if ret == "y":
@@ -182,14 +217,14 @@ def record():
 		sock.close()
 	os.system("git commit -m '%s' %s %s" % (msg, opts, " ".join(flist)))
 
-def main():
+def main(argv):
 	if len(sys.argv) == 1:
 		print "usage()"
 	else:
-		if sys.argv[1] == "rec":
-			record()
+		if sys.argv[1][:3] == "rec":
+			record(argv[1:])
 		else:
 			print "usage()"
 
 if __name__ == "__main__":
-	main()
+	main(sys.argv[1:])
