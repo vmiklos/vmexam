88304ae84a860c179db2d867caf73167a6a9c6eb
VMiklos <vmiklos@frugalware.org>
2007-06-14 11:18:54 +0000

    dg: support for darcs-like "rm foo.c && record" operation in record() till now you were forced to use dg rm to remove files

--- python/dg.py
+++ python/dg.py
@@ -222,6 +222,9 @@ Options:
 		if ret == "q":
 			sys.exit(0)
 		print "Invalid response, try again!"
+	# in darcs, it was possible to simply rm a file and then record a
+	# patch. support this
+	os.system("git ls-files -z --deleted | git update-index -z --remove --stdin")
 	if options.all:
 		os.system("git commit -a -m '%s' %s %s" % (msg, opts, options.files))
 		sys.exit(0)
