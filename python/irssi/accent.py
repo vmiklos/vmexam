f89cfcf376d7cfb7a34760fa07592c235dc8590c
VMiklos <vmiklos@frugalware.org>
2007-06-26 14:12:21 +0000

    irssi/accent: added a /nounaccent command

--- python/irssi/accent.py
+++ python/irssi/accent.py
@@ -7,13 +7,14 @@
 	type /py load accent to load it
 
 	changelog:
+	- 2.1: added /nounaccent command
 	- 2.0: rewritten in python
 	- 1.34: initial perl script by Tamas SZERB
 """
 
 __author__ = "Miklos Vajna <vmiklos@frugalware.org>"
-__version__ = "2.0"
-__date__ = "Tue, 26 Jun 2007 00:08:48 +0200"
+__version__ = "2.1"
+__date__ = "Tue, 26 Jun 2007 16:11:21 +0200"
 __copyright__ = "Copyright (c) 2007 Miklos Vajna"
 __license__ = "GPL"
 
@@ -38,10 +39,17 @@ def send(msg, server, witem):
 	if stripped_out:
 		return
 	signal = irssi.signal_get_emitted()
-	msg = unaccent(msg)
+	if msg.startswith("/nounaccent"):
+		msg = msg[len("/nounaccent")+1:]
+	else:
+		msg = unaccent(msg)
 	stripped_out = True
 	irssi.signal_stop()
 	irssi.signal_emit(signal, msg, server, witem)
 	stripped_out = False
 
+def nounaccent(msg, server, witem):
+	irssi.signal_emit("send command", "/ /nounaccent" + msg, server, witem)
+
 irssi.signal_add("send command", send)
+irssi.command_bind('nounaccent', nounaccent)
