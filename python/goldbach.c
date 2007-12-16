27a00abe066c1b6bceb0b538b0df59f6b63c1d0b
Miklos Vajna <vmiklos@frugalware.org>
2007-12-16 15:50:35 +0100

    goldbach.c: remove a few whitespaces

--- python/goldbach.c
+++ python/goldbach.c
@@ -7,9 +7,9 @@ int isprime(int n)
 	int i;
 
 	// make sure n is a positive integer
-	n = abs(n);
+	n=abs(n);
 	// 0 and 1 are not primes
-	if(n < 2)
+	if(n<2)
 		return(0);
 	// 2 is the only even prime number
 	if(n==2)
@@ -19,21 +19,21 @@ int isprime(int n)
 		return(0);
 	/* range starts with 3 and only needs to go up the squareroot of n
 	 * for all odd numbers */
-	for(i=3;i<=pow(n, 0.5)+1; i+=2)
-		if(n % i == 0)
+	for(i=3;i<=pow(n,0.5)+1;i+=2)
+		if(n%i==0)
 			return(0);
 	return(1);
 }
 
 int main()
 {
-	int input = 22;
-	int output = 0;
+	int input=22;
+	int output=0;
 	int i, j;
 
-	for(i=1; i<=input;i++)
-		for(j=1; j<=i;j++)
-			if(isprime(i) && isprime(j) && i+j==input)
+	for(i=1;i<=input;i++)
+		for(j=1;j<=i;j++)
+			if(isprime(i)&&isprime(j)&&i+j==input)
 				output++;
 	printf("%d\n", output);
 }
