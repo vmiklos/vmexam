f5d8a8324acb9f181350f750a2acc1d7682f7028
Miklos Vajna <vmiklos@frugalware.org>
2008-03-24 20:13:26 +0100

    startup: add roadmap link

--- python/mod_python/firefox/firefox.py
+++ python/mod_python/firefox/firefox.py
@@ -71,6 +71,7 @@ if __name__ == "firefox":
 		<a href="http://blogs.frugalware.org/vmiklos">fwblog</a> |
 		<a href="http://bugs.frugalware.org/?dev=vmiklos&order=dateopened&sort=desc">my tasks</a> |
 		<a href="http://bugs.frugalware.org/?string=[SEC]&order=lastedit&sort=desc">sec tasks</a> |
+		<a href="http://bugs.frugalware.org/?do=roadmap">roadmap</a> |
 		<a href="http://bugs.frugalware.org/?sev[]=5">critical bugs</a> |
 		<a href="http://rss.gmane.org/gmane.linux.frugalware.cvs">-darcs rss</a> |
 		<a href="http://rss.gmane.org/gmane.linux.frugalware.scm">-git rss</a> |
