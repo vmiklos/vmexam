869a09f053519c00126e680bf76e89225605ee3b
Miklos Vajna <vmiklos@frugalware.org>
2008-09-22 00:37:00 +0200

    perl/antipsyc.pl: import

---
+++ perl/antipsyc.pl
@@ -0,0 +1,30 @@
+use strict;
+use vars qw($VERSION %IRSSI);
+
+use Irssi;
+$VERSION = '0.1';
+%IRSSI = (
+	authors     => 'Miklos Vajna',
+	contact     => 'vmiklos@vmiklos.hu',
+	name        => 'antipsyc',
+	description => 'This one actually makes your life really happier.',
+	license     => 'GPL',
+);
+
+my $stripped_in  = 0;
+
+sub psyc_in {
+	if(Irssi::settings_get_bool('psyc_strip_in') && !$stripped_in) {
+		my $emitted_signal = Irssi::signal_get_emitted();
+		my ($dummy0, $text, $dummy3, $dummy4, $dummy5) = @_;
+		my ($server, $data, $nick, $address, $junk) = @_;
+		$nick =~ s/net\/irc\/user#//;
+		$stripped_in=1;
+		Irssi::signal_emit("$emitted_signal", $server, $data, $nick, $address, $junk);
+		Irssi::signal_stop();
+		$stripped_in=0;
+	}
+}
+
+Irssi::settings_add_bool('lookandfeel', 'psyc_strip_in', 1);
+Irssi::signal_add_first('server event', 'psyc_in');
