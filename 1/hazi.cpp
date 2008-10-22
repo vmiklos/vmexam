988892ac134d85c92cac8ac2dc74dc4af7b28850
Miklos Vajna <vmiklos@frugalware.org>
2008-10-22 21:25:23 +0200

    atiras 3dre

--- 1/hazi.cpp
+++ 1/hazi.cpp
@@ -35,53 +35,75 @@
 
 class Vector {
 public:
-	float x, y;
+	float x, y, z;
 
-	Vector(float x0, float y0) {
+	Vector(float x0, float y0, float z0) {
 		x = x0;
 		y = y0;
+		z = z0;
 	}
 
 	Vector operator+(const Vector& v) {
-		return Vector(x + v.x, y + v.y);
+		return Vector(x + v.x, y + v.y, z + v.z);
 	}
 
 	Vector operator*(float f) {
-		return Vector(x * f, y * f);
+		return Vector(x * f, y * f, z * f);
 	}
 };
 
 class Matrix {
 public:
-	float m[3][3];
+	float m[4][4];
 
 	Vector operator*(const Vector& v) {
-		float Xh = m[0][0] * v.x + m[0][1] * v.y + m[0][2];
-		float Yh = m[1][0] * v.x + m[1][1] * v.y + m[1][2];
-		float  h = m[2][0] * v.x + m[2][1] * v.y + m[2][2];
+		float Xh = m[0][0] * v.x + m[0][1] * v.y + m[0][2] * v.z + m[0][3];
+		float Yh = m[1][0] * v.x + m[1][1] * v.y + m[1][2] * v.z + m[1][3];
+		float Zh = m[2][0] * v.x + m[2][1] * v.y + m[2][2] * v.z + m[2][3];
+		float  h = m[3][0] * v.x + m[3][1] * v.y + m[3][2] * v.z + m[3][3];
 
-		return Vector(Xh/h, Yh/h);
+		return Vector(Xh/h, Yh/h, Zh/h);
 	}
 };
 
-Vector* points[2][7];
+const Vector* points[2][7];
+
+Matrix* transs[1];
 
 void onInitialization( ) {
-	points[0][0] = new Vector(10, 20);
-	points[0][1] = new Vector(100, 80);
-	points[0][2] = new Vector(120, 20);
-	points[0][3] = new Vector(210, 80);
-	points[0][4] = new Vector(230, 20);
-	points[0][5] = new Vector(320, 80);
-	points[0][6] = new Vector(340, 20);
+	points[0][0] = new Vector(10, 20, 0);
+	points[0][1] = new Vector(100, 80, 0);
+	points[0][2] = new Vector(120, 20, 0);
+	points[0][3] = new Vector(210, 80, 0);
+	points[0][4] = new Vector(230, 20, 0);
+	points[0][5] = new Vector(320, 80, 0);
+	points[0][6] = new Vector(340, 20, 0);
 
-	points[1][0] = new Vector(10, 120);
-	points[1][1] = new Vector(100, 180);
-	points[1][2] = new Vector(120, 120);
-	points[1][3] = new Vector(210, 180);
-	points[1][4] = new Vector(230, 120);
-	points[1][5] = new Vector(320, 180);
-	points[1][6] = new Vector(340, 120);
+	points[1][0] = new Vector(10, 120, 0);
+	points[1][1] = new Vector(100, 180, 0);
+	points[1][2] = new Vector(120, 120, 0);
+	points[1][3] = new Vector(210, 180, 0);
+	points[1][4] = new Vector(230, 120, 0);
+	points[1][5] = new Vector(320, 180, 0);
+	points[1][6] = new Vector(340, 120, 0);
+
+	transs[0] = new Matrix();
+	transs[0]->m[0][0] = 2;
+	transs[0]->m[0][1] = 0;
+	transs[0]->m[0][2] = 0;
+	transs[0]->m[0][3] = 0;
+	transs[0]->m[1][0] = 0;
+	transs[0]->m[1][1] = 2;
+	transs[0]->m[1][2] = 0;
+	transs[0]->m[1][3] = 0;
+	transs[0]->m[2][0] = 0;
+	transs[0]->m[2][1] = 0;
+	transs[0]->m[2][2] = 2;
+	transs[0]->m[2][3] = 0;
+	transs[0]->m[3][0] = 0;
+	transs[0]->m[3][1] = 0;
+	transs[0]->m[3][2] = 0;
+	transs[0]->m[3][3] = 1;
 
 	gluOrtho2D(0., 500., 0., 500.);
 }
@@ -93,8 +115,10 @@ void onDisplay( ) {
 
 	for (int i = 0; i < ARRAY_SIZE(points); i++) {
 		glBegin(GL_LINE_STRIP);
-		for (int j = 0; j < ARRAY_SIZE(points[i]); j++)
-			glVertex2d(points[i][j]->x, points[i][j]->y);
+		for (int j = 0; j < ARRAY_SIZE(points[i]); j++) {
+			Vector v = *transs[0] * *points[i][j];
+			glVertex2d(v.x, v.y);
+		}
 		glEnd();
 	}
 
