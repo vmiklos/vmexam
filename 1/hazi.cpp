8345dc7f30abd39c4fdc834e69c6bb09f966c5bf
Miklos Vajna <vmiklos@frugalware.org>
2008-10-22 20:19:13 +0200

    minimalis alakzat

--- 1/hazi.cpp
+++ 1/hazi.cpp
@@ -21,6 +21,7 @@
 #include <GL/glu.h>
 // A GLUT-ot le kell tolteni: http://www.opengl.org/resources/libraries/glut/
 #include <GL/glut.h>	
+#include <stdio.h>
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Innentol modosithatod...
@@ -30,14 +31,45 @@
 // Neptun:
 //--------------------------------------------------------
 
+#define ARRAY_SIZE(x) (sizeof(x)/sizeof(x[0]))
+
+class Vector {
+public:
+	float x, y;
+
+	Vector(float x0, float y0) {
+		x = x0;
+		y = y0;
+	}
+
+	Vector operator+(const Vector& v) {
+		return Vector(x + v.x, y + v.y);
+	}
+
+	Vector operator*(float f) {
+		return Vector(x * f, y * f);
+	}
+};
+
+Vector* points[4];
+
 void onInitialization( ) {
+	points[0] = new Vector(10, 10);
+	points[1] = new Vector(10, 20);
+	points[2] = new Vector(20, 10);
+	points[3] = new Vector(20, 20);
 }
 
 void onDisplay( ) {
 	glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	// ...
+	gluOrtho2D(0., 100., 0., 100.);
+	glColor4d(0.9f, 0.8f, 0.7f, 1.0f);
+	glBegin(GL_LINE_LOOP);
+	for (int i = 0; i < ARRAY_SIZE(points); i++)
+		glVertex2d(points[i]->x, points[i]->y);
+	glEnd();
 
 	// Buffercsere: rajzolas vege
 	glFinish();
