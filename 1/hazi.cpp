3fbb92c9b2f9906dd2ea81812c0fd6f2995edb71
Miklos Vajna <vmiklos@frugalware.org>
2008-10-22 19:45:33 +0200

    4space -> tab

--- 1/hazi.cpp
+++ 1/hazi.cpp
@@ -1,5 +1,5 @@
 //========================================================
-// Hazi feladat keret.          
+// Hazi feladat keret.		  
 // A //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // sorokon beluli reszben celszeru garazdalkodni, mert
 // a tobbit ugyis toroljuk. 
@@ -14,19 +14,19 @@
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
 // MsWindows-on ez is kell
-#include <windows.h>     
+#include <windows.h>	 
 #endif // Win32 platform
 
 #include <GL/gl.h>
 #include <GL/glu.h>
 // A GLUT-ot le kell tolteni: http://www.opengl.org/resources/libraries/glut/
-#include <GL/glut.h>     
+#include <GL/glut.h>	 
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Innentol modosithatod...
 
 //--------------------------------------------------------
-// Nev:          
+// Nev:		  
 // Neptun:
 //--------------------------------------------------------
 
@@ -34,19 +34,19 @@ void onInitialization( ) {
 }
 
 void onDisplay( ) {
-    glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // ...
+	// ...
 
-    // Buffercsere: rajzolas vege
-    glFinish();
-    glutSwapBuffers();
+	// Buffercsere: rajzolas vege
+	glFinish();
+	glutSwapBuffers();
 }
 
 void onMouse(int button, int state, int x, int y) {
-    // A GLUT_LEFT_BUTTON / GLUT_RIGHT_BUTTON
-    // ill. a GLUT_DOWN / GLUT_UP makrokat hasznald.
+	// A GLUT_LEFT_BUTTON / GLUT_RIGHT_BUTTON
+	// ill. a GLUT_DOWN / GLUT_UP makrokat hasznald.
 }
 
 void onIdle( ) {
@@ -59,26 +59,26 @@ void onKeyboard(unsigned char key, int x, int y) {
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 int main(int argc, char **argv) {
-    glutInit(&argc, argv); 
-    glutInitWindowSize(600, 600);
-    glutInitWindowPosition(100, 100);
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInit(&argc, argv); 
+	glutInitWindowSize(600, 600);
+	glutInitWindowPosition(100, 100);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
 
-    glutCreateWindow("Grafika hazi feladat");
+	glutCreateWindow("Grafika hazi feladat");
 
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
 
-    onInitialization();
+	onInitialization();
 
-    glutDisplayFunc(onDisplay);
-    glutMouseFunc(onMouse); 
-    glutIdleFunc(onIdle);
-    glutKeyboardFunc(onKeyboard);
+	glutDisplayFunc(onDisplay);
+	glutMouseFunc(onMouse); 
+	glutIdleFunc(onIdle);
+	glutKeyboardFunc(onKeyboard);
 
-    glutMainLoop();
-    
-    return 0;    
+	glutMainLoop();
+	
+	return 0;	
 }
