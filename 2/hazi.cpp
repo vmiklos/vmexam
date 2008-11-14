fe5b2828bc50af9468ffe094c430132be935eb70
Miklos Vajna <vmiklos@frugalware.org>
2008-11-14 22:28:54 +0100

    elso verzio amit mar akar fel is lehetne tolteni

--- 2/hazi.cpp
+++ 2/hazi.cpp
@@ -806,9 +806,6 @@ void onInitialization( ) {
     gluLookAt(scene.camera.eyep.x, scene.camera.eyep.y, scene.camera.eyep.z,
 		    scene.camera.eyep.x-scene.camera.lookp.x, scene.camera.eyep.y-scene.camera.lookp.y, scene.camera.eyep.z-scene.camera.lookp.z,
 		    scene.camera.updir.x, scene.camera.updir.y, scene.camera.updir.z);
-			scene.camera.eyep.x, scene.camera.eyep.y, scene.camera.eyep.z,
-			scene.camera.lookp.x, scene.camera.lookp.y, scene.camera.lookp.z,
-			scene.camera.updir.x, scene.camera.updir.y, scene.camera.updir.z);
     glMatrixMode(GL_PROJECTION);
     gluPerspective(scene.camera.fov*2, 1, scene.camera.nearClip, scene.camera.farClip);
 	Render();
@@ -822,10 +819,18 @@ void onDisplay( ) {
     glDrawPixels(SIZE, SIZE, GL_RGB, GL_FLOAT, pixels);
 
     glColor3f(1.0f, 1.0f, 1.0f);
+    // eloszor a pontokat kotjuk ossze
     glBegin(GL_LINE_STRIP);
     for (unsigned i = 0; i < raypoints.size(); i++) {
 	    glVertex3f(raypoints[i].x, raypoints[i].y, raypoints[i].z);
 	    printf("debug, drawing %f, %f, %f\n", raypoints[i].x, raypoints[i].y, raypoints[i].z);
+    }
+    glEnd();
+    // majd az egyes pontokat a fenyforrassal
+    glBegin(GL_LINES);
+    for (unsigned i = 0; i < raypoints.size(); i++) {
+	    glVertex3f(raypoints[i].x, raypoints[i].y, raypoints[i].z);
+	    glVertex3f(scene.lights[0]->location.x, scene.lights[0]->location.y, scene.lights[0]->location.z);
     }
     glEnd();
 
