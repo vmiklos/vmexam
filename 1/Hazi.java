64c5c94da5f3463e10f7b76ab370367c9b7de548
Miklos Vajna <vmiklos@frugalware.org>
2008-11-25 03:17:13 +0100

    port sort_node()

--- 1/Hazi.java
+++ 1/Hazi.java
@@ -1,5 +1,7 @@
+import java.util.*;
+
 public class Hazi {
-	class Node {
+	class Node implements Comparable<Node> {
 		String name;
 		int f, g, h;
 		public Node(String name, int g, int h) {
@@ -7,6 +9,11 @@ public class Hazi {
 			this.f = g + h;
 			this.g = g;
 			this.h = h;
+		}
+		public int compareTo(Node n) {
+			Integer fi = new Integer(f);
+			Integer nfi = new Integer(n.f);
+			return fi.compareTo(nfi);
 		}
 	}
 	public Hazi() {
