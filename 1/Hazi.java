18b5279e452f695dae65b9e102c63410fc64561e
Miklos Vajna <vmiklos@frugalware.org>
2008-11-25 16:43:46 +0100

    kommentalas

--- 1/Hazi.java
+++ 1/Hazi.java
@@ -1,29 +1,73 @@
 import java.util.*;
 import java.io.*;
 
+/**
+ * Ez a class valositja meg tenylegesen a hazit.
+ */
 public class Hazi {
+	/**
+	 * A prserben: bennevagyunk-e egy hn blokkban.
+	 */
 	boolean inhn = false;
+	/**
+	 * Parser: bennevagyunk-e egy gn blokkban.
+	 */
 	boolean ingn = false;
+	/**
+	 * A kiindulo node neve.
+	 */
 	String start = null;
+	/**
+	 * A cel neve.
+	 */
 	String end = null;
+	/**
+	 * Ebben fogjuk tarolni a heurisztika erteteket.
+	 */
 	HashMap<String,Integer> hn;
+	/**
+	 * Ebben pedig az egyes node-ok tavolsagat.
+	 */
 	HashMap<String,HashMap<String,Integer>> gn;
+	/**
+	 * Egy lista arrol, hogy honnan jottunk.
+	 */
 	HashMap<String,String> cameFrom;
+	/**
+	 * Egy varost valosit meg.
+	 */
 	class Node implements Comparable<Node> {
+		/**
+		 * Nev.
+		 */
 		String name;
+		/**
+		 * f(x) = g(x) + h(x)
+		 *
+		 * Ahol g(x) az idaig megtett ut, h(x) pedig a heurisztika.
+		 */
 		int f, g, h;
+		/**
+		 * Konstruktor.
+		 */
 		public Node(String name, int g, int h) {
 			this.name = name;
 			this.f = g + h;
 			this.g = g;
 			this.h = h;
 		}
+		/**
+		 * Osszehasonlito, rendezeshez.
+		 */
 		public int compareTo(Node n) {
 			Integer fi = new Integer(f);
 			Integer nfi = new Integer(n.f);
 			return fi.compareTo(nfi);
 		}
 	}
+	/**
+	 * Megnezi, hogy egy adott nevu varos bennevan-e a listaban.
+	 */
 	boolean nameInList(String y, List l) {
 		for (Iterator i = l.listIterator(); i.hasNext();) {
 			Node n = (Node) i.next();
@@ -32,6 +76,9 @@ public class Hazi {
 		}
 		return false;
 	}
+	/**
+	 * Visszaad egy varost a neve alapjan egy listabol.
+	 */
 	Node nodeFromList(String y, List l) {
 		for (Iterator i = l.listIterator(); i.hasNext();) {
 			Node n = (Node) i.next();
@@ -40,6 +87,10 @@ public class Hazi {
 		}
 		return null;
 	}
+	/**
+	 * A cameFrom tartalmazza, hogy az egyes pontok eseten honnan jottunk.
+	 * Ez a fuggveny ebbol epit egy listat.
+	 */
 	List<String> reconstructPath(HashMap<String,String>cameFrom, String currentNode){
 		boolean in_keys = false;
 		for (Iterator i = cameFrom.keySet().iterator(); i.hasNext(); ) {
@@ -59,16 +110,24 @@ public class Hazi {
 			return p;
 		}
 	}
+	/**
+	 * Maga az A* algoritmus.
+	 */
 	boolean aStar(String start, String end) {
 		try {
+			// Megnyitjuk a kimeneti file-t
 			BufferedWriter sock = new BufferedWriter(
 					new OutputStreamWriter(
 						new FileOutputStream("output.txt"), "8859_2")
 					);
+			// A kiertekelendo node-ok listaja, kezdetben csak a kiindulasi ponttal
 			List<Node> openlist = new LinkedList<Node>();
 			openlist.add(new Node(start, 0, hn.get(start)));
+			// A mar kiertekelt node-ok listaja, kezdetben ures
 			List<Node> closedlist = new LinkedList<Node>();
+			// Addig megyunk amig van mit kifejteni
 			for (int count = 0; openlist.size() > 0; count++) {
+				// Kiirjuk a mostani allapotunkat
 				Collections.sort(openlist);
 				sock.write("(:openlist " + count);
 				for (Iterator i = openlist.listIterator(); i.hasNext();) {
@@ -85,9 +144,13 @@ public class Hazi {
 				sock.write(")");
 				sock.newLine();
 				
+				// Legyen x az openlistbeli elemek kozul a
+				// legkisebb f(x) ertekkel rendelkezo
 				Node x = openlist.get(0);
 				openlist.remove(0);
 				if (x.name.equals(end)) {
+					// Keszen vagyunk, kiirjuk a megoldast
+					// es visszaterunk
 					sock.write("(:sol "+x.f);
 					List<String> l = reconstructPath(cameFrom, end);
 					for (Iterator i = l.listIterator(); i.hasNext();) {
@@ -99,13 +162,16 @@ public class Hazi {
 					return true;
 				}
 				closedlist.add(x);
+				// X minden egyes szomszedjara
 				for (Iterator i = gn.get(x.name).keySet().iterator(); i.hasNext(); ) {
 					String y = (String) i.next();
 					if (nameInList(y, closedlist))
 						continue;
+					// g(x) es x-y tavolsaga
 					int tentative_g_score = x.g + gn.get(x.name).get(y);
 					boolean tentative_is_better = false;
 					if (!nameInList(y, openlist)) {
+						// Harom parameter: nev, g(x), h(x)
 						openlist.add(new Node(y, tentative_g_score, hn.get(y)));
 						tentative_is_better = true;
 					} else if (tentative_g_score < nodeFromList(y, openlist).g) {
@@ -120,7 +186,11 @@ public class Hazi {
 		}
 		return false;
 	}
+	/**
+	 * A hazi class konstuktora.
+	 */
 	public Hazi(String filename) {
+		// Beolvassuk a bemeneti file-t.
 		try {
 			hn = new HashMap<String,Integer>();
 			gn = new HashMap<String,HashMap<String,Integer>>();
@@ -172,6 +242,7 @@ public class Hazi {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+		// Megkeresunk egy optimalis utat
 		aStar(start, end);
 	}
 }
--- 1/Main.java
+++ 1/Main.java
@@ -1,3 +1,6 @@
+/**
+ * Ez a class a program belepesi pontja.
+ */
 public class Main {
 	public static void main(String[] args) {
 		new Hazi(args[0]);
