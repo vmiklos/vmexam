afdf4180f908de3c48609905616d9b4b09d611f0
Miklos Vajna <vmiklos@frugalware.org>
2008-12-13 14:39:12 +0100

    fix up for newer mod_python

--- python/mod_python/firefox/firefox.py
+++ python/mod_python/firefox/firefox.py
@@ -53,42 +53,41 @@ def dumpfeed(url):
 		ret = []
 	return "\n".join(ret)
 
-if __name__ == "firefox":
-	def handler(req):
-		out = []
+def handler(req):
+	out = []
 
-		req.content_type = "text/html; charset=utf-8"
-		out.append("""
-		<html>
-		<head>
-		<link rel="stylesheet" type="text/css" href="firefox.css" />
-		<title>Firefox Startup Page</title>
-		</head>
-		<body>
-		<div class="header">
-		<a href="http://frugalware.org/">frugalware</a> |
-		<a href="http://vmiklos.hu/blog">blog</a> |
-		<a href="http://blogs.frugalware.org/vmiklos">fwblog</a> |
-		<a href="http://bugs.frugalware.org/?dev=vmiklos&order=lastedit&sort=desc">my tasks</a> |
-		<a href="http://bugs.frugalware.org/?string=[SEC]&order=lastedit&sort=desc">sec tasks</a> |
-		<a href="http://bugs.frugalware.org/?do=roadmap">roadmap</a> |
-		<a href="http://bugs.frugalware.org/?sev[]=5">critical bugs</a> |
-		<a href="http://rss.gmane.org/gmane.linux.frugalware.scm">-git rss</a> |
-		<a href="http://ftp.frugalware.org/pub/other/people/vmiklos/">devspace</a> |
-		<a href="http://vmiklos.hu">homepage</a>
-		</div>
-		<div class="dummybox"></div>
-		<div id="main">
-		""")
-		out.append(dumpfeed("http://feeds.feedburner.com/HUP"))
-		out.append(dumpfeed("http://frugalware.org/~vmiklos/rss/irc/irc.py"))
-		out.append(dumpfeed("http://rss.slashdot.org/Slashdot/slashdot"))
-		out.append(dumpfeed("http://frugalware.org/~vmiklos/rss/legalja/legalja.py"))
-		out.append("""
-		</div>
-		</body>
-		</html>
-		""")
-		req.write("".join(out))
-		dumpcache()
-		return apache.OK
+	req.content_type = "text/html; charset=utf-8"
+	out.append("""
+	<html>
+	<head>
+	<link rel="stylesheet" type="text/css" href="firefox.css" />
+	<title>Firefox Startup Page</title>
+	</head>
+	<body>
+	<div class="header">
+	<a href="http://frugalware.org/">frugalware</a> |
+	<a href="http://vmiklos.hu/blog">blog</a> |
+	<a href="http://blogs.frugalware.org/vmiklos">fwblog</a> |
+	<a href="http://bugs.frugalware.org/?dev=vmiklos&order=lastedit&sort=desc">my tasks</a> |
+	<a href="http://bugs.frugalware.org/?string=[SEC]&order=lastedit&sort=desc">sec tasks</a> |
+	<a href="http://bugs.frugalware.org/?do=roadmap">roadmap</a> |
+	<a href="http://bugs.frugalware.org/?sev[]=5">critical bugs</a> |
+	<a href="http://rss.gmane.org/gmane.linux.frugalware.scm">-git rss</a> |
+	<a href="http://ftp.frugalware.org/pub/other/people/vmiklos/">devspace</a> |
+	<a href="http://vmiklos.hu">homepage</a>
+	</div>
+	<div class="dummybox"></div>
+	<div id="main">
+	""")
+	out.append(dumpfeed("http://feeds.feedburner.com/HUP"))
+	out.append(dumpfeed("http://frugalware.org/~vmiklos/rss/irc/irc.py"))
+	out.append(dumpfeed("http://rss.slashdot.org/Slashdot/slashdot"))
+	out.append(dumpfeed("http://frugalware.org/~vmiklos/rss/legalja/legalja.py"))
+	out.append("""
+	</div>
+	</body>
+	</html>
+	""")
+	req.write("".join(out))
+	dumpcache()
+	return apache.OK
