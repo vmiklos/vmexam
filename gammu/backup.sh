b0e884c843c4b48cf45a6a68b4b1c804a34c94f1
Miklos Vajna <vmiklos@frugalware.org>
2009-01-30 13:36:13 +0100

    gl: move old files under the old-2oo7 dir

--- gammu/backup.sh
+++ gammu/backup.sh
@@ -3,4 +3,4 @@ date=`date +%Y%m%d`
 echo -e "no\nALL" |gammu --backup backup-$date.txt
 echo -e "no\nALL" |gammu --backupsms backupsms-$date.txt
 gammu --geteachsms > eachsms-$date.txt
-python backup-fs.py
+#python backup-fs.py
