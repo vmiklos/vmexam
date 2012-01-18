f0307112ff846728df472d00a747731050f486b8
Miklos Vajna <vmiklos@frugalware.org>
2012-01-18 15:20:02 +0100

    virt: enable ssh in qemu-ppc

--- virt/run-frugalware-current-ppc.sh
+++ virt/run-frugalware-current-ppc.sh
@@ -1,1 +1,1 @@
-qemu-system-ppc -m 1G -kernel vmlinux -append 'ro root=/dev/hdc3' -hda frugalware-current.img
+qemu-system-ppc -m 1G -kernel vmlinux -append 'ro root=/dev/hdc3' -hda frugalware-current.img -redir tcp:2222::22
