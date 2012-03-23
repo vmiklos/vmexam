eea68a1ce491f0e0e6d61212ed5b31ad0eb6c56d
Miklos Vajna <vmiklos@frugalware.org>
2012-03-23 09:24:20 +0100

    lo: i want tags even when JVM crashes

--- libreoffice/up.sh
+++ libreoffice/up.sh
@@ -4,6 +4,6 @@ time sh -c 'git pull -r && \
 	make && \
 	rm -rf $(readlink install) && \
 	make dev-install && \
-	make subsequentcheck && \
 	make tags && \
+	make subsequentcheck && \
 	git rev-parse HEAD > last-success' 2>&1 |tee log
