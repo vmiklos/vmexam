faeac22420b03ad550414cad97af5b1d11c0c8a8
Miklos Vajna <vmiklos@suse.cz>
2012-04-23 10:26:24 +0200

    lo: this auto-rebuild didn't work out quite well, subsequentcheck kicked in

--- libreoffice/up.sh
+++ libreoffice/up.sh
@@ -4,6 +4,5 @@ time sh -c 'git pull -r && \
 	make && \
 	make dev-install && \
 	make tags && \
-	(for i in oox writerfilter sw; do cd $i; make clean; make -sr dbglevel=2 -j4; cd -; done) && \
 	make subsequentcheck && \
 	git rev-parse HEAD > last-success' 2>&1 |tee log
