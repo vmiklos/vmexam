be06a7d1a5e8572dc0a9b70054fdc0618c8b7859
Miklos Vajna <vmiklos@suse.cz>
2012-11-09 11:51:13 +0100

    lo: add distro/suse/suse-3.6 config

--- libreoffice/daily.sh
+++ libreoffice/daily.sh
@@ -1,7 +1,7 @@
 daily_dir=$HOME/git/libreoffice/daily
 rm -rf $daily_dir/opt
 make cmd cmd="ooinstall $daily_dir/opt"
-git rev-parse HEAD > $daily_dir/build-info.txt
+git log -10 > $daily_dir/build-info.txt
 cd $daily_dir
 git add -A
 git commit -m "$(date +%Y-%m-%d)"
