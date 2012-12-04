4bd86187da0d55593e1971ec52bf667ae18f0cd6
Miklos Vajna <vmiklos@suse.cz>
2012-12-04 14:46:12 +0100

    lo daily: allow custom date

--- libreoffice/daily.sh
+++ libreoffice/daily.sh
@@ -1,7 +1,12 @@
+if [ -n "$1" ]; then
+	date=$1
+else
+	date=$(date +%Y-%m-%d)
+fi
 daily_dir=$HOME/git/libreoffice/daily
 rm -rf $daily_dir/opt
 make cmd cmd="ooinstall $daily_dir/opt"
 git log -10 > $daily_dir/build-info.txt
 cd $daily_dir
 git add -A
-git commit -m "$(date +%Y-%m-%d)"
+git commit -m "$date"
