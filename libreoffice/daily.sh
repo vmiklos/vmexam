753ad53aa33e751c20925a6312b766a588b85489
Miklos Vajna <vmiklos@suse.cz>
2013-03-03 13:14:38 +0100

    libreoffice: default to dbgutil build, strip binaries on install

--- libreoffice/daily.sh
+++ libreoffice/daily.sh
@@ -5,7 +5,7 @@ else
 fi
 daily_dir=$HOME/git/libreoffice/daily
 rm -rf $daily_dir/opt
-make cmd cmd="ooinstall $daily_dir/opt"
+make cmd cmd="ooinstall --strip $daily_dir/opt"
 git log -10 > $daily_dir/build-info.txt
 cd $daily_dir
 git add -A
