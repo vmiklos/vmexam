f970ad1a591f57cb8a93c0ca96bd4aa7f99d5b5a
Miklos Vajna <vmiklos@suse.cz>
2013-04-23 10:34:27 +0200

    avoid committing to a detached head

--- libreoffice/daily.sh
+++ libreoffice/daily.sh
@@ -4,6 +4,13 @@ else
 	date=$(date +%Y-%m-%d)
 fi
 daily_dir=$HOME/git/libreoffice/daily
+
+# this is here to avoid committing to a detached head
+cd $daily_dir
+branch=$(git symbolic-ref -q HEAD)
+[ "${branch##*/}" == "master" ] || git checkout master
+cd -
+
 rm -rf $daily_dir/opt
 make cmd cmd="ooinstall --strip $daily_dir/opt"
 git log -10 > $daily_dir/build-info.txt
