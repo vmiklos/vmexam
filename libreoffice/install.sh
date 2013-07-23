0a65f01abee038f2636cb07fd4b156c8d7ca7cf4
Miklos Vajna <vmiklos@suse.cz>
2013-07-23 11:02:49 +0200

    lo: don't rely on relocation when installing

--- libreoffice/install.sh
+++ libreoffice/install.sh
@@ -4,5 +4,11 @@ if [ -z "$1" ]; then
 	echo "usage: $0 <prefix to install to>"
 	exit 1
 fi
-rpm --dbpath $1/.RPM_OFFICE_DATABASE --query -a
-rpm --upgrade --ignoresize --nodeps -vh --relocate /opt=$1/opt --dbpath $1/.RPM_OFFICE_DATABASE RPMS/*.rpm
+#rpm --dbpath $1/.RPM_OFFICE_DATABASE --query -a
+#rpm --upgrade --ignoresize --nodeps -vh --relocate /opt/libreoffice4.1=$1/opt/libreoffice4.1 --dbpath $1/.RPM_OFFICE_DATABASE RPMS/*.rpm
+CWD=$(pwd)
+cd $1
+for i in $CWD/RPMS/*.rpm
+do
+	rpm2cpio $i |cpio -idmv
+done
