4fe8df44435c1b9e6adca7672c3e8404bcd47074
Miklos Vajna <vmiklos@collabora.co.uk>
2013-09-16 13:00:18 +0200

    lo: set user profile so after clean build we don't start with a new one all the time

--- libreoffice/up.sh
+++ libreoffice/up.sh
@@ -4,4 +4,5 @@ time sh -c "git pull -r && \
 	make dev-install && \
 	sh ~/git/vmexam/libreoffice/daily.sh && \
 	make tags && \
+	(cd instdir/* && ln -s $HOME/.config/libreofficedev/4/user) && \
 	make subsequentcheck" 2>&1 |tee log
