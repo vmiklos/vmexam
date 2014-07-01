631124e7f4f77d2bc09823f0cbd283ede045cc4f
Miklos Vajna <vmiklos@collabora.co.uk>
2014-07-01 10:36:15 +0200

    lo: also invoke pdftocairo in montage.sh if necessary

--- libreoffice/compare/montage.sh
+++ libreoffice/compare/montage.sh
@@ -2,5 +2,13 @@ for i in *.pdf
 do
 	name="$(basename "$i" .pdf)"
 	echo "$name"
-	montage "${name}.pdf-1.png" "rt/${name}.pdf-1.png" orig-label.png rt-label.png -geometry +2+2 "out/Comparison Screenshot - ${name}.png"
+	if [ ! -e "${name}-1.png" ]; then
+		pdftocairo -png $i
+	fi
+	cd rt
+	if [ ! -e "rt/${name}-1.png" ]; then
+		pdftocairo -png $i
+	fi
+	cd ..
+	montage "${name}-1.png" "rt/${name}-1.png" orig-label.png rt-label.png -geometry +2+2 "out/Comparison Screenshot - ${name}.png"
 done
