7e9855880312c95497a7e258ef3da08bc93506c4
Miklos Vajna <vmiklos@collabora.co.uk>
2014-08-30 13:01:38 +0200

    one more unnecessary option

--- openoffice/up.sh
+++ openoffice/up.sh
@@ -1,7 +1,8 @@
+# git clone git://git.apache.org/openoffice.git
 time sh -c "git pull -r && \
 	autoconf && \
-	./configure --disable-epm --enable-category-b
+	./configure --disable-epm
 	make -f Makefile clean && \
 	rm -rf install && \
 	make -f Makefile && \
-	(. ./*Env.Set.sh && cd instsetoo_native/util; LOCALINSTALLDIR=$(pwd)/../../install dmake openoffice_en-US PKGFORMAT=installed)" 2>&1 |tee log
+	(. ./*Env.Set.sh && cd instsetoo_native/util; LOCALINSTALLDIR=$(pwd)/install dmake openoffice_en-US PKGFORMAT=installed)" 2>&1 |tee log
