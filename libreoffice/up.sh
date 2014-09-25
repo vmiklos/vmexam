9b5c8e215a34bf634fbbf5c591214b04bb2b7f5a
Miklos Vajna <vmiklos@collabora.co.uk>
2014-09-25 10:42:21 +0200

    lo: run style checks as well after daily.sh

--- libreoffice/up.sh
+++ libreoffice/up.sh
@@ -5,4 +5,5 @@ time sh -c "git pull -r && \
 	sh ~/git/vmexam/libreoffice/daily.sh && \
 	make tags && \
 	(cd instdir && ln -s $HOME/.config/libreofficedev/4/user) && \
-	make check" 2>&1 |tee log
+	make check &&
+	style-check-files" 2>&1 |tee log
