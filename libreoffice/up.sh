a95ca6d43be0f38a7d2861b49d1ef7b83069df98
Miklos Vajna <vmiklos@collabora.co.uk>
2014-11-09 17:44:06 +0100

    lo: document how to invoke daily.sh daily

--- libreoffice/up.sh
+++ libreoffice/up.sh
@@ -1,9 +1,20 @@
+#!/bin/bash
+#
+# The build uses ccache, so sometimes the build completes pretty quickly,
+# sometimes it takes quite somt time. To run it exactly once a day, you can use
+# something like:
+#
+# while true; do ~/git/vmexam/bash/sleep-until 04:00; . up.sh; done
+#
+
 time sh -c "git pull -r && \
-	./autogen.sh && \
-	make clean && \
-	make build-nocheck &&
-	sh ~/git/vmexam/libreoffice/daily.sh && \
-	make tags && \
-	(cd instdir && ln -s $HOME/.config/libreofficedev/4/user) && \
-	make check &&
-	style-check-files" 2>&1 |tee log
+    ./autogen.sh && \
+    make clean && \
+    make build-nocheck &&
+    sh ~/git/vmexam/libreoffice/daily.sh && \
+    make tags && \
+    (cd instdir && ln -s $HOME/.config/libreofficedev/4/user) && \
+    make check &&
+    style-check-files" 2>&1 |tee log
+
+# vim:set shiftwidth=4 expandtab:
