cc1c5a0fda041f2826f3128362fd3a7191dc39ff
Miklos Vajna <vmiklos@collabora.co.uk>
2015-01-19 10:24:21 +0100

    lo: get rid of generated user profile
    I think it is a bug if that directory exists, but when I tried to
    bisect, the suspected first bad commit was good, so it's not that easy.
    Just get rid of that dir instead.

--- libreoffice/up.sh
+++ libreoffice/up.sh
@@ -13,7 +13,7 @@ time sh -c "git pull -r && \
     make build-nocheck &&
     sh ~/git/vmexam/libreoffice/daily.sh && \
     make tags && \
-    (cd instdir && ln -s $HOME/.config/libreofficedev/4/user) && \
+    (cd instdir && rm -rf user && ln -s $HOME/.config/libreofficedev/4/user) && \
     make check &&
     style-check-files" 2>&1 |tee log
 
