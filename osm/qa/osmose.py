139aeebdb7a9068b089e3653af5609dd93da08b2
Miklos Vajna <vmiklos@collabora.co.uk>
2015-08-30 12:00:53 +0200

    osm: import osmose test
    currently fails, so not yet part of 'make check'

---
+++ osm/qa/osmose.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+import os
+import unittest
+from xml.dom import minidom
+
+
+def parseFeed():
+    rssPath = "workdir/osmose-%s.rss" % os.environ["USER"]
+    return minidom.parse(rssPath)
+
+
+def parseDescription(node):
+    for line in node.firstChild.wholeText.split("\n"):
+        if len(line):
+            return line
+
+
+class Test(unittest.TestCase):
+    def test_none(self):
+        """This test makes sure that there are no osmose issues assigned to
+        the user at <http://osmose.openstreetmap.fr/en/byuser/>."""
+
+        feed = parseFeed()
+        issues = []
+        for item in feed.getElementsByTagName("item"):
+            for description in item.getElementsByTagName("description"):
+                issues.append(parseDescription(description))
+        self.assertEqual([], issues)
+
+if __name__ == '__main__':
+    unittest.main()
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab:
