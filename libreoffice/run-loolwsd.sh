d2080bf34e75f86dca62a614ed4ae25c590a22fa
Miklos Vajna <vmiklos@collabora.co.uk>
2015-10-21 12:19:37 +0200

    loolwsd run: move jails out of build tree, create chache dir

--- libreoffice/run-loolwsd.sh
+++ libreoffice/run-loolwsd.sh
@@ -3,11 +3,11 @@
 #INSTDIR=opt
 export MASTER=~/git/libreoffice/master
 INSTDIR=instdir
-SYSTEMPLATE=`pwd`/systemplate
-ROOTFORJAILS=`pwd`/jails
+SYSTEMPLATE=`pwd`/../systemplate
+ROOTFORJAILS=`pwd`/../jails
 
-rm -Rf ${SYSTEMPLATE} ${ROOTFORJAILS}
+rm -Rf ${SYSTEMPLATE} ${ROOTFORJAILS} install/var/cache/loolwsd
 ./loolwsd-systemplate-setup ${SYSTEMPLATE} ${MASTER}/$INSTDIR
-mkdir -p ${ROOTFORJAILS}
+mkdir -p ${ROOTFORJAILS} install/var/cache/loolwsd
 make clean-cache
 ./loolwsd --systemplate=${SYSTEMPLATE} --lotemplate=${MASTER}/$INSTDIR --childroot=${ROOTFORJAILS} --numprespawns=1
