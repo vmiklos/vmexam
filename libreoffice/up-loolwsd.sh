17f7d90aac475a39ca45f7915ba4b9298247c185
Miklos Vajna <vmiklos@collabora.co.uk>
2015-10-27 10:25:36 +0100

    lo online: avoid make check for now

--- libreoffice/up-loolwsd.sh
+++ libreoffice/up-loolwsd.sh
@@ -6,7 +6,9 @@ time sh -c "git pull -r && \
     ./configure --prefix=$PWD/install --enable-debug --with-lokit-path=$HOME/git/libreoffice/master/include \
     make clean && \
     make &&
-    make tags && \
-    make check" 2>&1 |tee log
+    make tags" 2>&1 |tee log
+
+# Would be nice to run 'make check' here, but we're not there yet, currently
+# loolwsd has to be started manually first.
 
 # vim:set shiftwidth=4 expandtab:
