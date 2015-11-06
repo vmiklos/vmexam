6c965b753620ce44281dc7e5c593d2dacf51e977
Miklos Vajna <vmiklos@collabora.co.uk>
2015-11-06 14:01:04 +0100

    lool: check for style errors as part of the build

--- libreoffice/up-loolwsd.sh
+++ libreoffice/up-loolwsd.sh
@@ -6,7 +6,10 @@ time sh -c "git pull -r && \
     ./configure --prefix=$PWD/install --enable-debug --with-lokit-path=$HOME/git/libreoffice/master/include \
     make clean && \
     make &&
-    make tags" 2>&1 |tee log
+    make tags &&
+    style-check-files" 2>&1 |tee log
+
+# Self-built poco: --with-poco-includes=$HOME/git/poco/install/include -with-poco-libs=$HOME/git/poco/lib/Linux/x86_64
 
 # Would be nice to run 'make check' here, but we're not there yet, currently
 # loolwsd has to be started manually first.
