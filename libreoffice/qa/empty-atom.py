21440e39c08cd110894dfd3deaec65562a573fce
Miklos Vajna <vmiklos@collabora.co.uk>
2015-11-17 22:35:37 +0100

    lo: add test that does not pass yet

---
+++ libreoffice/qa/empty-atom.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from xml.dom import minidom
+import os
+import sys
+import unittest
+
+
+def parseFeed():
+    rssPath = os.environ["TDOC"]
+    return minidom.parse(rssPath)
+
+
+class Test(unittest.TestCase):
+    def test_none(self):
+        """This test makes sure that there are no entries in the downloaded
+        atom feed, assuming that each entry points out a problem."""
+
+        feed = parseFeed()
+        issues = []
+        for item in feed.getElementsByTagName("entry"):
+            for description in item.getElementsByTagName("title"):
+                issues.append(description.firstChild.wholeText)
+        self.assertEqual([], issues)
+
+if __name__ == '__main__':
+    unittest.main()
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab:
