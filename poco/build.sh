9e26436abbd072ae968b1f5d1e6d4d1d8ca0f18f
Miklos Vajna <vmiklos@collabora.co.uk>
2016-01-05 16:57:35 +0100

    poco: work around build system oddness

---
+++ poco/build.sh
@@ -0,0 +1,4 @@
+# this can do a build after a 'git clean -x -d -f'
+ln -s ~/git/vmexam/poco/configure.input
+ln -s ~/git/vmexam/poco/up.sh
+. ./up.sh
--- poco/up.sh
+++ poco/up.sh
@@ -3,6 +3,9 @@
 time sh -c "git pull -r && \
     ./configure --cflags=\"$(cat configure.input)\" && \
     make clean && \
-    make -j$(getconf _NPROCESSORS_ONLN)" 2>&1 |tee log
+    make -s -j$(getconf _NPROCESSORS_ONLN)" 2>&1 |tee log
+if grep -q Werror= log; then
+    echo "Found warnings to fix."
+fi
 
 # vim:set shiftwidth=4 expandtab:
