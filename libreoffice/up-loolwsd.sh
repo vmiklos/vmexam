1253e8d32c9a14b0c182dd5e33432420a526ce8b
Miklos Vajna <vmiklos@collabora.co.uk>
2016-01-06 21:51:35 +0100

    lo: typo

--- libreoffice/up-loolwsd.sh
+++ libreoffice/up-loolwsd.sh
@@ -9,7 +9,7 @@ time sh -c "git pull -r && \
     make tags && \
     style-check-files" 2>&1 |tee log
 
-# Self-built poco: --with-poco-includes=$HOME/git/poco/install/include -with-poco-libs=$HOME/git/poco/lib/Linux/x86_64
+# Self-built poco: --with-poco-includes=$HOME/git/poco/install/include --with-poco-libs=$HOME/git/poco/lib/Linux/x86_64
 
 # Would be nice to run 'make check' here, but we're not there yet, currently
 # loolwsd has to be started manually first.
