6a03a1f5488f9b98cf9ab2004a4fdc284f6ae6e6
Miklos Vajna <vmiklos@collabora.co.uk>
2016-01-15 23:51:20 +0100

    cpp: add crtp example

---
+++ cpp/crtp.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+
+// https://en.wikipedia.org/wiki/Curiously_recurring_template_pattern
+
+template <typename Derived>
+class A
+{
+public:
+    void foo()
+    {
+        std::cerr << "A::foo" << std::endl;
+    }
+
+    void bar()
+    {
+        std::cerr << "A::bar" << std::endl;
+    }
+};
+
+class B : public A<B>
+{
+public:
+    void foo()
+    {
+        std::cerr << "B::foo" << std::endl;
+    }
+};
+
+int main()
+{
+    B b;
+    b.foo();
+    b.bar();
+    return 0;
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
