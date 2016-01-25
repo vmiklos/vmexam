a90c722d5566a4e1e24aa6f4c3276c07af5cebc0
Miklos Vajna <vmiklos@collabora.co.uk>
2016-01-25 21:57:37 +0100

    clang: import initial testsuite attempt

---
+++ clang/qa/test-rename.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+test_assert_equal()
+{
+    if ! diff -u $1 $2; then
+        echo "Error: assertion failure in ${test_name}."
+        exit 1
+    fi
+}
+
+declare_rename_test()
+{
+    test_name="Rename::${1}"
+    test_input="qa/data/${2}"
+    test_output="qa/data/${2}.new-rename"
+    test_expected="qa/data/${2}.expected"
+}
+
+declare_rename_test "testFieldDecl" "rename-field-decl.cxx"
+bin/rename -old-name=C::nX -new-name=m_nX $test_input --
+test_assert_equal $test_expected $test_output
+
+# vi:set shiftwidth=4 expandtab:
