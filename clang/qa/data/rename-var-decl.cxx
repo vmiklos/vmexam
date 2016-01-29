8ab46498cc622d31e1d07deb187ad8551261cd01
Miklos Vajna <vmiklos@collabora.co.uk>
2016-01-29 17:51:30 +0100

    VarDecl test

---
+++ clang/qa/data/rename-var-decl.cxx
@@ -0,0 +1,5 @@
+class C
+{
+public:
+    static const int aS[];
+};
--- clang/qa/test-rename.sh
+++ clang/qa/test-rename.sh
@@ -20,4 +20,8 @@ declare_rename_test "testFieldDecl" "rename-field-decl.cxx"
 bin/rename -old-name=C::nX -new-name=m_nX $test_input --
 test_assert_equal $test_expected $test_output
 
+declare_rename_test "testVarDecl" "rename-var-decl.cxx"
+bin/rename -old-name=C::aS -new-name=m_aS $test_input --
+test_assert_equal $test_expected $test_output
+
 # vi:set shiftwidth=4 expandtab:
