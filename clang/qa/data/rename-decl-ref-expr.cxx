622c78cb126db8c93f400ee779ffd8973fc0c0ef
Miklos Vajna <vmiklos@collabora.co.uk>
2016-01-31 01:00:27 +0100

    add DeclRefExpr macro test

--- clang/qa/data/rename-decl-ref-expr.cxx
+++ clang/qa/data/rename-decl-ref-expr.cxx
@@ -4,3 +4,14 @@ public:
     static const int aS[];
     static const int* getS() { return aS; }
 };
+
+int foo(const int* pS)
+{
+    return 0;
+}
+#define FOO(a) foo(a)
+
+int main()
+{
+    FOO(C::aS);
+}
