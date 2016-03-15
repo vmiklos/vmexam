63267814cdd1e10262ad4cba3ed3177c472b5ff4
Miklos Vajna <vmiklos@collabora.co.uk>
2016-03-15 21:42:13 +0100

    libclang: add completion skeleton

---
+++ libclang/completion.cxx
@@ -0,0 +1,47 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * libclang version of
+ * <http://vim.wikia.com/wiki/Show_current_function_name_in_C_programs>, i.e.
+ * show the fully qualified name of the function, if the cursor is inside a
+ * function definition.
+ */
+
+#include <iostream>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+#include <clang-c/Index.h>
+
+int main(int argc, char** argv)
+{
+    if (argc < 5)
+    {
+        std::cerr << "usage: " << argv[0] << " <file> <linenum> <colnum> <compiler args...>" << std::endl;
+        return 1;
+    }
+
+    CXIndex pIndex = clang_createIndex(/*excludeDeclsFromPCH=*/1, /*displayDiagnostics=*/0);
+
+    std::string aFile = argv[1];
+    std::vector<std::string> aArgs;
+    for (int i = 4; i < argc; ++i)
+        aArgs.push_back(argv[i]);
+    std::vector<const char*> aArgPtrs(aArgs.size());
+    for (size_t i = 0; i < aArgs.size(); ++i)
+        aArgPtrs[i] = aArgs[i].c_str();
+    CXTranslationUnit pUnit = clang_parseTranslationUnit(pIndex, aFile.c_str(), aArgPtrs.data(), aArgPtrs.size(), nullptr, 0, CXTranslationUnit_Incomplete);
+
+    if (pUnit)
+    {
+    }
+
+    clang_disposeTranslationUnit(pUnit);
+    clang_disposeIndex(pIndex);
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
