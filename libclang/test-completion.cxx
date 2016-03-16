c2b76de11c9493a9bf4b8c87e647b1536349d758
Miklos Vajna <vmiklos@collabora.co.uk>
2016-03-16 20:31:28 +0100

    libclang: add completion test file
    forgot to add this earlier

---
+++ libclang/test-completion.cxx
@@ -0,0 +1,27 @@
+#include <iostream>
+
+namespace ns
+{
+class C
+{
+    int foo(int x);
+    int bar(int x);
+};
+
+int C::foo(int x)
+{
+    int y = 0;
+}
+int C::bar(int x)
+{
+    int y = 0;
+}
+}
+
+int main()
+{
+    std::cout << "hello" << std::endl;
+    ns::C c;
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
