a4741ef9422df72cabd565abc36aa0cfc4ded78e
Miklos Vajna <vmiklos@collabora.co.uk>
2016-04-20 21:30:53 +0200

    make-dep: show how to use target-specific variables
    See
    <https://www.gnu.org/software/make/manual/html_node/Target_002dspecific.html>.

---
+++ make-dep/lib2.cxx
@@ -0,0 +1,8 @@
+#include "lib.hxx"
+
+std::string lib()
+{
+    return std::string("lib");
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
---
+++ make-dep/main2.cxx
@@ -0,0 +1,12 @@
+#include "main.hxx"
+
+#include <iostream>
+
+#include "lib.hxx"
+
+int main()
+{
+    std::cerr << lib() << std::endl;
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
