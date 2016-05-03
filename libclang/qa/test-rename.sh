03aeb1cf6c3c57ce4e509e9e7044c35e61539279
Miklos Vajna <vmiklos@collabora.co.uk>
2016-05-03 22:02:50 +0200

    clang: port testVarDeclClass

--- libclang/qa/test-rename.sh
+++ libclang/qa/test-rename.sh
@@ -63,9 +63,9 @@ declare_rename_test "testVarDecl" "rename-var-decl.cxx"
 clang-rename -offset 40 -new-name m_aS $test_input -- >${test_input}.new-rename
 test_assert_equal $test_expected $test_output
 #
-#declare_rename_test "testVarDeclClass" "rename-var-decl-class.cxx"
-#bin/rename -old-name=C -new-name=D $test_input --
-#test_assert_equal $test_expected $test_output
+declare_rename_test "testVarDeclClass" "rename-var-decl-class.cxx"
+clang-rename -offset 6 -new-name D $test_input -- > ${test_input}.new-rename
+test_assert_equal $test_expected $test_output
 #
 #declare_rename_test "testCXXConstructorDecl" "rename-cxx-constructor-decl.cxx"
 #bin/rename -csv=qa/data/rename-cxx-constructor-decl.csv $test_input --
