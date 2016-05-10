64d776dabb12d0409e146e18b48eee0b0f958e07
Miklos Vajna <vmiklos@collabora.co.uk>
2016-05-10 20:38:57 +0200

    libclang: fix test + git-svn/testsuite integration

--- libclang/up.sh
+++ libclang/up.sh
@@ -5,9 +5,11 @@ time sh -ce "git pull -r
     git pull -r
     cd tools/extra
     git pull -r
+    git svn rebase -l
     cd ../../../../workdir
     cmake -G 'Unix Makefiles' -DCMAKE_INSTALL_PREFIX=$(pwd)/instdir -DCMAKE_BUILD_TYPE=Release ..
     make -j8
+    make -j8 check-clang-tools
     make install" 2>&1 |tee log
 
 # vim:set shiftwidth=4 expandtab:
