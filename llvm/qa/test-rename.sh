0f8cc62a2b4557086f488e2171df4ed930c05c2d
Miklos Vajna <vmiklos@collabora.co.uk>
2016-05-20 11:25:06 +0200

    llvm: next failing test

--- llvm/qa/test-rename.sh
+++ llvm/qa/test-rename.sh
@@ -81,9 +81,9 @@ declare_rename_test "testMemberExpr" "rename-member-expr.cxx"
 clang-rename -offset 26 -new-name=m_nX $test_input -- > ${test_input}.new-rename
 test_assert_equal $test_expected $test_output
 #
-#declare_rename_test "testDeclRefExpr" "rename-decl-ref-expr.cxx"
-#bin/rename -old-name=C::aS -new-name=m_aS $test_input --
-#test_assert_equal $test_expected $test_output
+declare_rename_test "testDeclRefExpr" "rename-decl-ref-expr.cxx"
+clang-rename -offset 39 -new-name=m_aS $test_input -- > ${test_input}.new-rename
+test_assert_equal $test_expected $test_output
 #
 #declare_rename_test "testCXXMethodDecl" "rename-cxx-method-decl.cxx"
 #bin/rename -old-name=C::foo -new-name=bar $test_input --
