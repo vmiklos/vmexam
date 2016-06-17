7ffc28174dd20ee2e8cefcb9548497667b226d29
Miklos Vajna <vmiklos@collabora.co.uk>
2016-06-18 00:17:04 +0200

    tdf#100352 notes

--- libreoffice/up-loolwsd.sh
+++ libreoffice/up-loolwsd.sh
@@ -6,11 +6,9 @@ time sh -c "git pull -r && \
     make clean && \
     make -j$(getconf _NPROCESSORS_ONLN) && \
     make ctags && \
+    make check && \
     style-check-files" 2>&1 |tee log
 
 # Self-built poco: --with-poco-includes=$HOME/git/poco/install/include --with-poco-libs=$HOME/git/poco/lib/Linux/x86_64
-
-# Would be nice to run 'make check' here, but we're not there yet, currently
-# loolwsd has to be started manually first.
 
 # vim:set shiftwidth=4 expandtab:
