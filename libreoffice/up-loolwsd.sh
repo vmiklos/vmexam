af6a5b9310f3957f9e7420a084831bac1c1d8cbd
Miklos Vajna <vmiklos@collabora.co.uk>
2016-06-24 11:00:16 +0200

    lo: 2 new indented files

--- libreoffice/up-loolwsd.sh
+++ libreoffice/up-loolwsd.sh
@@ -7,8 +7,11 @@ time sh -c "git pull -r && \
     make -j$(getconf _NPROCESSORS_ONLN) && \
     make ctags && \
     make check && \
-    style-check-files" 2>&1 |tee log
+    style-check-files"
 
 # Self-built poco: --with-poco-includes=$HOME/git/poco/install/include --with-poco-libs=$HOME/git/poco/lib/Linux/x86_64
+
+# Would be nice to append '2>&1 |tee log' at the end, that currently makes
+# 'make check' fail for some reason.
 
 # vim:set shiftwidth=4 expandtab:
