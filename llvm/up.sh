665515ce2b790a114ae8cd90c1bdb3478e944661
Miklos Vajna <vmiklos@collabora.co.uk>
2016-07-16 23:03:13 +0200

    llvm: doc hacking notes

--- llvm/up.sh
+++ llvm/up.sh
@@ -7,7 +7,7 @@ time sh -ce "git pull -r
     git pull -r
     git svn rebase -l
     cd ../../../../workdir
-    cmake -G 'Unix Makefiles' -DCMAKE_INSTALL_PREFIX=$(pwd)/instdir -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON ..
+    cmake -G 'Unix Makefiles' -DCMAKE_INSTALL_PREFIX=$(pwd)/instdir -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_SPHINX=ON ..
     make -j8
     make -j8 check-clang-tools
     make install" 2>&1 |tee log
