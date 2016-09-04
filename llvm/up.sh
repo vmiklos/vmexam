9246593bb3084281fd2aa2157bd83b7811bf8f1b
Miklos Vajna <vmiklos@collabora.co.uk>
2016-09-04 16:09:16 +0200

    llvm: enable shared libs + notes on profiling

--- llvm/up.sh
+++ llvm/up.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 time sh -ce "git pull -r
+    mkdir -p workdir
     cd projects/compiler-rt
     git pull -r
     cd ../../tools/clang
@@ -9,10 +10,12 @@ time sh -ce "git pull -r
     git pull -r
     git svn rebase -l
     cd ../../../../workdir
-    cmake -G 'Unix Makefiles' -DCMAKE_INSTALL_PREFIX=$(pwd)/instdir -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_SPHINX=ON ..
+    cmake -G 'Unix Makefiles' -DCMAKE_INSTALL_PREFIX=$(pwd)/instdir -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_SPHINX=ON -DBUILD_SHARED_LIBS=ON ..
     make -j8
     make -j8 check-clang-tools
     make install
     ~/git/vmexam/llvm/llvm-style-check-files" 2>&1 |tee log
+
+# How to enable symbols for profiling: -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_FLAGS='-fno-omit-frame-pointer' -DCMAKE_CXX_FLAGS='-fno-omit-frame-pointer'
 
 # vim:set shiftwidth=4 expandtab:
