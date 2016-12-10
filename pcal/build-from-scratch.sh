9cd742c09e1c8e826b4fbae74b56a89106f123c8
Miklos Vajna <vmiklos@collabora.co.uk>
2016-12-10 17:21:29 +0100

    pcal: fix this to not require buggy pnmtops

--- pcal/build-from-scratch.sh
+++ pcal/build-from-scratch.sh
@@ -4,15 +4,9 @@ cd scratch
 src=""
 for i in $(seq -w 1 12)
 do
-	# We typically do a calendar for the next year.
-	../pcal -f ../calendar_hu.txt $i $(($(date +%Y)+1)) > cal$i.ps
-	pnmtopsopts="-width 11.7 -height 8.3"
-	convert ../pic/$i.jpg img$i.pnm
-	pnmtops -noturn $pnmtopsopts img$i.pnm >img$i.ps
-	rm img$i.pnm 
-	gsopts="-dDEVICEWIDTHPOINTS=842 -dDEVICEHEIGHTPOINTS=595"
-	gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite $gsopts -sOutputFile="img$i.pdf" img$i.ps
-	gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite $gsopts -sOutputFile="cal$i.pdf" cal$i.ps
+	../pcal -f ../calendar_hu.txt $i 2017 > cal$i.ps
+	ps2pdf cal$i.ps
+	convert ../pic/$i.jpg img$i.pdf
 	src="$src img$i.pdf cal$i.pdf"
 done
 pdftk $src cat output withpic.pdf
