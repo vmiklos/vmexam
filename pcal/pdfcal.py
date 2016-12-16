23d665851cd59f0b7e964099ffe5b38c2f750257
Miklos Vajna <vmiklos@collabora.co.uk>
2016-12-17 00:54:30 +0100

    pdfcal: convert ps to pdf on the fly

--- pcal/pdfcal.py
+++ pcal/pdfcal.py
@@ -10,11 +10,27 @@
 
 # TODO:
 # - don't expect images in .pdf (A4 landscape) format under images/
-# - don't expect month calendars in .pdf (A4 landscape) under images/
+# - don't expect month calendars in .ps (A4 landscape) under images/
 # - A5 output instead of A4 output
 
 import PyPDF2
+import io
 import math
+import subprocess
+
+
+# Converts inPs as a buffer-like object containing PS, and converts it to PDF.
+def ps2Pdf(inPs):
+    bufPdf = io.BytesIO()
+
+    sock = subprocess.Popen(["ps2pdf", "-", "-"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    sock.stdin.write(inPs.read())
+    sock.stdin.close()
+    bufPdf.write(sock.stdout.read())
+    sock.stdout.close()
+    bufPdf.seek(0)
+
+    return bufPdf
 
 # A4: 210 x 297 mm.
 a4Width = 595.275590551
@@ -27,7 +43,7 @@ for month in range(1, 13):
 
     imagePdf = PyPDF2.PdfFileReader(open("images/img" + monthString + ".pdf", "rb"))
     imagePage = imagePdf.getPage(0)
-    calPdf = PyPDF2.PdfFileReader(open("images/cal" + monthString + ".pdf", "rb"))
+    calPdf = PyPDF2.PdfFileReader(ps2Pdf(open("images/cal" + monthString + ".ps", "rb")))
     calPage = calPdf.getPage(0)
 
     # Portrait A4 page: upper half contains the image, lower half contains the
