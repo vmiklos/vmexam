85913fb20d3b9baa20911805d91706bfd24097f2
Miklos Vajna <vmiklos@collabora.co.uk>
2016-12-28 23:49:10 +0100

    gtktpconv: add actual convert() stub

--- gtktpconv/source/main.cxx
+++ gtktpconv/source/main.cxx
@@ -3,16 +3,34 @@
 
 #include <gtk/gtk.h>
 
+/// Hold references to all data needed to perform a conversion.
+class Conversion
+{
+  public:
+    GtkEntry* _amount;
+    GtkComboBox* _from;
+    GtkComboBox* _to;
+    GtkLabel* _result;
+
+    Conversion();
+};
+
+Conversion::Conversion()
+    : _amount(nullptr), _from(nullptr), _to(nullptr), _result(nullptr)
+{
+}
+
 namespace
 {
 
-void initAmount(GtkWidget* grid)
+void initAmount(GtkWidget* grid, Conversion& conversion)
 {
     GtkWidget* amount = gtk_entry_new();
+    conversion._amount = GTK_ENTRY(amount);
     gtk_grid_attach(GTK_GRID(grid), amount, 0, 0, 1, 1);
 }
 
-void initUnit(GtkWidget* grid, int active, int top)
+GtkComboBox* initUnit(GtkWidget* grid, int active, int top)
 {
     GtkWidget* unitCombo = gtk_combo_box_text_new();
     std::initializer_list<std::string> units = {"inch",  "point", "twip",
@@ -26,19 +44,30 @@ void initUnit(GtkWidget* grid, int active, int top)
                                   unit.c_str());
     gtk_combo_box_set_active(GTK_COMBO_BOX(unitCombo), active);
     gtk_grid_attach(GTK_GRID(grid), unitCombo, 1, top, 1, 1);
+    return GTK_COMBO_BOX(unitCombo);
 }
 
-void initResult(GtkWidget* grid)
+void initResult(GtkWidget* grid, Conversion& conversion)
 {
     GtkWidget* result = gtk_label_new("");
-    gtk_label_set_selectable(GTK_LABEL(result), TRUE);
+    conversion._result = GTK_LABEL(result);
+    gtk_label_set_selectable(conversion._result, TRUE);
     gtk_grid_attach(GTK_GRID(grid), result, 0, 1, 1, 1);
 }
 
-void initConvert(GtkWidget* grid)
+void convert(GtkWidget* /*widget*/, gpointer userData)
 {
-    GtkWidget* convert = gtk_button_new_with_label("Convert");
-    gtk_grid_attach(GTK_GRID(grid), convert, 0, 2, 1, 1);
+    Conversion* conversion = static_cast<Conversion*>(userData);
+    // TODO
+    (void)conversion;
+}
+
+void initConvert(GtkWidget* grid, Conversion& conversion)
+{
+    GtkWidget* convertButton = gtk_button_new_with_label("Convert");
+    gtk_grid_attach(GTK_GRID(grid), convertButton, 0, 2, 1, 1);
+    g_signal_connect(convertButton, "clicked", G_CALLBACK(convert),
+                     &conversion);
 }
 
 void initQuit(GtkWidget* grid)
@@ -48,14 +77,14 @@ void initQuit(GtkWidget* grid)
     gtk_grid_attach(GTK_GRID(grid), quit, 1, 2, 1, 1);
 }
 
-void initControls(GtkWidget* window)
+void initControls(GtkWidget* window, Conversion& conversion)
 {
     GtkWidget* grid = gtk_grid_new();
-    initAmount(grid);
-    initUnit(grid, 0, 0);
-    initUnit(grid, 1, 1);
-    initResult(grid);
-    initConvert(grid);
+    initAmount(grid, conversion);
+    conversion._from = initUnit(grid, 0, 0);
+    conversion._to = initUnit(grid, 1, 1);
+    initResult(grid, conversion);
+    initConvert(grid, conversion);
     initQuit(grid);
     gtk_container_add(GTK_CONTAINER(window), grid);
 }
@@ -69,7 +98,8 @@ int main(int argc, char** argv)
     gtk_window_set_title(GTK_WINDOW(window), "gtktpconv");
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), nullptr);
 
-    initControls(window);
+    Conversion conversion;
+    initControls(window, conversion);
 
     gtk_widget_show_all(window);
 
