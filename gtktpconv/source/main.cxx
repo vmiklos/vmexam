7bb4c208c12df600d642c0820dacd273dd3787c4
Miklos Vajna <vmiklos@collabora.co.uk>
2016-12-28 16:17:46 +0100

    gtktpconv: more skeleton UI

--- gtktpconv/source/main.cxx
+++ gtktpconv/source/main.cxx
@@ -1,4 +1,10 @@
+#include <initializer_list>
+#include <string>
+
 #include <gtk/gtk.h>
+
+namespace
+{
 
 void initAmount(GtkWidget* box)
 {
@@ -6,12 +12,40 @@ void initAmount(GtkWidget* box)
     gtk_box_pack_start(GTK_BOX(box), amount, TRUE, TRUE, 2);
 }
 
+void initUnit(GtkWidget* box)
+{
+    GtkWidget* unitCombo = gtk_combo_box_text_new();
+    std::initializer_list<std::string> units = {"inch",  "point", "twip",
+
+                                                "m",     "cm",    "mm",
+                                                "mm100",
+
+                                                "emu"};
+    for (const auto& unit : units)
+        gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(unitCombo), nullptr,
+                                  unit.c_str());
+    gtk_combo_box_set_active(GTK_COMBO_BOX(unitCombo), 0);
+    gtk_box_pack_start(GTK_BOX(box), unitCombo, TRUE, TRUE, 2);
+}
+
+void initResult(GtkWidget* box)
+{
+    GtkWidget* result = gtk_label_new("foo");
+    gtk_label_set_selectable(GTK_LABEL(result), TRUE);
+    gtk_box_pack_start(GTK_BOX(box), result, TRUE, TRUE, 2);
+}
+
 void initControls(GtkWidget* window)
 {
     GtkWidget* box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
     initAmount(box);
+    initUnit(box);
+    initUnit(box);
+    initResult(box);
     gtk_container_add(GTK_CONTAINER(window), box);
 }
+
+} // anonymous namespace
 
 int main(int argc, char** argv)
 {
