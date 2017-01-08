4ecdd2d6dc1004cf32584a012d9d0cbab6b10746
Miklos Vajna <vmiklos@collabora.co.uk>
2017-01-08 17:00:49 +0100

    gtktpconv: use cppunit

--- gtktpconv/qa/test.cxx
+++ gtktpconv/qa/test.cxx
@@ -4,15 +4,31 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-#include "lib.hxx"
+#include <iostream>
 
-int main(int /*argc*/, char** /*argv*/)
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/TestRunner.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+
+int main()
 {
-    if (tpconv::convert(1, tpconv::ConversionUnit::Inch,
-                        tpconv::ConversionUnit::Point) != 72)
-        return 1;
+    CPPUNIT_NS::TestResult controller;
+    CPPUNIT_NS::TestResultCollector result;
+    controller.addListener(&result);
+    CPPUNIT_NS::BriefTestProgressListener progress;
+    controller.addListener(&progress);
 
-    return 0;
+    CPPUNIT_NS::TestRunner runner;
+    runner.addTest(CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest());
+    runner.run(controller);
+
+    CPPUNIT_NS::CompilerOutputter outputter(&result, std::cerr);
+    outputter.write();
+
+    return result.wasSuccessful() ? 0 : 1;
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
---
+++ gtktpconv/qa/tpconvtest.cxx
@@ -0,0 +1,30 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#include <cppunit/extensions/HelperMacros.h>
+
+#include "lib.hxx"
+
+class TpconvTest : public CPPUNIT_NS::TestFixture
+{
+    CPPUNIT_TEST_SUITE(TpconvTest);
+    CPPUNIT_TEST(testConvert);
+    CPPUNIT_TEST_SUITE_END();
+
+    void testConvert();
+};
+
+void TpconvTest::testConvert()
+{
+    CPPUNIT_ASSERT_DOUBLES_EQUAL(
+        72, tpconv::convert(1, tpconv::ConversionUnit::Inch,
+                            tpconv::ConversionUnit::Point),
+        10e-4);
+}
+
+CPPUNIT_TEST_SUITE_REGISTRATION(TpconvTest);
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
