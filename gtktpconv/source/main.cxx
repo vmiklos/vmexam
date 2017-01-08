a2d8c7690c808a647bcc776a98e136cdadbc1265
Miklos Vajna <vmiklos@collabora.co.uk>
2017-01-08 15:56:34 +0100

    gtktpconv: separate UI and logic pieces

--- gtktpconv/source/main.cxx
+++ gtktpconv/source/main.cxx
@@ -5,6 +5,9 @@
 
 #include <gtk/gtk.h>
 
+namespace tpconv
+{
+
 /// List of unit types we handle.
 enum class ConversionUnit
 {
@@ -20,61 +23,20 @@ enum class ConversionUnit
     Emu
 };
 
-/// Hold references to all data needed to perform a conversion.
-class Conversion
+/// List of string representation of ConversionUnit elements.
+std::initializer_list<std::string> getUnitNames()
 {
-  public:
-    GtkEntry* _amount;
-    GtkComboBox* _from;
-    GtkComboBox* _to;
-    GtkLabel* _result;
+    static std::initializer_list<std::string> units = {"inch",  "point", "twip",
 
-    Conversion();
-};
+                                                       "m",     "cm",    "mm",
+                                                       "mm100",
 
-Conversion::Conversion()
-    : _amount(nullptr), _from(nullptr), _to(nullptr), _result(nullptr)
-{
+                                                       "emu"};
+    return units;
 }
 
-namespace
+double convert(double amount, ConversionUnit from, ConversionUnit to)
 {
-
-void initAmount(GtkWidget* grid, Conversion& conversion)
-{
-    GtkWidget* amount = gtk_entry_new();
-    conversion._amount = GTK_ENTRY(amount);
-    gtk_grid_attach(GTK_GRID(grid), amount, 0, 0, 1, 1);
-}
-
-GtkComboBox* initUnit(GtkWidget* grid, int active, int top)
-{
-    GtkWidget* unitCombo = gtk_combo_box_text_new();
-    std::initializer_list<std::string> units = {"inch",  "point", "twip",
-
-                                                "m",     "cm",    "mm",
-                                                "mm100",
-
-                                                "emu"};
-    for (const auto& unit : units)
-        gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(unitCombo), nullptr,
-                                  unit.c_str());
-    gtk_combo_box_set_active(GTK_COMBO_BOX(unitCombo), active);
-    gtk_grid_attach(GTK_GRID(grid), unitCombo, 1, top, 1, 1);
-    return GTK_COMBO_BOX(unitCombo);
-}
-
-void initResult(GtkWidget* grid, Conversion& conversion)
-{
-    GtkWidget* result = gtk_label_new("");
-    conversion._result = GTK_LABEL(result);
-    gtk_label_set_selectable(conversion._result, TRUE);
-    gtk_grid_attach(GTK_GRID(grid), result, 0, 1, 1, 1);
-}
-
-void convert(GtkWidget* /*widget*/, gpointer userData)
-{
-    Conversion* conversion = static_cast<Conversion*>(userData);
     static std::map<ConversionUnit, double> units;
     if (units.empty())
     {
@@ -90,18 +52,102 @@ void convert(GtkWidget* /*widget*/, gpointer userData)
         units[ConversionUnit::Emu] = 1;
     }
 
-    std::string text;
     // Convert to EMU.
+    double emu = amount * units[from];
+    return emu / units[to];
+}
+
+} // namespace tpconv
+
+/// Hold Gtk references to all data needed to perform a conversion.
+class Conversion
+{
+    GtkEntry* _amount;
+    GtkComboBox* _from;
+    GtkComboBox* _to;
+    GtkLabel* _result;
+
+    void initAmount(GtkWidget* grid);
+    static GtkComboBox* initUnit(GtkWidget* grid, int active, int top);
+    void initResult(GtkWidget* grid);
+    void initConvert(GtkWidget* grid);
+    static void convert(GtkWidget* widget, gpointer userData);
+    void convertImpl();
+    static void initQuit(GtkWidget* grid);
+
+  public:
+    Conversion();
+    void initControls(GtkWidget* window);
+};
+
+Conversion::Conversion()
+    : _amount(nullptr), _from(nullptr), _to(nullptr), _result(nullptr)
+{
+}
+
+void Conversion::initAmount(GtkWidget* grid)
+{
+    GtkWidget* amount = gtk_entry_new();
+    _amount = GTK_ENTRY(amount);
+    gtk_grid_attach(GTK_GRID(grid), amount, 0, 0, 1, 1);
+}
+
+GtkComboBox* Conversion::initUnit(GtkWidget* grid, int active, int top)
+{
+    GtkWidget* unitCombo = gtk_combo_box_text_new();
+    for (const auto& unit : tpconv::getUnitNames())
+        gtk_combo_box_text_append(GTK_COMBO_BOX_TEXT(unitCombo), nullptr,
+                                  unit.c_str());
+    gtk_combo_box_set_active(GTK_COMBO_BOX(unitCombo), active);
+    gtk_grid_attach(GTK_GRID(grid), unitCombo, 1, top, 1, 1);
+    return GTK_COMBO_BOX(unitCombo);
+}
+
+void Conversion::initResult(GtkWidget* grid)
+{
+    GtkWidget* result = gtk_label_new("");
+    _result = GTK_LABEL(result);
+    gtk_label_set_selectable(_result, TRUE);
+    gtk_grid_attach(GTK_GRID(grid), result, 0, 1, 1, 1);
+}
+
+void Conversion::initConvert(GtkWidget* grid)
+{
+    GtkWidget* convertButton = gtk_button_new_with_label("Convert");
+    gtk_grid_attach(GTK_GRID(grid), convertButton, 0, 2, 1, 1);
+    g_signal_connect(convertButton, "clicked", G_CALLBACK(Conversion::convert),
+                     this);
+}
+
+void Conversion::initControls(GtkWidget* window)
+{
+    GtkWidget* grid = gtk_grid_new();
+    initAmount(grid);
+    _from = Conversion::initUnit(grid, 0, 0);
+    _to = Conversion::initUnit(grid, 1, 1);
+    initResult(grid);
+    initConvert(grid);
+    initQuit(grid);
+    gtk_container_add(GTK_CONTAINER(window), grid);
+}
+
+void Conversion::convert(GtkWidget* /*widget*/, gpointer userData)
+{
+    Conversion* conversion = static_cast<Conversion*>(userData);
+    conversion->convertImpl();
+}
+
+void Conversion::convertImpl()
+{
+    std::string text;
     try
     {
-        double amount = std::stod(gtk_entry_get_text(conversion->_amount));
-        auto from = static_cast<ConversionUnit>(
-            gtk_combo_box_get_active(conversion->_from));
-        double emu = amount * units[from];
-
-        auto to = static_cast<ConversionUnit>(
-            gtk_combo_box_get_active(conversion->_to));
-        double ret = emu / units[to];
+        double amount = std::stod(gtk_entry_get_text(_amount));
+        auto from = static_cast<tpconv::ConversionUnit>(
+            gtk_combo_box_get_active(_from));
+        auto to =
+            static_cast<tpconv::ConversionUnit>(gtk_combo_box_get_active(_to));
+        double ret = tpconv::convert(amount, from, to);
         text = std::to_string(ret);
     }
     catch (const std::invalid_argument& exception)
@@ -111,37 +157,15 @@ void convert(GtkWidget* /*widget*/, gpointer userData)
         text = ss.str();
     }
 
-    gtk_label_set_text(conversion->_result, text.c_str());
+    gtk_label_set_text(_result, text.c_str());
 }
 
-void initConvert(GtkWidget* grid, Conversion& conversion)
-{
-    GtkWidget* convertButton = gtk_button_new_with_label("Convert");
-    gtk_grid_attach(GTK_GRID(grid), convertButton, 0, 2, 1, 1);
-    g_signal_connect(convertButton, "clicked", G_CALLBACK(convert),
-                     &conversion);
-}
-
-void initQuit(GtkWidget* grid)
+void Conversion::initQuit(GtkWidget* grid)
 {
     GtkWidget* quit = gtk_button_new_with_label("Quit");
     g_signal_connect(quit, "clicked", G_CALLBACK(gtk_main_quit), nullptr);
     gtk_grid_attach(GTK_GRID(grid), quit, 1, 2, 1, 1);
 }
-
-void initControls(GtkWidget* window, Conversion& conversion)
-{
-    GtkWidget* grid = gtk_grid_new();
-    initAmount(grid, conversion);
-    conversion._from = initUnit(grid, 0, 0);
-    conversion._to = initUnit(grid, 1, 1);
-    initResult(grid, conversion);
-    initConvert(grid, conversion);
-    initQuit(grid);
-    gtk_container_add(GTK_CONTAINER(window), grid);
-}
-
-} // anonymous namespace
 
 int main(int argc, char** argv)
 {
@@ -151,7 +175,7 @@ int main(int argc, char** argv)
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), nullptr);
 
     Conversion conversion;
-    initControls(window, conversion);
+    conversion.initControls(window);
 
     gtk_widget_show_all(window);
 
