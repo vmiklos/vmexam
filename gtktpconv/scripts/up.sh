8c50579f574369e88eb7c049b596de7cc123b88f
Miklos Vajna <vmiklos@collabora.co.uk>
2017-01-08 22:03:02 +0100

    scripts: turn on debug and run tests

--- gtktpconv/scripts/up.sh
+++ gtktpconv/scripts/up.sh
@@ -5,7 +5,8 @@ time sh -ce "git pull -r
     mkdir -p workdir
     cd workdir
 
-    CC=clang CXX=clang++ cmake -DCMAKE_INSTALL_PREFIX=$(pwd)/instdir -DCMAKE_BUILD_TYPE=Release ..
-    make -j8" 2>&1 |tee log
+    CC=clang CXX=clang++ cmake -DCMAKE_INSTALL_PREFIX=$(pwd)/instdir -DCMAKE_BUILD_TYPE=Debug  ..
+    make -j8
+    make check" 2>&1 |tee log
 
 # vim:set shiftwidth=4 expandtab:
