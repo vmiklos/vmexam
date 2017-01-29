1cb2c494ceedb77c73a33fa540c0dcb05e05f66d
Miklos Vajna <vmiklos@collabora.co.uk>
2017-01-29 16:41:59 +0100

    xmlsec: clean up version.h generation

--- xmlsec/up-cygwin.sh
+++ xmlsec/up-cygwin.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-# FIXME better version.h generation
+XMLSEC_VERSION_MAJOR=$(grep ^XMLSEC_VERSION_MAJOR= configure.ac|sed 's/.*=//')
+XMLSEC_VERSION_MINOR=$(grep ^XMLSEC_VERSION_MINOR= configure.ac|sed 's/.*=//')
+XMLSEC_VERSION_SUBMINOR=$(grep ^XMLSEC_VERSION_SUBMINOR= configure.ac|sed 's/.*=//')
+XMLSEC_VERSION="$XMLSEC_VERSION_MAJOR.$XMLSEC_VERSION_MINOR.$XMLSEC_VERSION_SUBMINOR"
+XMLSEC_VERSION_INFO=`echo $XMLSEC_VERSION | awk -F. '{ printf "%d:%d:%d", $1+$2, $3, $2 }'`
 
 myinc='/I"c:/Program Files/Microsoft Visual Studio 12.0/VC/include"'
 myinc+=' /I"c:/lo/master/workdir/UnpackedTarball/xml2/include"'
@@ -12,11 +16,11 @@ time sh -ce "git pull -r
     git clean -x -d -f
     export PATH='$(cygpath -u 'c:/Program Files/Microsoft Visual Studio 12.0/VC/bin/'):$PATH'
     cat include/xmlsec/version.h.in > include/xmlsec/version.h
-    sed -i 's/@XMLSEC_VERSION@/1.2.23/' include/xmlsec/version.h
-    sed -i 's/@XMLSEC_VERSION_MAJOR@/1/' include/xmlsec/version.h
-    sed -i 's/@XMLSEC_VERSION_MINOR@/2/' include/xmlsec/version.h
-    sed -i 's/@XMLSEC_VERSION_SUBMINOR@/23/' include/xmlsec/version.h
-    sed -i 's/@XMLSEC_VERSION_INFO@/3:23:2/' include/xmlsec/version.h
+    sed -i 's/@XMLSEC_VERSION_MAJOR@/${XMLSEC_VERSION_MAJOR}/' include/xmlsec/version.h
+    sed -i 's/@XMLSEC_VERSION_MINOR@/${XMLSEC_VERSION_MINOR}/' include/xmlsec/version.h
+    sed -i 's/@XMLSEC_VERSION_SUBMINOR@/${XMLSEC_VERSION_SUBMINOR}/' include/xmlsec/version.h
+    sed -i 's/@XMLSEC_VERSION@/${XMLSEC_VERSION}/' include/xmlsec/version.h
+    sed -i 's/@XMLSEC_VERSION_INFO@/${XMLSEC_VERSION_INFO}/' include/xmlsec/version.h
     cd win32
     cscript configure.js crypto=mscrypto xslt=no iconv=no static=no debug=yes
     sed -i -e 's|/I\$(INCPREFIX)|/I\$(INCPREFIX) $myinc|' Makefile
