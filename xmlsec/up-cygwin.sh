661508a124a8bc8c6ed6ab8242dead2d0a474a5d
Miklos Vajna <vmiklos@collabora.co.uk>
2017-01-29 16:59:12 +0100

    xmlsec: license header, description of what's this

--- xmlsec/up-cygwin.sh
+++ xmlsec/up-cygwin.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# This script is a wrapper around the libxmlsec nmake build system, so it work
+# out of the box at least on my 32bit MSVC install.
 
 XMLSEC_VERSION_MAJOR=$(grep ^XMLSEC_VERSION_MAJOR= configure.ac|sed 's/.*=//')
 XMLSEC_VERSION_MINOR=$(grep ^XMLSEC_VERSION_MINOR= configure.ac|sed 's/.*=//')
