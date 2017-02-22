14b1f0ceed723ce95e537ddd1357099127050dc8
Miklos Vajna <vmiklos@collabora.co.uk>
2017-02-22 18:20:34 +0100

    nss PoC

---
+++ nss/test.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include <cert.h>
+
+void test(const char* string)
+{
+    CERTName* name = CERT_AsciiToName(string);
+    if (name)
+    {
+        std::cerr << "CERT_AsciiToName() succeeded for '" << string << "'" << std::endl;
+        CERT_DestroyName(name);
+    }
+    else
+        std::cerr << "CERT_AsciiToName() failed for '" << string << "'" << std::endl;
+}
+
+int main()
+{
+    test("C=HU,L=Budapest,O=NISZ Nemzeti Infokommunikációs Szolgáltató Zrt.,CN=Állampolgári Tanúsítványkiadó - Qualified Citizen CA,UNDEF=VATHU-10585560");
+    test("C=HU,L=Budapest,O=NISZ Nemzeti Infokommunikációs Szolgáltató Zrt.,CN=Állampolgári Tanúsítványkiadó - Qualified Citizen CA");
+
+    return 0;
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
