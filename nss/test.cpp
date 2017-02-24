22ed6080ea2b26669298d9fdfff0518f39dc3181
Miklos Vajna <vmiklos@collabora.co.uk>
2017-02-24 22:33:26 +0100

    nss: reproducer that currently fails, but should work

--- nss/test.cpp
+++ nss/test.cpp
@@ -7,17 +7,22 @@ void test(const char* string)
     CERTName* name = CERT_AsciiToName(string);
     if (name)
     {
-        std::cerr << "CERT_AsciiToName() succeeded for '" << string << "'" << std::endl;
+        std::cerr << "CERT_AsciiToName() succeeded for '" << string << "'"
+                  << std::endl;
         CERT_DestroyName(name);
     }
     else
-        std::cerr << "CERT_AsciiToName() failed for '" << string << "'" << std::endl;
+        std::cerr << "CERT_AsciiToName() failed for '" << string << "'"
+                  << std::endl;
 }
 
 int main()
 {
-    test("C=HU,L=Budapest,O=NISZ Nemzeti Infokommunikációs Szolgáltató Zrt.,CN=Állampolgári Tanúsítványkiadó - Qualified Citizen CA,2.5.4.97=VATHU-10585560");
-    test("C=HU,L=Budapest,O=NISZ Nemzeti Infokommunikációs Szolgáltató Zrt.,CN=Állampolgári Tanúsítványkiadó - Qualified Citizen CA");
+    test("C=HU,L=Budapest,O=NISZ Nemzeti Infokommunikációs Szolgáltató "
+         "Zrt.,CN=Állampolgári Tanúsítványkiadó - Qualified Citizen "
+         "CA,2.5.4.97=VATHU-10585560");
+    // test("C=HU,L=Budapest,O=NISZ Nemzeti Infokommunikációs Szolgáltató
+    // Zrt.,CN=Állampolgári Tanúsítványkiadó - Qualified Citizen CA");
 
     return 0;
 }
