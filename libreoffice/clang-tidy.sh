5ba015cda054d62c09105edde333958105627330
Miklos Vajna <vmiklos@collabora.co.uk>
2017-03-17 23:44:49 +0100

    lo: add clang-tidy / core script

---
+++ libreoffice/clang-tidy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -ex
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+header_filter=""
+for header in $(grep hxx$ .git/indented-files.cache)
+do
+    if [ -n "$header_filter" ]; then
+        header_filter+="|"
+    fi
+    header_filter+="$PWD/$header"
+done
+
+if [ -n "$1" ]; then
+    clang-tidy -header-filter="$header_filter" $1
+else
+    for object in $(grep cxx$ .git/indented-files.cache|grep -v '/qa/')
+    do
+        clang-tidy -header-filter="$header_filter" $object
+    done
+fi
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab:
