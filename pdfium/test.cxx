382cd6a6e99653b80fb7f9cef0058b282795ac8e
Miklos Vajna <vmiklos@collabora.co.uk>
2017-03-18 01:12:02 +0100

    pdfium: load a doc

--- pdfium/test.cxx
+++ pdfium/test.cxx
@@ -1,13 +1,27 @@
+#include <cassert>
+#include <fstream>
+#include <iterator>
+#include <vector>
+
 #include <fpdfview.h>
 
 int main()
 {
-    FPDF_LIBRARY_CONFIG aConfig;
-    aConfig.version = 2;
-    aConfig.m_pUserFontPaths = nullptr;
-    aConfig.m_pIsolate = nullptr;
-    aConfig.m_v8EmbedderSlot = 0;
-    FPDF_InitLibraryWithConfig(&aConfig);
+    FPDF_LIBRARY_CONFIG config;
+    config.version = 2;
+    config.m_pUserFontPaths = nullptr;
+    config.m_pIsolate = nullptr;
+    config.m_v8EmbedderSlot = 0;
+    FPDF_InitLibraryWithConfig(&config);
+
+    std::ifstream testFile("tdf106059.pdf", std::ios::binary);
+    std::vector<char> fileContents((std::istreambuf_iterator<char>(testFile)),
+                                   std::istreambuf_iterator<char>());
+    FPDF_DOCUMENT document = FPDF_LoadMemDocument(
+        fileContents.data(), fileContents.size(), /*password=*/nullptr);
+    assert(document);
+
+    FPDF_CloseDocument(pPdfDocument);
 
     FPDF_DestroyLibrary();
 }
