c6c2a210caf68fcd1e322a47f04cc4846848f1da
Miklos Vajna <vmiklos@collabora.co.uk>
2017-04-28 01:15:46 +0200

    gtktpconv: update NDK/SDK path to match reality

--- gtktpconv/android/scripts/up.sh
+++ gtktpconv/android/scripts/up.sh
@@ -2,8 +2,8 @@
 
 time sh -ce "git pull -r
     git clean -x -d -f
-    echo 'ndk.dir=$HOME/Android/Sdk/ndk-bundle' > local.properties
-    echo 'sdk.dir=$HOME/Android/Sdk' >> local.properties
+    echo 'ndk.dir=$HOME/git/android/Android/Sdk/ndk-bundle' > local.properties
+    echo 'sdk.dir=$HOME/git/android/Android/Sdk' >> local.properties
     ./gradlew installDebug" 2>&1 |tee log
 
 # vim:set shiftwidth=4 expandtab:
