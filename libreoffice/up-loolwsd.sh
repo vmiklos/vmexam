7c1c0b07bbcade731b1d5bf5942463c1145487e4
Miklos Vajna <vmiklos@collabora.co.uk>
2017-06-20 09:53:43 +0200

    lo: logging now works with loolwsd

--- libreoffice/up-loolwsd.sh
+++ libreoffice/up-loolwsd.sh
@@ -2,16 +2,13 @@
 
 time sh -c "git pull -r && \
     ./autogen.sh && \
-    ./configure --prefix=$PWD/install --enable-debug --with-lo-path=$HOME/git/libreoffice/master/instdir --with-lokit-path=$HOME/git/libreoffice/master/include CC='ccache gcc' CXX='ccache g++' && \
+    ./configure --prefix=$PWD/install --enable-debug --with-lo-path=$HOME/git/libreoffice/master/instdir --with-lokit-path=$HOME/git/libreoffice/master/include CC='ccache $HOME/git/llvm/instdir/bin/clang' CXX='ccache $HOME/git/llvm/instdir/bin/clang++' && \
     make clean && \
     make -j$(getconf _NPROCESSORS_ONLN) && \
     make ctags && \
     make check && \
-    style-check-files"
+    style-check-files" 2>&1 |tee log
 
 # Self-built poco: --with-poco-includes=$HOME/git/poco/install/include --with-poco-libs=$HOME/git/poco/lib/Linux/x86_64
-
-# Would be nice to append '2>&1 |tee log' at the end, that currently makes
-# 'make check' fail for some reason.
 
 # vim:set shiftwidth=4 expandtab:
