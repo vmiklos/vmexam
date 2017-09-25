5c74971ad4deda122900261a6e586caf2917cb77
Miklos Vajna <vmiklos@collabora.co.uk>
2017-09-25 10:58:50 +0200

    lo wsd: give up on style effort

--- libreoffice/up-loolwsd.sh
+++ libreoffice/up-loolwsd.sh
@@ -6,8 +6,7 @@ time sh -c "git pull -r && \
     make clean && \
     make -j$(getconf _NPROCESSORS_ONLN) && \
     make ctags && \
-    make check && \
-    style-check-files" 2>&1 |tee log
+    make check" 2>&1 |tee log
 
 # Self-built poco: --with-poco-includes=$HOME/git/poco/install/include --with-poco-libs=$HOME/git/poco/lib/Linux/x86_64
 
