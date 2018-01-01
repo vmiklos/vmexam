69d6bfe45763665b8966317b556359aa81f86098
Miklos Vajna <vmiklos@collabora.co.uk>
2018-01-01 20:43:11 +0100

    llvm: switch to building with clang
    gcc-4.8 seems to be too old to build trunk

--- llvm/up.sh
+++ llvm/up.sh
@@ -1,21 +1,30 @@
-#!/bin/bash
+#!/bin/bash -ex
 
-time sh -ce "git pull -r
-    mkdir -p workdir
-    cd projects/compiler-rt
-    git pull -r
-    cd ../../tools/clang
-    git pull -r
-    git svn rebase -l
-    cd tools/extra
-    git pull -r
-    git svn rebase -l
-    cd ../../../../workdir
-    cmake -G 'Unix Makefiles' -DCMAKE_INSTALL_PREFIX=$(pwd)/instdir -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_ENABLE_SPHINX=ON -DBUILD_SHARED_LIBS=ON ..
-    make -j8
-    make -j8 check-clang
-    make install
-    ~/git/vmexam/llvm/llvm-style-check-files" 2>&1 |tee log
+git pull -r
+mkdir -p workdir
+cd projects/compiler-rt
+git pull -r
+cd ../../tools/clang
+git pull -r
+git svn rebase -l
+cd tools/extra
+git pull -r
+git svn rebase -l
+cd ../../../../workdir
+cmake \
+    -G 'Unix Makefiles' \
+    -DCMAKE_INSTALL_PREFIX=$PWD/../instdir \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DLLVM_ENABLE_ASSERTIONS=ON \
+    -DLLVM_ENABLE_SPHINX=ON \
+    -DBUILD_SHARED_LIBS=ON \
+    -DCMAKE_C_COMPILER="clang" \
+    -DCMAKE_CXX_COMPILER="clang++" \
+    ..
+make -j8
+make -j8 check-clang
+make install
+~/git/vmexam/llvm/llvm-style-check-files
 
 # How to enable symbols for profiling: -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_FLAGS='-fno-omit-frame-pointer' -DCMAKE_CXX_FLAGS='-fno-omit-frame-pointer'
 
