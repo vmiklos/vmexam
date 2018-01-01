905c72dc6a2f9df5dffe58fe94347c3e16e1e39e
Miklos Vajna <vmiklos@collabora.co.uk>
2018-01-01 22:32:00 +0100

    xmlsec msvc: make it easier to source the build env

--- xmlsec/up-cygwin.sh
+++ xmlsec/up-cygwin.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -ex
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -11,8 +11,9 @@
 # changes.
 
 msvcdir="c:/Program Files (x86)/Microsoft Visual Studio 14.0/VC"
-xml2dir="c:/lo/master/workdir/UnpackedTarball/xml2"
-icudir="c:/lo/master/workdir/UnpackedTarball/icu"
+lodir="c:/lo/master"
+xml2dir="$lodir/workdir/UnpackedTarball/xml2"
+icudir="$lodir/workdir/UnpackedTarball/icu"
 sdkdir="c:/Program Files (x86)/Windows Kits/8.1"
 sdkdir10="c:/Program Files (x86)/Windows Kits/10"
 
@@ -31,14 +32,16 @@ libpath+=";$sdkdir/Lib/winv6.3/um/x86"
 incpath+=";$sdkdir10/Include/10.0.10240.0/ucrt"
 libpath+=";$sdkdir10/Lib/10.0.10240.0/ucrt/x86"
 
-time sh -cex "git pull -r
-    git clean -x -d -f
-    export PATH='$(cygpath -u "$msvcdir/bin/"):$PATH'
-    cd win32
-    cscript configure.js crypto=mscrypto xslt=no iconv=no static=no debug=yes
-    LIB='$libpath' INCLUDE='$incpath' '$msvcdir/bin/nmake.exe'
-    cp $xml2dir/win32/bin.msvc/libxml2.dll binaries/
-    cp $icudir/source/lib/icuuc60.dll binaries/
-    cp $icudir/source/lib/icudt60.dll binaries/" 2>&1 |tee log
+git pull -r
+git clean -x -d -f
+export PATH="$(cygpath -u "$msvcdir/bin/")":"$PATH"
+cd win32
+cscript configure.js crypto=mscrypto xslt=no iconv=no static=no debug=yes
+export LIB="$libpath"
+export INCLUDE="$incpath"
+nmake
+cp $lodir/instdir/program/libxml2.dll binaries/
+cp $lodir/instdir/program/icuuc*.dll binaries/
+cp $lodir/instdir/program/icudt*.dll binaries/
 
 # vim:set shiftwidth=4 expandtab:
