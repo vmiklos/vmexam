83cecdc4043643c94579899129ea62e1dd0cafb5
Miklos Vajna <vmiklos@collabora.co.uk>
2018-01-17 18:10:56 +0100

    include-tidy: add sw-inc "area"

--- libreoffice/include-tidy.sh
+++ libreoffice/include-tidy.sh
@@ -9,6 +9,14 @@
 # ~/git/include-what-you-use/iwyu_tool.py -p . $object 2>&1 |tee out
 # cat out | ~/git/vmexam/libreoffice/find-unneeded-includes2
 
+if [ "$1" == "sw-inc" ]; then
+    for object in $(git ls-files|grep sw/inc/.*.hxx)
+    do
+        ~/git/include-what-you-use/iwyu_tool.py -p . -a sw/source/core/doc/docnew.cxx $object 2>&1 | ~/git/vmexam/libreoffice/find-unneeded-includes2
+    done
+    exit
+fi
+
 for object in $(cat .git/indented-files.cache .git/indented-files2.cache|grep cxx$)
 do
     ~/git/include-what-you-use/iwyu_tool.py -p . $object 2>&1 | ~/git/vmexam/libreoffice/find-unneeded-includes2
