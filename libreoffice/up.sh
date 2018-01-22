2a267ee6506fa86a59f6f7484db0d571e3081896
Miklos Vajna <vmiklos@collabora.co.uk>
2018-01-22 09:06:31 +0100

    lo update: avoid shell wrapper

--- libreoffice/up.sh
+++ libreoffice/up.sh
@@ -1,21 +1,25 @@
-#!/bin/bash
+#!/bin/bash -ex
 #
 # The build uses ccache, so sometimes the build completes pretty quickly,
 # sometimes it takes quite somt time. To run it exactly once a day, you can use
 # something like:
 #
-# while true; do ~/git/vmexam/bash/sleep-until 04:00; . up.sh; done
+# while true; do ~/git/vmexam/bash/sleep-until 04:00; ./up.sh; done
 #
 
-time sh -c "git pull -r && \
-    if [ -e Makefile ]; then make distclean; fi && \
-    ./autogen.sh && \
-    make build-nocheck &&
-    if [ \"$(git config libreoffice.bibisect)\" == "true" ]; then sh ~/git/vmexam/libreoffice/daily.sh; fi && \
-    make tags && \
-    (cd instdir && rm -rf user && ln -s $HOME/.config/libreofficedev/master/user) && \
-    make check && \
-    make vim-ide-integration && \
-    style-check-files" 2>&1 |tee log
+git pull -r
+if [ -e Makefile ]; then
+    make distclean
+fi
+./autogen.sh 2>&1 |tee log
+make build-nocheck 2>&1 |tee -a log
+if [ "$(git config libreoffice.bibisect)" == "true" ]; then
+    sh ~/git/vmexam/libreoffice/daily.sh
+fi
+make tags
+(cd instdir && rm -rf user && ln -s $HOME/.config/libreofficedev/master/user)
+make check 2>&1 |tee -a log
+make vim-ide-integration
+style-check-files
 
 # vim:set shiftwidth=4 expandtab:
