748937e2ba5de800318b1261a12f4cb8c9f40226
Miklos Vajna <vmiklos@collabora.co.uk>
2018-01-22 17:06:48 +0100

    loolwsd: drop shell wrapper from update script

--- libreoffice/up-loolwsd.sh
+++ libreoffice/up-loolwsd.sh
@@ -1,13 +1,24 @@
-#!/bin/bash
+#!/bin/bash -ex
 
-time sh -c "git pull -r && \
-    ./autogen.sh && \
-    ./configure --prefix=$PWD/install --enable-debug --with-lo-path=$HOME/git/libreoffice/master/instdir --with-lokit-path=$HOME/git/libreoffice/master/include CC='ccache $HOME/git/llvm/instdir/bin/clang' CXX='ccache $HOME/git/llvm/instdir/bin/clang++' CFLAGS='-g -O0' CXXFLAGS='-g -O0' && \
-    make clean && \
-    make -j$(getconf _NPROCESSORS_ONLN) && \
-    make ctags && \
-    make check" 2>&1 |tee log
+git pull -r
+./autogen.sh
+./configure \
+    --prefix=$PWD/install \
+    --enable-debug \
+    --with-lo-path=$HOME/git/libreoffice/master/instdir \
+    --with-lokit-path=$HOME/git/libreoffice/master/include \
+    CC="ccache $HOME/git/llvm/instdir/bin/clang" \
+    CXX="ccache $HOME/git/llvm/instdir/bin/clang++" \
+    CFLAGS='-g -O0' \
+    CXXFLAGS='-g -O0' \
 
-# Self-built poco: --with-poco-includes=$HOME/git/poco/install/include --with-poco-libs=$HOME/git/poco/lib/Linux/x86_64
+# Self-built poco:
+#    --with-poco-includes=$HOME/git/poco/install/include \
+#    --with-poco-libs=$HOME/git/poco/lib/Linux/x86_64 \
+
+make clean
+make -j$(getconf _NPROCESSORS_ONLN)
+make ctags
+make check
 
 # vim:set shiftwidth=4 expandtab:
