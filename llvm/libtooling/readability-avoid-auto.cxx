b6e25941b7f27086d9813be9c4c2e5ec7b8c381f
Miklos Vajna <vmiklos@collabora.co.uk>
2018-02-20 21:54:42 +0100

    readability-avoid-auto: fix reported location / range

--- llvm/libtooling/readability-avoid-auto.cxx
+++ llvm/libtooling/readability-avoid-auto.cxx
@@ -5,25 +5,32 @@
 #include <clang/Frontend/FrontendActions.h>
 #include <clang/Tooling/CommonOptionsParser.h>
 #include <clang/Tooling/Refactoring.h>
+#include <clang/Tooling/ReplacementsYaml.h>
 #include <llvm/Support/Signals.h>
+#include <llvm/Support/YAMLTraits.h>
 
 namespace
 {
-class AvoidAutoCallback : public clang::ast_matchers::MatchFinder::MatchCallback
+class Callback : public clang::ast_matchers::MatchFinder::MatchCallback
 {
   public:
-    AvoidAutoCallback(clang::tooling::Replacements* /*pReplace*/) {}
+    Callback(clang::tooling::Replacements* pReplacements)
+        : m_pReplacements(pReplacements)
+    {
+    }
 
     void
     run(const clang::ast_matchers::MatchFinder::MatchResult& rResult) override
     {
         if (const auto pDecl = rResult.Nodes.getNodeAs<clang::VarDecl>("decl"))
         {
+            clang::SourceRange aRange(
+                pDecl->getTypeSourceInfo()->getTypeLoc().getSourceRange());
             clang::QualType aType = pDecl->getType();
-            report(rResult.Context,
-                   "avoid auto, consider spelling out %0 instead",
-                   pDecl->getLocation())
-                << pDecl->getSourceRange() << aType;
+            clang::FixItHint aFixIt =
+                fixit(rResult.Context, aRange, aType.getAsString());
+            report(rResult.Context, "avoid auto", aRange.getBegin()) << aRange
+                                                                     << aFixIt;
         }
     }
 
@@ -33,10 +40,28 @@ class AvoidAutoCallback : public clang::ast_matchers::MatchFinder::MatchCallback
                                     clang::SourceLocation aLocation) const
     {
         clang::DiagnosticsEngine& rEngine = pContext->getDiagnostics();
-        return rEngine.Report(aLocation,
-                              rEngine.getDiagnosticIDs()->getCustomDiagID(
-                                  clang::DiagnosticIDs::Level::Error, aString));
+        clang::DiagnosticIDs::Level eLevel =
+            clang::DiagnosticIDs::Level::Warning;
+        if (rEngine.getWarningsAsErrors())
+            eLevel = clang::DiagnosticIDs::Level::Error;
+        return rEngine.Report(
+            aLocation,
+            rEngine.getDiagnosticIDs()->getCustomDiagID(eLevel, aString));
     }
+
+    clang::FixItHint fixit(clang::ASTContext* pContext,
+                           const clang::SourceRange& rRange,
+                           const std::string& rString)
+    {
+        clang::FixItHint aFixIt =
+            clang::FixItHint::CreateReplacement(rRange, rString);
+        m_pReplacements->insert(clang::tooling::Replacement(
+            pContext->getSourceManager(), aFixIt.RemoveRange,
+            aFixIt.CodeToInsert));
+        return aFixIt;
+    }
+
+    clang::tooling::Replacements* m_pReplacements;
 };
 
 clang::ast_matchers::StatementMatcher makeMatcher()
@@ -47,20 +72,47 @@ clang::ast_matchers::StatementMatcher makeMatcher()
 
 llvm::cl::extrahelp
     aCommonHelp(clang::tooling::CommonOptionsParser::HelpMessage);
-llvm::cl::OptionCategory aAvoidAutoCategory("avoid-auto options");
+llvm::cl::OptionCategory aCategory("avoid-auto options");
+llvm::cl::opt<std::string>
+    aExportFixes("export-fixes",
+                 llvm::cl::desc("YAML file to store suggested fixes in."),
+                 llvm::cl::value_desc("filename"), llvm::cl::cat(aCategory));
 }
 
 int main(int argc, const char** argv)
 {
     llvm::sys::PrintStackTraceOnErrorSignal();
-    clang::tooling::CommonOptionsParser aOptionsParser(argc, argv,
-                                                       aAvoidAutoCategory);
+    clang::tooling::CommonOptionsParser aOptionsParser(argc, argv, aCategory);
     clang::tooling::RefactoringTool aTool(aOptionsParser.getCompilations(),
                                           aOptionsParser.getSourcePathList());
     clang::ast_matchers::MatchFinder aFinder;
-    AvoidAutoCallback aCallback(&aTool.getReplacements());
+    Callback aCallback(&aTool.getReplacements());
     aFinder.addMatcher(makeMatcher(), &aCallback);
-    return aTool.run(clang::tooling::newFrontendActionFactory(&aFinder).get());
+    int nRet =
+        aTool.run(clang::tooling::newFrontendActionFactory(&aFinder).get());
+
+    if (!aExportFixes.empty())
+    {
+        std::error_code aEC;
+        llvm::raw_fd_ostream aOS(aExportFixes, aEC, llvm::sys::fs::F_None);
+        if (aEC)
+        {
+            llvm::errs() << "Error opening output file: " << aEC.message()
+                         << '\n';
+            return 1;
+        }
+
+        clang::tooling::TranslationUnitReplacements aTUR;
+        const clang::tooling::Replacements& rReplacements =
+            aTool.getReplacements();
+        aTUR.Replacements.insert(aTUR.Replacements.end(), rReplacements.begin(),
+                                 rReplacements.end());
+        llvm::yaml::Output aYAML(aOS);
+        aYAML << aTUR;
+        aOS.close();
+    }
+
+    return nRet;
 }
 
 /* vim:set shiftwidth=4 softtabstop=4 expandtab: */
