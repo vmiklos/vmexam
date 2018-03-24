4c4c402499b5bb2830cf4020f81f0837779fb157
Miklos Vajna <vmiklos@collabora.co.uk>
2018-03-24 16:31:58 +0100

    drop find-unneeded-includes v1
    it encouraged non-self-contained headers, forget it

--- libreoffice/include-tidy.sh
+++ libreoffice/include-tidy.sh
@@ -7,19 +7,19 @@
 
 # To debug:
 # ~/git/include-what-you-use/iwyu_tool.py -p . $object 2>&1 |tee out
-# cat out | ~/git/vmexam/libreoffice/find-unneeded-includes2
+# cat out | ~/git/vmexam/libreoffice/find-unneeded-includes
 
 if [ "$1" == "sw-inc" ]; then
     for object in $(git ls-files|grep sw/inc/.*.hxx |grep -v /pch/)
     do
-        ~/git/include-what-you-use/iwyu_tool.py -p . -a sw/source/core/doc/docnew.cxx $object 2>&1 | ~/git/vmexam/libreoffice/find-unneeded-includes2
+        ~/git/include-what-you-use/iwyu_tool.py -p . -a sw/source/core/doc/docnew.cxx $object 2>&1 | ~/git/vmexam/libreoffice/find-unneeded-includes
     done
     exit
 fi
 
 for object in $(cat .git/indented-files.cache .git/indented-files2.cache|grep cxx$)
 do
-    ~/git/include-what-you-use/iwyu_tool.py -p . $object 2>&1 | ~/git/vmexam/libreoffice/find-unneeded-includes2
+    ~/git/include-what-you-use/iwyu_tool.py -p . $object 2>&1 | ~/git/vmexam/libreoffice/find-unneeded-includes
 done
 
 # vim:set shiftwidth=4 softtabstop=4 expandtab:
