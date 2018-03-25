4bc0537b84c2abf32f7e3d8b13c0a204c7c01dea
Miklos Vajna <vmiklos@collabora.co.uk>
2018-03-25 15:49:43 +0200

    include-tidy: thread this

--- libreoffice/include-tidy.py
+++ libreoffice/include-tidy.py
@@ -6,9 +6,12 @@
 #
 
 import glob
+import multiprocessing
 import os
+import queue
 import subprocess
 import sys
+import threading
 
 
 def getIndentedPaths():
@@ -26,23 +29,51 @@ def getIndentedPaths():
     return ret
 
 
-def tidy(paths, assume=None):
-    for path in paths:
-        iwyu_tool = os.path.join(os.environ["HOME"], "git/include-what-you-use/iwyu_tool.py")
-        find_unneeded_includes = os.path.join(os.environ["HOME"], "git/vmexam/libreoffice/find-unneeded-includes")
+def run_tool(assume, task_queue, failed_files):
+    while True:
+        invocation1, invocation2 = task_queue.get()
+        if not len(failed_files):
+            print(" ".join(invocation1) + " | " + " ".join(invocation2))
+            p1 = subprocess.Popen(invocation1, stdout=subprocess.PIPE)
+            p2 = subprocess.Popen(invocation2, stdin=p1.stdout, stdout=subprocess.PIPE)
+            p1.stdout.close()
+            sys.stdout.write(p2.communicate()[0].decode('utf-8'))
+            if p2.returncode != 0:
+                failed_files.append(invocation1)
+        task_queue.task_done()
 
-        invocation1 = [iwyu_tool, "-p", "."]
-        if assume is not None:
-            invocation1 += ["-a", assume]
-        invocation1 += [path]
-        invocation2 = [find_unneeded_includes]
-        print(" ".join(invocation1) + " | " + " ".join(invocation2))
-        p1 = subprocess.Popen(invocation1, stdout=subprocess.PIPE)
-        p2 = subprocess.Popen(invocation2, stdin=p1.stdout, stdout=subprocess.PIPE)
-        p1.stdout.close()
-        sys.stdout.write(p2.communicate()[0].decode('utf-8'))
-        if p2.returncode != 0:
-            break
+
+def tidy(paths, assume=None):
+    return_code = 0
+    try:
+        max_task = multiprocessing.cpu_count()
+        task_queue = queue.Queue(max_task)
+        failed_files = []
+        for _ in range(max_task):
+            t = threading.Thread(target=run_tool, args=(assume, task_queue, failed_files))
+            t.daemon = True
+            t.start()
+
+        for path in sorted(paths):
+            iwyu_tool = os.path.join(os.environ["HOME"], "git/include-what-you-use/iwyu_tool.py")
+            find_unneeded_includes = os.path.join(os.environ["HOME"], "git/vmexam/libreoffice/find-unneeded-includes")
+
+            invocation1 = [iwyu_tool, "-p", "."]
+            if assume is not None:
+                invocation1 += ["-a", assume]
+            invocation1 += [path]
+            invocation2 = [find_unneeded_includes]
+            task_queue.put((invocation1, invocation2))
+
+        task_queue.join()
+        if len(failed_files):
+            return_code = 1
+
+    except KeyboardInterrupt:
+        print('\nCtrl-C detected, goodbye.')
+        os.kill(0, 9)
+
+    sys.exit(return_code)
 
 if __name__ == '__main__':
     area = None
