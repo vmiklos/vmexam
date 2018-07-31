bad52b37cd891901be701d5a78a351594482603c
Miklos Vajna <vmiklos@collabora.co.uk>
2018-07-31 09:17:48 +0200

    openoffice: get this to build again

--- openoffice/up.sh
+++ openoffice/up.sh
@@ -1,8 +1,9 @@
 # git clone git://git.apache.org/openoffice.git
-# tested in a Ubuntu 10.04 chroot, build breaks with a non-ancient toolchain
+# tested in a Ubuntu 14.04 chroot, build breaks with a non-ancient toolchain
+# last successfull commit: 3003f94b10b21648b953df2e71d66680e3605f06 (Added graphics for Norwegian, 2018-07-29)
 time sh -c "git pull -r && \
 	autoconf && \
-	./configure --disable-epm
+	./configure --disable-epm --with-dmake-url=https://sourceforge.net/projects/oooextras.mirror/files/dmake-4.12.tar.bz2 && \
 	make -f Makefile clean && \
 	rm -rf install && \
 	make -f Makefile && \
