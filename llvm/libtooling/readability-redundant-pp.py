1e4f9c9d41a7348177d6e65ae51075ab218bcb6a
Miklos Vajna <vmiklos@collabora.co.uk>
2018-11-03 23:48:25 +0100

    readability-redundant-pp: report inside the main file only, for now

--- llvm/libtooling/readability-redundant-pp.py
+++ llvm/libtooling/readability-redundant-pp.py
@@ -4,13 +4,23 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-# Parses clang-pp output and detects redundant preprocessor directives. Only
-# #ifndef for now.
+# Parses clang's pp-trace output and detects redundant preprocessor directives.
+# Only #ifndef for now.
 #
 
-sock = open("out.yaml")
+import sys
+
+
+# "foo:line:col" -> foo
+def getFileName(loc):
+    return loc[1:-1].split(':')[0]
+
+
+sock = open(sys.argv[1])
 
 inEntry = False
+# Report inside own file only, deciding what is a header guard is tricky.
+ownFile = None
 
 stack = []
 entry = {}
@@ -22,15 +32,20 @@ for line in sock.readlines():
             if entry["Callback"] == "Ifndef":
                 for s in stack:
                     if s["MacroNameTok"] == entry["MacroNameTok"]:
-                        print("nested ifndef of '"+entry["MacroNameTok"]+"', current location is '"+entry["Loc"]+"', previous loc is '"+s["Loc"]+"'")
+                        if getFileName(entry["Loc"]) == ownFile:
+                            print("nested ifndef of '" + entry["MacroNameTok"] + "', current location is '" + entry["Loc"][1:-1] + "', previous loc is '" + s["Loc"][1:-1] + "'")
                 stack.append(entry)
             elif entry["Callback"] == "Endif" and len(stack) and entry["IfLoc"] == stack[-1]["Loc"]:
                 stack.pop()
             entry = {}
         inEntry = True
         entry["Callback"] = line[len("- Callback: "):]
-    elif inEntry and line.startswith("Loc: "):
-        entry["Loc"] = line[len("Loc: "):]
+    elif line.startswith("Loc: "):
+        if inEntry:
+            entry["Loc"] = line[len("Loc: "):]
+        if not ownFile:
+            # "foo:x:y" -> foo
+            ownFile = getFileName(line[len("Loc: "):])
     elif inEntry and line.startswith("IfLoc: "):
         entry["IfLoc"] = line[len("IfLoc: "):]
     elif inEntry and line.startswith("MacroNameTok: "):
