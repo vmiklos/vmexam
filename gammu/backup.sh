c8fa661d1259f235e4a038c079075f0c5995fe63
Miklos Vajna <vmiklos@collabora.com>
2018-11-18 22:57:38 +0100

    gammu: remove old feature phone python2 reader

--- gammu/backup.sh
+++
@@ -1,6 +0,0 @@
-date=`date +%Y%m%d`
-
-echo -e "no\nALL" |gammu --backup backup-$date.txt
-echo -e "no\nALL" |gammu --backupsms backupsms-$date.txt
-gammu --geteachsms > eachsms-$date.txt
-#python backup-fs.py
