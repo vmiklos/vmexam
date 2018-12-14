5c8f7166a69940e31d5d131f8d4be5d4a21691d8
Miklos Vajna <vmiklos@collabora.com>
2018-12-15 00:37:15 +0100

    virt: libvirt-less notes

--- virt/install-opensuse.sh
+++ virt/install-opensuse.sh
@@ -4,3 +4,9 @@ virt-install --name=opensuse --arch=x86_64 --vcpus=1 --ram=8192 --os-type=linux 
 	--cdrom=/home/vmiklos/virt/opensuse/opensuse.iso \
 	--disk path=/home/vmiklos/virt/opensuse/opensuse.img,size=16 --accelerate --vnc \
 	--noautoconsole
+
+# manually:
+# qemu-img create -f qcow2 opensuse.img 16G
+# -cdrom opensuse.iso
+# -net user -redir tcp:2222::22
+qemu-system-x86_64 -enable-kvm -m 1024 -hda opensuse.img -device e1000,netdev=mynet0 -netdev user,id=mynet0,hostfwd=tcp::2222-:22 -vnc :0
