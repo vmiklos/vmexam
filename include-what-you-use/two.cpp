c6ca335f4bc62710c69ffb2395104825301bfcc9
Miklos Vajna <vmiklos@vmiklos.hu>
2019-02-04 22:41:26 +0100

    simplify

--- include-what-you-use/two.cpp
+++ include-what-you-use/two.cpp
@@ -1,5 +1,8 @@
-#include "two.h"
+#include "one.h"
 
-Two::Two(a::b::c::One& /*one*/) {}
-Two::Two(a::b::One2& /*one*/) {}
-Two::Two(a::One3& /*one*/) {}
+struct Two
+{
+    Two(a::b::c::One& one);
+    Two(a::b::One2& one);
+    Two(a::One3& one);
+};
--- include-what-you-use/two.h
+++
@@ -1,8 +0,0 @@
-#include "one.h"
-
-struct Two
-{
-    Two(a::b::c::One& one);
-    Two(a::b::One2& one);
-    Two(a::One3& one);
-};
