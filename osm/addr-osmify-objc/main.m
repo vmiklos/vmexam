aa09b1a42dbd6204619eafec6c4ab73f7c20ef9f
Miklos Vajna <vmiklos@vmiklos.hu>
2019-02-15 23:55:05 +0100

    addr-osmify-objc: wrap long lines

--- osm/addr-osmify-objc/main.m
+++ osm/addr-osmify-objc/main.m
@@ -10,45 +10,57 @@ NSData* queryTurbo(NSString* query)
 {
     NSString* url = @"http://overpass-api.de/api/interpreter";
 
-    NSMutableURLRequest* urlRequest = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:url]];
+    NSMutableURLRequest* urlRequest =
+        [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:url]];
     [urlRequest setHTTPMethod:@"POST"];
     NSData* data = [query dataUsingEncoding:NSUTF8StringEncoding];
     [urlRequest setHTTPBody:data];
     CFRunLoopRef runloop = CFRunLoopGetCurrent();
     NSURLSession* session = [NSURLSession sharedSession];
     __block NSData* ret = nil;
-    NSURLSessionDataTask* dataTask = [session dataTaskWithRequest:urlRequest completionHandler:^(NSData* data, NSURLResponse* response, NSError* error) {
-        NSHTTPURLResponse* httpResponse = (NSHTTPURLResponse *)response;
-        if (!error && httpResponse.statusCode == 200)
-        {
-            ret = data;
-        }
-        CFRunLoopStop(runloop);
-    }];
+    NSURLSessionDataTask* dataTask = [session
+        dataTaskWithRequest:urlRequest
+          completionHandler:^(NSData* data, NSURLResponse* response,
+                              NSError* error) {
+            NSHTTPURLResponse* httpResponse = (NSHTTPURLResponse*)response;
+            if (!error && httpResponse.statusCode == 200)
+            {
+                ret = data;
+            }
+            CFRunLoopStop(runloop);
+          }];
     [dataTask resume];
     CFRunLoopRun();
 
     return ret;
 }
+
 NSData* queryNominatim(NSString* query)
 {
     NSString* urlPrefix = @"http://nominatim.openstreetmap.org/search.php?";
-    NSString* queryEncoded = [query stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLHostAllowedCharacterSet]];
-    NSString* url = [urlPrefix stringByAppendingFormat:@"q=%@&format=json", queryEncoded];
+    NSString* queryEncoded =
+        [query stringByAddingPercentEncodingWithAllowedCharacters:
+                   [NSCharacterSet URLHostAllowedCharacterSet]];
+    NSString* url =
+        [urlPrefix stringByAppendingFormat:@"q=%@&format=json", queryEncoded];
 
-    NSMutableURLRequest* urlRequest = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:url]];
+    NSMutableURLRequest* urlRequest =
+        [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:url]];
     [urlRequest setHTTPMethod:@"GET"];
     CFRunLoopRef runloop = CFRunLoopGetCurrent();
     NSURLSession* session = [NSURLSession sharedSession];
     __block NSData* ret = nil;
-    NSURLSessionDataTask* dataTask = [session dataTaskWithRequest:urlRequest completionHandler:^(NSData* data, NSURLResponse* response, NSError* error) {
-        NSHTTPURLResponse* httpResponse = (NSHTTPURLResponse *)response;
-        if (!error && httpResponse.statusCode == 200)
-        {
-            ret = data;
-        }
-        CFRunLoopStop(runloop);
-    }];
+    NSURLSessionDataTask* dataTask = [session
+        dataTaskWithRequest:urlRequest
+          completionHandler:^(NSData* data, NSURLResponse* response,
+                              NSError* error) {
+            NSHTTPURLResponse* httpResponse = (NSHTTPURLResponse*)response;
+            if (!error && httpResponse.statusCode == 200)
+            {
+                ret = data;
+            }
+            CFRunLoopStop(runloop);
+          }];
     [dataTask resume];
     CFRunLoopRun();
 
@@ -66,7 +78,10 @@ void osmify(NSString* query)
         return;
     }
 
-    NSArray* elements = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:&parseError];
+    NSArray* elements =
+        [NSJSONSerialization JSONObjectWithData:data
+                                        options:NSJSONReadingMutableContainers
+                                          error:&parseError];
     if ([elements count] < 1)
     {
         printf("No results from nominatim.\n");
@@ -86,10 +101,11 @@ void osmify(NSString* query)
 
     // Use overpass to get the properties of the object.
     NSString* overpassQuery = [NSString stringWithFormat:@"[out:json];\n"
-        "(\n"
-        "%@(%@);\n"
-        ");\n"
-        "out body;", objectType, objectId];
+                                                          "(\n"
+                                                          "%@(%@);\n"
+                                                          ");\n"
+                                                          "out body;",
+                                                         objectType, objectId];
     data = queryTurbo(overpassQuery);
     if (!data)
     {
@@ -97,22 +113,30 @@ void osmify(NSString* query)
         return;
     }
 
-    NSDictionary* j = [NSJSONSerialization JSONObjectWithData:data options:0 error:&parseError];
+    NSDictionary* j = [NSJSONSerialization JSONObjectWithData:data
+                                                      options:0
+                                                        error:&parseError];
     elements = [j objectForKey:@"elements"];
-    if ([elements count] < 1) {
+    if ([elements count] < 1)
+    {
         printf("No results from overpass.\n");
         return;
     }
 
     element = elements[0];
     NSString* city = [[element objectForKey:@"tags"] objectForKey:@"addr:city"];
-    NSString* housenumber = [[element objectForKey:@"tags"] objectForKey:@"addr:housenumber"];
-    NSString* postcode = [[element objectForKey:@"tags"] objectForKey:@"addr:postcode"];
-    NSString* street = [[element objectForKey:@"tags"] objectForKey:@"addr:street"];
-    NSString* addr = [NSString stringWithFormat:@"%@ %@, %@ %@", postcode, city, street, housenumber];
+    NSString* housenumber =
+        [[element objectForKey:@"tags"] objectForKey:@"addr:housenumber"];
+    NSString* postcode =
+        [[element objectForKey:@"tags"] objectForKey:@"addr:postcode"];
+    NSString* street =
+        [[element objectForKey:@"tags"] objectForKey:@"addr:street"];
+    NSString* addr = [NSString
+        stringWithFormat:@"%@ %@, %@ %@", postcode, city, street, housenumber];
 
     // Print the result.
-    printf("%s\n", [[NSString stringWithFormat:@"geo:%@,%@ (%@)", lat, lon, addr] UTF8String]);
+    printf("%s\n", [[NSString stringWithFormat:@"geo:%@,%@ (%@)", lat, lon,
+                                               addr] UTF8String]);
 }
 
 int main(int argc, const char* argv[])
@@ -125,9 +149,10 @@ int main(int argc, const char* argv[])
         }
         else
         {
-            printf("%s\n", [@"usage: addr-osmify <query>" cStringUsingEncoding:NSUTF8StringEncoding]);
+            printf("%s\n", [@"usage: addr-osmify <query>" UTF8String]);
             printf("\n");
-            printf("%s\n", [@"e.g. addr-osmify 'Mészáros utca 58/a, Budapest'" cStringUsingEncoding:NSUTF8StringEncoding]);
+            printf("%s\n", [@"e.g. addr-osmify 'Mészáros utca 58/a, Budapest'"
+                               UTF8String]);
         }
     }
     return 0;
