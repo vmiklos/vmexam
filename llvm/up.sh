c05f3db41be19caf53ebb21e72680fb6e01ff998
Miklos Vajna <vmiklos@vmiklos.hu>
2019-03-09 22:33:27 +0100

    llvm: add back timing and logging
    finally figured out how to do this without quoting issues...

--- llvm/up.sh
+++ llvm/up.sh
@@ -1,28 +1,30 @@
 #!/bin/bash -ex
 
-git pull -r
-mkdir -p workdir
-cd projects/compiler-rt
-git pull -r
-cd ../../tools/clang
-git pull -r
-git svn rebase -l
-cd tools/extra
-git pull -r
-git svn rebase -l
-cd ../../../../workdir
-cmake \
-    -G 'Unix Makefiles' \
-    -DCMAKE_INSTALL_PREFIX=$PWD/../instdir \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DLLVM_ENABLE_ASSERTIONS=ON \
-    -DLLVM_ENABLE_SPHINX=ON \
-    -DCMAKE_C_COMPILER="clang" \
-    -DCMAKE_CXX_COMPILER="clang++" \
-    ..
-make -j8
-make install
-make -j8 check-clang-tools
+time (
+    git pull -r
+    mkdir -p workdir
+    cd projects/compiler-rt
+    git pull -r
+    cd ../../tools/clang
+    git pull -r
+    git svn rebase -l
+    cd tools/extra
+    git pull -r
+    git svn rebase -l
+    cd ../../../../workdir
+    cmake \
+        -G 'Unix Makefiles' \
+        -DCMAKE_INSTALL_PREFIX=$PWD/../instdir \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DLLVM_ENABLE_ASSERTIONS=ON \
+        -DLLVM_ENABLE_SPHINX=ON \
+        -DCMAKE_C_COMPILER="clang" \
+        -DCMAKE_CXX_COMPILER="clang++" \
+        ..
+    make -j8
+    make install
+    make -j8 check-clang-tools
+) 2>&1 |tee log
 
 # How to enable symbols for profiling: -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_FLAGS='-fno-omit-frame-pointer' -DCMAKE_CXX_FLAGS='-fno-omit-frame-pointer'
 # How to enable shared libs: -DBUILD_SHARED_LIBS=ON
