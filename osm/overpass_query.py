cd15dbf70f47edac0802e991f0c2560731ea2e91
Miklos Vajna <vmiklos@vmiklos.hu>
2019-04-27 20:07:31 +0200

    osm: refactor overpass-query to be a library

---
+++ osm/overpass_query.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+#
+# Copyright 2019 Miklos Vajna. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+
+from urllib.request import urlopen
+import sys
+
+def overpassQuery(query):
+    url = "http://overpass-api.de/api/interpreter"
+
+    sock = urlopen(url, bytes(query, "utf-8"))
+    buf = sock.read()
+    sock.close()
+
+    return buf
+
+if __name__ == "__main__":
+    sock = open(sys.argv[1])
+    query = sock.read()
+    sock.close()
+
+    buf = overpassQuery(query)
+
+    sys.stdout.write(buf.decode("utf-8"))
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab:
