6032546885ab235b639a67e4ae718b2195dff6be
Miklos Vajna <vmiklos@collabora.com>
2019-05-07 10:06:08 +0200

    lo: restore loolwsd build timing and log

--- libreoffice/up-loolwsd.sh
+++ libreoffice/up-loolwsd.sh
@@ -1,26 +1,28 @@
 #!/bin/bash -ex
 
-git pull -r
-if [ -e Makefile ]; then
-    make distclean
-fi
-./autogen.sh
-./configure \
-    --prefix=$PWD/install \
-    --enable-debug \
-    --with-lo-path=$HOME/git/libreoffice/master/instdir \
-    --with-lokit-path=$HOME/git/libreoffice/master/include \
-    CC="ccache $HOME/git/llvm/instdir/bin/clang" \
-    CXX="ccache $HOME/git/llvm/instdir/bin/clang++" \
-    CFLAGS='-g -O0' \
-    CXXFLAGS='-g -O0' \
+time (
+    git pull -r
+    if [ -e Makefile ]; then
+        make distclean
+    fi
+    ./autogen.sh
+    ./configure \
+        --prefix=$PWD/install \
+        --enable-debug \
+        --with-lo-path=$HOME/git/libreoffice/master/instdir \
+        --with-lokit-path=$HOME/git/libreoffice/master/include \
+        CC="ccache $HOME/git/llvm/instdir/bin/clang" \
+        CXX="ccache $HOME/git/llvm/instdir/bin/clang++" \
+        CFLAGS='-g -O0' \
+        CXXFLAGS='-g -O0' \
 
-# Self-built poco:
-#    --with-poco-includes=$HOME/git/poco/install/include \
-#    --with-poco-libs=$HOME/git/poco/lib/Linux/x86_64 \
+    # Self-built poco:
+    #    --with-poco-includes=$HOME/git/poco/install/include \
+    #    --with-poco-libs=$HOME/git/poco/lib/Linux/x86_64 \
 
-make -j$(getconf _NPROCESSORS_ONLN)
-make ctags
-make check
+    make -j$(getconf _NPROCESSORS_ONLN)
+    make ctags
+    make check
+) 2>&1 |tee log
 
 # vim:set shiftwidth=4 expandtab:
