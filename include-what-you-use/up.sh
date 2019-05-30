a57f0bb8ec7cfadfccbd84cf8fe98e21c5374cb7
Miklos Vajna <vmiklos@vmiklos.hu>
2019-05-30 17:52:48 +0200

    iwyu: fix up build script to work with clang trunk
    openSUSE Leap 15.1 defaults to clang version 5.0.2, and I'm interested
    in --no_fwd_decls (which needs clang 8).

--- include-what-you-use/up.sh
+++ include-what-you-use/up.sh
@@ -1,19 +1,22 @@
 #!/bin/bash -ex
 
-# cmake prefix path is meant to be valid in a Travis env
+# cmake prefix path is meant to be valid for a self-built clang
 
 git pull -r
+rm -rf workdir instdir
 mkdir -p workdir
 cd workdir
 cmake \
     -G 'Unix Makefiles' \
     -DCMAKE_INSTALL_PREFIX=$PWD/../instdir \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_PREFIX_PATH=/usr/lib/llvm-7 \
+    -DCMAKE_PREFIX_PATH=$HOME/git/llvm/instdir \
     -DCMAKE_C_COMPILER="clang" \
     -DCMAKE_CXX_COMPILER="clang++" \
     ..
 make -j8
 make install
+cd ../instdir
+ln -s $HOME/git/llvm/instdir/lib
 
 # vim:set shiftwidth=4 expandtab:
