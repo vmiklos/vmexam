d62174d40bc360095271ea3f23fffcbea1f6c43d
Miklos Vajna <vmiklos@vmiklos.hu>
2019-08-09 22:14:26 +0200

    js: szihkcal: replace copy&paste with config

--- js/szihkcal/node_modules/cldr/lib/normalizeLocaleId.js
+++
@@ -1,6 +0,0 @@
-/*
- * Replace - with _ and convert to lower case: en-GB => en_gb
- */
-module.exports = function normalizeLocaleId(localeId) {
-    return localeId && localeId.replace(/-/g, '_').toLowerCase();
-};
