d3afadf08010e77e7572e989c0a611772f70c8ee
Miklos Vajna <vmiklos@vmiklos.hu>
2019-08-09 22:04:50 +0200

    addr-osmify-js: print url, minify more

--- osm/addr-osmify-js/simple-https-server.py
+++ osm/addr-osmify-js/simple-https-server.py
@@ -11,4 +11,5 @@ import ssl
 
 httpd = http.server.HTTPServer(('localhost', 4443), http.server.SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket (httpd.socket, certfile='./server.pem', server_side=True)
+print("Serving HTTP on localhost port 4443 (https://localhost:4443/) ...")
 httpd.serve_forever()
