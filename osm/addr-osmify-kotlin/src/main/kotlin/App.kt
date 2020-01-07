3c42046313721e9179a2b8446134bfa10282ec90
Miklos Vajna <vmiklos@vmiklos.hu>
2020-01-07 18:55:57 +0100

    addr-osmify-kotlin: get rid of the hello package name, add Makefile

---
+++ osm/addr-osmify-kotlin/src/main/kotlin/App.kt
@@ -0,0 +1,5 @@
+package hu.vmiklos.addr_osmify
+
+fun main(args: Array<String>) {
+    println("addr_osmify")
+}
---
+++ osm/addr-osmify-kotlin/src/test/kotlin/AppTest.kt
@@ -0,0 +1,10 @@
+package hu.vmiklos.addr_osmify
+
+import kotlin.test.assertEquals
+import org.junit.Test
+
+class AppTest {
+    @Test fun testAssert() {
+        assertEquals(2, 1 + 1)
+    }
+}
