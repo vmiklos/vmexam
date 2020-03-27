8759abd4b4e7d57b7fdef135cde4b13b2a9d057d
Miklos Vajna <vmiklos@vmiklos.hu>
2020-03-27 21:51:53 +0100

    hungary-housenumbers-progressbar: more refactor to allow multiple charts

--- osm/qa/hungary-housenumbers-progressbar/stats.py
+++ osm/qa/hungary-housenumbers-progressbar/stats.py
@@ -10,19 +10,25 @@ import time
 import sys
 
 
-def main() -> None:
-    """Commandline interface to this module."""
+def handle_progress(j):
+    """Generates stats for a global progressbar."""
+    ret = {}
     with open("ref.count", "r") as stream:
         num_ref = int(stream.read().strip())
     today = time.strftime("%Y-%m-%d")
     with open("%s.count" % today, "r") as stream:
         num_osm = int(stream.read().strip())
     percentage = round(num_osm * 100 / num_ref, 2)
+    ret["date"] = today
+    ret["percentage"] = percentage
+    ret["reference"] = num_ref
+    ret["osm"] = num_osm
+    j["progress"] = ret
+
+def main() -> None:
+    """Commandline interface to this module."""
     j = {}
-    j["date"] = today
-    j["percentage"] = percentage
-    j["reference"] = num_ref
-    j["osm"] = num_osm
+    handle_progress(j)
     print(json.dumps(j))
 
 
