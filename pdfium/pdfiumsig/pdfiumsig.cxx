3128ea0f7b4023f809f5b0d4046d7b99d5a16e02
Miklos Vajna <vmiklos@vmiklos.hu>
2020-06-25 22:39:46 +0200

    pdfium: updates notes

---
+++ pdfium/pdfiumsig/pdfiumsig.cxx
@@ -0,0 +1,36 @@
+#include <cassert>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+#include <fpdf_edit.h>
+#include <fpdf_save.h>
+#include <fpdf_signature.h>
+#include <fpdfview.h>
+
+int main(int argc, char* argv[])
+{
+    FPDF_LIBRARY_CONFIG config;
+    config.version = 2;
+    config.m_pUserFontPaths = nullptr;
+    config.m_pIsolate = nullptr;
+    config.m_v8EmbedderSlot = 0;
+    FPDF_InitLibraryWithConfig(&config);
+
+    std::ifstream testFile(argv[1], std::ios::binary);
+    std::vector<char> fileContents((std::istreambuf_iterator<char>(testFile)),
+                                   std::istreambuf_iterator<char>());
+    FPDF_DOCUMENT document = FPDF_LoadMemDocument(
+        fileContents.data(), fileContents.size(), /*password=*/nullptr);
+    assert(document);
+
+    int signatureCount = FPDF_GetSignatureCount(document);
+    std::cerr << "debug, signatureCount is " << signatureCount << std::endl;
+
+    FPDF_CloseDocument(document);
+
+    FPDF_DestroyLibrary();
+}
+
+/* vim:set shiftwidth=4 softtabstop=4 expandtab: */
