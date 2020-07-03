56c6e769827ab99712d4f1a9bb8753d6320ec427
Miklos Vajna <vmiklos@vmiklos.hu>
2020-07-03 14:41:22 +0200

    pdfiumsig: towards digest verify

--- pdfium/pdfiumsig/pdfiumsig.cxx
+++ pdfium/pdfiumsig/pdfiumsig.cxx
@@ -1,5 +1,6 @@
 #include <cassert>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <iterator>
 #include <vector>
@@ -8,6 +9,71 @@
 #include <fpdf_save.h>
 #include <fpdf_signature.h>
 #include <fpdfview.h>
+
+#include "core/fpdfapi/parser/cpdf_array.h"
+#include "core/fpdfapi/parser/cpdf_dictionary.h"
+#include "fpdfsdk/cpdfsdk_helpers.h"
+
+void validateBytes(const std::vector<char>& bytes,
+                   const std::vector<std::pair<size_t, size_t>>& byteRanges,
+                   const std::vector<char>& contents)
+{
+}
+
+void validateSignature(const std::vector<char>& bytes, FPDF_SIGNATURE signature,
+                       int signatureIndex)
+{
+    std::cerr << "Signature #" << signatureIndex << ":" << std::endl;
+    int contentsLen = FPDFSignatureObj_GetContents(signature, nullptr, 0);
+    std::vector<char> contents(contentsLen);
+    FPDFSignatureObj_GetContents(signature, contents.data(), contents.size());
+
+    CPDF_Dictionary* signatureDict = CPDFDictionaryFromFPDFSignature(signature);
+    CPDF_Dictionary* valueDict = signatureDict->GetDictFor("V");
+    const CPDF_Array* byteRangeArray = valueDict->GetArrayFor("ByteRange");
+    ByteString subFilter = valueDict->GetNameFor("SubFilter");
+
+    std::vector<std::pair<size_t, size_t>> byteRanges;
+    size_t byteRangeOffset = 0;
+    for (size_t i = 0; i < byteRangeArray->size(); ++i)
+    {
+        float number = byteRangeArray->GetNumberAt(i);
+        if (i % 2 == 0)
+        {
+            byteRangeOffset = number;
+            continue;
+        }
+
+        size_t byteRangeLength = number;
+        byteRanges.emplace_back(byteRangeOffset, byteRangeLength);
+    }
+
+    // Sanity checks.
+    if (byteRanges.size() < 2)
+    {
+        std::cerr << "warning, expected 2 byte ranges" << std::endl;
+        return;
+    }
+
+    if (byteRanges[0].first != 0)
+    {
+        std::cerr << "warning, first range start is not 0" << std::endl;
+        return;
+    }
+
+    // Binary vs hex dump and 2 is the leading "<" and the trailing ">" around
+    // the hex string.
+    size_t signatureLength = contents.size() * 2 + 2;
+    if (byteRanges[1].first != (byteRanges[0].second + signatureLength))
+    {
+        std::cerr
+            << "warning, second range start is not the end of the signature"
+            << std::endl;
+        return;
+    }
+
+    validateBytes(bytes, byteRanges, contents);
+}
 
 int main(int argc, char* argv[])
 {
@@ -28,9 +94,8 @@ int main(int argc, char* argv[])
     int signatureCount = FPDF_GetSignatureCount(document);
     for (int i = 0; i < signatureCount; ++i)
     {
-        std::cerr << "Signature #" << i << ":" << std::endl;
-        std::cerr << "debug, signature is "
-                  << FPDF_GetSignatureObject(document, i) << std::endl;
+        FPDF_SIGNATURE signature = FPDF_GetSignatureObject(document, i);
+        validateSignature(fileContents, signature, i);
     }
 
     FPDF_CloseDocument(document);
