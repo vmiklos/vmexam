ef868db177a232d824944b8cd040bd5ff8106e57
Miklos Vajna <vmiklos@collabora.com>
2020-10-08 17:49:53 +0200

    git: add email sender script, git hook and system unit

---
+++ git/email.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Copyright 2020 Miklos Vajna. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+
+# I run this as ~/git/email/mirror.sh.
+
+export PATH=$HOME/bin:$PATH
+
+cd $(dirname $0)
+while true
+do
+    for repo in ged2dot online
+    do
+        cd $repo-mirror.git
+        git fetch -q github
+        git push -q email
+        cd - >/dev/null
+    done
+    sleep 60
+done
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab:
