e91140bdf02861d9cc1fe0f5fa1e96215725875a
Miklos Vajna <vmiklos@vmiklos.hu>
2020-11-25 20:10:20 +0100

    js/mid: migrate this to typescript

--- js/mid/main.ts
+++ js/mid/main.ts
@@ -4,45 +4,45 @@
  * found in the LICENSE file.
  */
 
-var domready = require('domready');
+import domready = require('domready');
 
 function mailArchiveJump()
 {
-    var msgid = document.getElementById('msgid').value;
+    const msgid = (<HTMLInputElement>document.getElementById('msgid')).value;
     document.location.href =
         'https://www.mail-archive.com/search?l=mid&q=' + msgid;
 }
 
 function debianJump()
 {
-    var msgid = document.getElementById('msgid').value;
+    const msgid = (<HTMLInputElement>document.getElementById('msgid')).value;
     document.location.href = 'https://lists.debian.org/msgid-search/' + msgid;
 }
 
 function mailArchiveShow()
 {
-    var msgidElement = document.getElementById('msgid');
-    var msgid = msgidElement.value;
+    const msgidElement = <HTMLInputElement>document.getElementById('msgid');
+    const msgid = msgidElement.value;
     msgidElement.value = 'https://www.mail-archive.com/search?l=mid&q=' + msgid;
 }
 
 function debianShow()
 {
-    var msgidElement = document.getElementById('msgid');
-    var msgid = msgidElement.value;
+    const msgidElement = <HTMLInputElement>document.getElementById('msgid');
+    const msgid = msgidElement.value;
     msgidElement.value = 'https://lists.debian.org/msgid-search/' + msgid;
 }
 
 domready(function() {
     // Create our page.
-    var body = document.getElementsByTagName('body')[0];
-    var desc = document.createElement('p');
+    const body = document.getElementsByTagName('body')[0];
+    const desc = document.createElement('p');
     desc.appendChild(document.createTextNode(
         'Takes a Message-ID from a public mailing list and attempts to look up a public archive entry for it from multiple sources.'));
     body.appendChild(desc);
 
-    var input = document.createElement('p');
-    var msgidInput = document.createElement('input');
+    const input = document.createElement('p');
+    const msgidInput = document.createElement('input');
     msgidInput.id = 'msgid';
     msgidInput.type = 'text';
     msgidInput.placeholder = 'Message-ID';
@@ -50,26 +50,26 @@ domready(function() {
     input.appendChild(msgidInput);
 
     input.appendChild(document.createElement('br'));
-    var mailArchiveLink = document.createElement('input');
+    const mailArchiveLink = document.createElement('input');
     mailArchiveLink.type = 'button';
     mailArchiveLink.value = 'Mail Archive Link';
     mailArchiveLink.onclick = mailArchiveShow;
     input.appendChild(mailArchiveLink);
     input.appendChild(document.createTextNode(' '));
-    var debianLink = document.createElement('input');
+    const debianLink = document.createElement('input');
     debianLink.type = 'button';
     debianLink.value = 'Debian Link';
     debianLink.onclick = debianShow;
     input.appendChild(debianLink);
 
     input.appendChild(document.createElement('br'));
-    var mailArchive = document.createElement('input');
+    const mailArchive = document.createElement('input');
     mailArchive.type = 'button';
     mailArchive.value = 'Mail Archive Jump';
     mailArchive.onclick = mailArchiveJump;
     input.appendChild(mailArchive);
     input.appendChild(document.createTextNode(' '));
-    var debian = document.createElement('input');
+    const debian = document.createElement('input');
     debian.type = 'button';
     debian.value = 'Debian Jump';
     debian.onclick = debianJump;
@@ -77,12 +77,12 @@ domready(function() {
 
     body.appendChild(input);
 
-    var maExample = document.createElement('p');
+    const maExample = document.createElement('p');
     maExample.appendChild(document.createTextNode(
         'Example Mail Archive MessageID: cfb1d155-499d-3205-8283-ce84c39dbb14@redhat.com'));
     body.appendChild(maExample);
 
-    var dExample = document.createElement('p');
+    const dExample = document.createElement('p');
     dExample.appendChild(document.createTextNode(
         'Example Debian MessageID: 20171121214924.kwjklln5t6t7dedh@rene-engelhard.de'));
     body.appendChild(dExample);
