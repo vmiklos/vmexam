5af9c262302d6b25fb1acd573b0b5b3a0b5c2320
Miklos Vajna <vmiklos@vmiklos.hu>
2021-04-08 17:57:56 +0200

    addr-osmify-cpp: lcov PR is now merged
    See https://github.com/linux-test-project/lcov/pull/119

--- osm/addr-osmify-cpp/coverage.sh
+++ osm/addr-osmify-cpp/coverage.sh
@@ -20,6 +20,7 @@ genhtml -o coverage osmify.info
 echo "Coverage report is now available at coverage/index.html."
 
 # Prints line coverage.
-lcov --summary osmify.info
+# lcov-1.14+git.20180307.a5dd952-lp152.3.2.noarch doesn't have --fail-under-lines yet.
+~/git/lcov/bin/lcov --summary osmify.info --fail-under-lines 100
 
 # vim:set shiftwidth=4 softtabstop=4 expandtab:
