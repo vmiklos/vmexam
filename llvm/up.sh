5332d7100dc4f8bc41a0ccb9e59216c448825346
Miklos Vajna <vmiklos@vmiklos.hu>
2021-04-10 20:07:48 +0200

    lo, clang-tidy: blacklist readability-function-cognitive-complexity

--- llvm/up.sh
+++ llvm/up.sh
@@ -18,6 +18,7 @@ time (
     make install
     make -j8 check-clang-tools
 ) 2>&1 |tee log
+exit ${PIPESTATUS[0]}
 
 # How to enable symbols for profiling: -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_FLAGS='-fno-omit-frame-pointer' -DCMAKE_CXX_FLAGS='-fno-omit-frame-pointer'
 # How to enable shared libs: -DBUILD_SHARED_LIBS=ON
