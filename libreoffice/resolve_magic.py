d9135a681437d4b48b5e134b5d32411d96dfb5eb
Miklos Vajna <vmiklos@vmiklos.hu>
2021-04-11 15:29:09 +0200

    lo: add script to help magic number killing

---
+++ libreoffice/resolve_magic.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+#
+# Resolves a magic number if we know it was constructed from flags of an enum.
+#
+# Example:
+#
+# $ ./resolve_magic.py SwFrameInvFlags 0x13
+# SwFrameInvFlags::InvalidatePrt | SwFrameInvFlags::InvalidateSize | SwFrameInvFlags::NextInvalidatePos
+#
+
+import sys
+
+SwFrameInvFlags = {
+    0x01: "InvalidatePrt",
+    0x02: "InvalidateSize",
+    0x04: "InvalidatePos",
+    0x08: "SetCompletePaint",
+    0x10: "NextInvalidatePos",
+    0x20: "NextSetCompletePaint",
+}
+
+
+def main():
+    enum = sys.argv[1]
+    magic = int(sys.argv[2], 16)
+
+    flags = []
+    enum_dict = globals()[enum]
+    for key, value in enum_dict.items():
+        if magic & key:
+            flags.append(value)
+    print(" | ".join(["%s::%s" % (enum, i) for i in flags]))
+
+
+if __name__ == '__main__':
+    main()
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab:
