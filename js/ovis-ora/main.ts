77d747246f895d3c6192bdd4ea8d996d2ee095ba
Miklos Vajna <vmiklos@vmiklos.hu>
2021-12-10 22:38:05 +0100

    ovis-ora: convert to typescript

---
+++ js/ovis-ora/main.ts
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2021 Miklos Vajna. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+import domready = require('domready');
+
+async function drawClock(ratio: number)
+{
+    const canvas = <HTMLCanvasElement>document.getElementById('mycanvas');
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, 1000, 1000);
+    ctx.fillStyle = 'rgb(0, 255, 0)';
+    ctx.fillRect(0, 0, 1000 * ratio, 1000);
+    ctx.fillStyle = 'rgb(255, 0, 0)';
+    ctx.fillRect(1000 * ratio, 0, 1000 - 1000 * ratio, 1000);
+}
+
+async function submitClick()
+{
+    const durationElement =
+        <HTMLInputElement>document.querySelector('#duration');
+    durationElement.style.display = 'none';
+    let duration = Number(durationElement.value);
+    const submitElement = <HTMLButtonElement>document.querySelector('#submit');
+    submitElement.style.display = 'none;';
+    const canvas = <HTMLCanvasElement>document.getElementById('mycanvas');
+    canvas.style.display = 'inline';
+    duration *= 60;
+    for (let i = 1; i <= duration; i++)
+    {
+        drawClock(i / duration);
+        await new Promise(r => setTimeout(r, 1000));
+    }
+
+    // Full green: end text.
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = "#000000";
+    ctx.font = '100px sans-serif';
+    const text = "Vége";
+    const textWidth = ctx.measureText(text).width;
+    ctx.fillText(text, (canvas.width / 2) - (textWidth / 2), 500);
+}
+
+domready(function() {
+    // Create our page.
+    const body = document.getElementsByTagName('body')[0];
+    const canvas = document.createElement('canvas');
+    canvas.id = 'mycanvas';
+    canvas.width = 1000;
+    canvas.height = 1000;
+    canvas.style.position = 'fixed';
+    canvas.style.left = '0';
+    canvas.style.top = '0'
+    canvas.style.width = '100%';
+    canvas.style.height = '100%';
+    canvas.style.display = 'none';
+    body.appendChild(canvas);
+
+    const duration = document.createElement('input');
+    duration.id = 'duration';
+    duration.type = 'text';
+    duration.placeholder = 'Perc';
+    duration.style.width = '50%';
+    body.appendChild(duration);
+
+    const submit = document.createElement('input');
+    submit.id = 'submit';
+    submit.type = 'button';
+    submit.value = 'OK';
+    submit.onclick = submitClick;
+    body.appendChild(submit);
+});
+
+// vim: shiftwidth=4 softtabstop=4 expandtab:
