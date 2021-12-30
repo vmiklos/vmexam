dc0bf11c5a2c06860ef1a7936f170edde3f3b748
Miklos Vajna <vmiklos@vmiklos.hu>
2021-12-30 15:53:41 +0100

    connect hyphen to rouille

--- rust/rouille/vmexam-rouille/src/hyphen.rs
+++ rust/rouille/vmexam-rouille/src/hyphen.rs
@@ -4,9 +4,36 @@ struct HyphenResult {
     error: String,
 }
 
-pub fn app(_request: &rouille::Request) -> rouille::Response {
+pub fn app(request: &rouille::Request) -> rouille::Response {
+    let word = match request.get_param("word") {
+        Some(value) => value,
+        None => {
+            return rouille::Response::json(&HyphenResult {
+                hyphenated: "".to_owned(),
+                error: "missing GET param: word".to_owned(),
+            });
+        }
+    };
+    let dict = match hyphen::HyphenDict::new("/usr/share/hyphen/hyph_hu_HU.dic") {
+        Ok(value) => value,
+        Err(err) => {
+            return rouille::Response::json(&HyphenResult {
+                hyphenated: "".to_owned(),
+                error: format!("failed to load the dict: {:?}", err),
+            });
+        }
+    };
+    let hyphenated = match dict.hyphenate(&word) {
+        Ok(value) => value,
+        Err(err) => {
+            return rouille::Response::json(&HyphenResult {
+                hyphenated: "".to_owned(),
+                error: format!("failed to hyphenate the word: {:?}", err),
+            });
+        }
+    };
     let response = rouille::Response::json(&HyphenResult {
-        hyphenated: "asz=szony=nyal".to_owned(),
+        hyphenated,
         error: "".to_owned(),
     });
     response
