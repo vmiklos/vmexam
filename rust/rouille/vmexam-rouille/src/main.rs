c12dd0452121e3ddcdb519e60b8d5e1ad161dc5b
Miklos Vajna <vmiklos@vmiklos.hu>
2021-12-30 15:58:54 +0100

    rouille: allow multiple apps on the same tcp port
    This is the whole point of this workspace, so that a single process can
    run multiple "apps".

--- rust/rouille/vmexam-rouille/src/main.rs
+++ rust/rouille/vmexam-rouille/src/main.rs
@@ -8,6 +8,10 @@ fn main() -> anyhow::Result<()> {
         port, prefix
     );
     rouille::start_server_with_pool(format!("127.0.0.1:{}", port), None, move |request| {
-        hyphen::app(request)
+        if request.url().starts_with("/rouille/hyphen") {
+            return hyphen::app(request);
+        }
+
+        rouille::Response::empty_404()
     });
 }
