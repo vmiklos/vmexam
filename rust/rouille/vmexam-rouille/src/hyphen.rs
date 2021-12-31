723adc772afc18630cf782cc9b5ecc6238a55cfa
Miklos Vajna <vmiklos@vmiklos.hu>
2021-12-31 21:31:38 +0100

    vmexam-rouille: clean up erro handling in the hyphen app
    And use /apps as the common prefix, Nextcloud-style.

--- rust/rouille/vmexam-rouille/src/hyphen.rs
+++ rust/rouille/vmexam-rouille/src/hyphen.rs
@@ -1,40 +1,31 @@
+use anyhow::Context;
+
 #[derive(serde::Serialize)]
 struct HyphenResult {
     hyphenated: String,
     error: String,
 }
 
+pub fn our_app(request: &rouille::Request) -> anyhow::Result<String> {
+    let word = request
+        .get_param("word")
+        .context("missing GET param: word")?
+        .to_string();
+    let dict = hyphen::HyphenDict::new("/usr/share/hyphen/hyph_hu_HU.dic")
+        .context("failed to load the dictionary")?;
+    Ok(dict.hyphenate(&word).context("failed to hyphenate")?)
+}
+
 pub fn app(request: &rouille::Request) -> rouille::Response {
-    let word = match request.get_param("word") {
-        Some(value) => value,
-        None => {
-            return rouille::Response::json(&HyphenResult {
-                hyphenated: "".to_owned(),
-                error: "missing GET param: word".to_owned(),
-            });
-        }
+    let result = match our_app(request) {
+        Ok(hyphenated) => HyphenResult {
+            hyphenated,
+            error: "".to_string(),
+        },
+        Err(err) => HyphenResult {
+            hyphenated: "".to_string(),
+            error: format!("{:?}", err),
+        },
     };
-    let dict = match hyphen::HyphenDict::new("/usr/share/hyphen/hyph_hu_HU.dic") {
-        Ok(value) => value,
-        Err(err) => {
-            return rouille::Response::json(&HyphenResult {
-                hyphenated: "".to_owned(),
-                error: format!("failed to load the dict: {:?}", err),
-            });
-        }
-    };
-    let hyphenated = match dict.hyphenate(&word) {
-        Ok(value) => value,
-        Err(err) => {
-            return rouille::Response::json(&HyphenResult {
-                hyphenated: "".to_owned(),
-                error: format!("failed to hyphenate the word: {:?}", err),
-            });
-        }
-    };
-    let response = rouille::Response::json(&HyphenResult {
-        hyphenated,
-        error: "".to_owned(),
-    });
-    response
+    rouille::Response::json(&result)
 }
--- rust/rouille/vmexam-rouille/src/main.rs
+++ rust/rouille/vmexam-rouille/src/main.rs
@@ -2,13 +2,13 @@ mod hyphen;
 
 fn main() -> anyhow::Result<()> {
     let port = 8001;
-    let prefix = "/rouille";
+    let prefix = "/apps";
     println!(
         "Starting the server at <http://127.0.0.1:{}{}/>.",
         port, prefix
     );
     rouille::start_server_with_pool(format!("127.0.0.1:{}", port), None, move |request| {
-        if request.url().starts_with("/rouille/hyphen") {
+        if request.url().starts_with("/apps/hyphen") {
             return hyphen::app(request);
         }
 
