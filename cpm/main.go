cbae0da3f441573100720169e9ec6c559f3319d4
Miklos Vajna <vmiklos@vmiklos.hu>
2022-07-05 22:21:04 +0200

    cpm: implement update and delete

--- cpm/main.go
+++ cpm/main.go
@@ -40,6 +40,65 @@ func newCreateCommand(db *sql.DB) *cobra.Command {
 	return cmd
 }
 
+func newUpdateCommand(db *sql.DB) *cobra.Command {
+	var machine string
+	var service string
+	var user string
+	var password string
+	var cmd = &cobra.Command{
+		Use:   "update",
+		Short: "updates an existing password",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			query, err := db.Prepare("update passwords set password=? where machine=? and service=? and user=?")
+			if err != nil {
+				return fmt.Errorf("db.Prepare(update) failed: %s", err)
+			}
+
+			_, err = query.Exec(password, machine, service, user)
+
+			return nil
+		},
+	}
+	cmd.Flags().StringVarP(&machine, "machine", "m", "", "machine (required)")
+	cmd.MarkFlagRequired("machine")
+	cmd.Flags().StringVarP(&service, "service", "s", "", "service (required)")
+	cmd.MarkFlagRequired("service")
+	cmd.Flags().StringVarP(&user, "user", "u", "", "user (required)")
+	cmd.MarkFlagRequired("user")
+	cmd.Flags().StringVarP(&password, "password", "p", "", "new password (required)")
+	cmd.MarkFlagRequired("password")
+
+	return cmd
+}
+
+func newDeleteCommand(db *sql.DB) *cobra.Command {
+	var machine string
+	var service string
+	var user string
+	var cmd = &cobra.Command{
+		Use:   "delete",
+		Short: "deletes an existing password",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			query, err := db.Prepare("delete from passwords where machine=? and service=? and user=?")
+			if err != nil {
+				return fmt.Errorf("db.Prepare(delete) failed: %s", err)
+			}
+
+			_, err = query.Exec(machine, service, user)
+
+			return nil
+		},
+	}
+	cmd.Flags().StringVarP(&machine, "machine", "m", "", "machine (required)")
+	cmd.MarkFlagRequired("machine")
+	cmd.Flags().StringVarP(&service, "service", "s", "", "service (required)")
+	cmd.MarkFlagRequired("service")
+	cmd.Flags().StringVarP(&user, "user", "u", "", "user (required)")
+	cmd.MarkFlagRequired("user")
+
+	return cmd
+}
+
 func newReadCommand(db *sql.DB) *cobra.Command {
 	var machineFlag string
 	var serviceFlag string
@@ -100,6 +159,8 @@ func newRootCommand(db *sql.DB) *cobra.Command {
 	}
 	cmd.AddCommand(newCreateCommand(db))
 	cmd.AddCommand(newReadCommand(db))
+	cmd.AddCommand(newUpdateCommand(db))
+	cmd.AddCommand(newDeleteCommand(db))
 
 	return cmd
 }
