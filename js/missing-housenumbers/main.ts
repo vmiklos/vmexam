b56ed04f7d981ce5672185da35f72d54749a5644
Miklos Vajna <vmiklos@vmiklos.hu>
2022-08-28 14:46:28 +0200

    js/missing-housenumbers: finish viranyos, add csilleberc

--- js/missing-housenumbers/main.ts
+++ js/missing-housenumbers/main.ts
@@ -40,7 +40,8 @@ const tracks = [
     {url : 'szechenyihegy.json', color : 'green'},
     {url : 'istenhegy.json', color : 'green'},
     {url : 'kutvolgy.json', color : 'green'},
-    {url : 'viranyos.json', color : 'orange'},
+    {url : 'viranyos.json', color : 'green'},
+    {url : 'csilleberc.json', color : 'orange'},
 ];
 
 // Boilerplate below.
