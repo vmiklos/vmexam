c1419f8677b77ab32e25081c1e976e06ce684d08
Miklos Vajna <vmiklos@vmiklos.hu>
2023-02-17 23:39:37 +0100

    ssh-proxy: variables can be used directly in the `format!` string

--- ssh-proxy/src/main.rs
+++ ssh-proxy/src/main.rs
@@ -10,7 +10,7 @@ fn main() -> anyhow::Result<()> {
     let home_dir: String = home_dir.to_str().context("to_str() failed")?.into();
     let config_path = home_dir + "/.config/ssh-proxyrc";
     let config_string = std::fs::read_to_string(&config_path)
-        .context(format!("failed to read config from '{}'", config_path))?;
+        .context(format!("failed to read config from '{config_path}'"))?;
     let config: Config = toml::from_str(&config_string)?;
     loop {
         let args = [
