38f13f6270f947a5ae5e56b53b294896d8a8966d
Miklos Vajna <vmiklos@vmiklos.hu>
2023-05-27 13:27:04 +0200

    lo: 3 more uitest that neds more work

--- osm/addr-osmify-cpp/coverage.sh
+++ osm/addr-osmify-cpp/coverage.sh
@@ -20,7 +20,6 @@ genhtml -o coverage osmify.info
 echo "Coverage report is now available at coverage/index.html."
 
 # Prints line coverage.
-# lcov-1.14+git.20180307.a5dd952-lp152.3.2.noarch doesn't have --fail-under-lines yet.
-~/git/lcov/bin/lcov --summary osmify.info --fail-under-lines 100
+lcov --summary osmify.info --fail-under-lines 100
 
 # vim:set shiftwidth=4 softtabstop=4 expandtab:
