33802f3fb774e24b596553292a7d3568ab8182cd
Miklos Vajna <vmiklos@vmiklos.hu>
2023-06-11 15:30:46 +0200

    nextcloud-open: add test when arg is a dir

--- nextcloud-open/src/tests.rs
+++ nextcloud-open/src/tests.rs
@@ -103,3 +103,48 @@ fn test_config_read_error() {
 
     assert_eq!(ret.is_err(), true);
 }
+
+/// Tests the case when the input is a directory, not a file.
+#[test]
+fn test_input_is_dir() {
+    let home_dir = home::home_dir().unwrap();
+    let home_path = home_dir.to_string_lossy();
+    let root: vfs::VfsPath = vfs::MemoryFS::new().into();
+    let home = root.join(&home_path).unwrap();
+    home.join(".config/Nextcloud")
+        .unwrap()
+        .create_dir_all()
+        .unwrap();
+    home.join("Nextcloud-Example/my dir")
+        .unwrap()
+        .create_dir_all()
+        .unwrap();
+    let config_file = home.join(".config/Nextcloud/nextcloud.cfg").unwrap();
+    config_file
+        .create_file()
+        .unwrap()
+        .write_all(
+            format!(
+                "[Accounts]\n\
+0\\Folders\\1\\localPath={home_path}/Nextcloud-Example/\n\
+0\\url=https://nextcloud.example.com\n\
+version=4\n\
+0\\keyWithNoValue"
+            )
+            .as_bytes(),
+        )
+        .unwrap();
+    let network: Rc<dyn Network> = Rc::new(TestNetwork::new());
+    let input = root
+        .join(format!("{home_path}/Nextcloud-Example/my dir"))
+        .unwrap();
+    let ctx = Context::new(root, network);
+
+    nextcloud_open(&ctx, &input).unwrap();
+
+    let network = ctx.network.as_any().downcast_ref::<TestNetwork>().unwrap();
+    let open_browsers = network.open_browsers.borrow_mut();
+    assert_eq!(open_browsers.len(), 1);
+    let expected = "https://nextcloud.example.com/apps/files/?dir=/my%20dir/";
+    assert_eq!(open_browsers[0].to_string(), expected);
+}
