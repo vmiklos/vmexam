a4d9cb9dfe913747922ef32a1eeb117b64994c48
Miklos Vajna <vmiklos@vmiklos.hu>
2023-06-19 19:50:57 +0200

    skype: bin this, obsolete

--- skype/receive.py
+++
@@ -1,8 +0,0 @@
-"""usage: send.py target 'hello world'"""
-
-import dbus, sys
-
-skypeout = dbus.SessionBus().get_object('com.Skype.API', '/com/Skype')
-skypeout.Invoke('NAME myapp')
-skypeout.Invoke('PROTOCOL 5')
-print skypeout.Invoke('GET CHATMESSAGE %s BODY' % sys.argv[1])
