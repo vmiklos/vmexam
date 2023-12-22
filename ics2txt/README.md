fbad3ba55b7cda35f46f828887a2108efb6c7e28
Miklos Vajna <vmiklos@collabora.com>
2023-12-22 15:01:33 +0100

    ics2txt: also show local time

--- ics2txt/README.md
+++ ics2txt/README.md
@@ -3,4 +3,6 @@
 This is meant to provide a plain text view of an ics file, used in calendar invite email
 attachments.
 
-For now it's similar to <https://github.com/terabyte/mutt-filters>.
+It's inspired by <https://github.com/terabyte/mutt-filters>, but ics2txt shows the start/end in
+local time zone, preserving also the original time zone, similar to what `mutt-display-filter` does
+for email.
--- ics2txt/src/main.rs
+++ ics2txt/src/main.rs
@@ -1,18 +1,61 @@
 use std::fs::File;
 use std::io::BufReader;
+use time_tz::PrimitiveDateTimeExt as _;
 
 /// See <https://datatracker.ietf.org/doc/html/rfc5545#section-3.3.11>:
 ///
 /// `ESCAPED-CHAR = ("\\" / "\;" / "\," / "\N" / "\n")`
 ///     \\ encodes \, \N or \n encodes newline
 ///     \; encodes ;, \, encodes ,
-fn decode_property_value(encoded: &str) -> String {
+fn decode_text(encoded: &str) -> String {
     encoded
         .replace(r#"\\"#, r#"\"#)
         .replace(r#"\;"#, r#";"#)
         .replace(r#"\,"#, r#","#)
         .replace(r#"\N"#, "\n")
         .replace(r#"\n"#, "\n")
+}
+
+/// See <https://datatracker.ietf.org/doc/html/rfc5545#section-3.3.5>, this is an ISO.8601 format,
+/// and the timezone is specified externally.
+///
+/// Returns an Rfc2822 date time, which contains timezone info.
+fn decode_date_time(property: &ical::property::Property) -> String {
+    let Some(ref value) = property.value else {
+        return "".into();
+    };
+    let ics_format = time::format_description::well_known::Iso8601::DEFAULT;
+    let date_time = time::PrimitiveDateTime::parse(value, &ics_format).unwrap();
+    let mut tzid = "".to_string();
+    if let Some(ref params) = property.params {
+        for (key, value) in params {
+            if key == "TZID" {
+                let time_zone = value.first();
+                if let Some(value) = time_zone {
+                    tzid = value.to_string();
+                }
+            }
+        }
+    }
+    if tzid.is_empty() {
+        return "".to_string();
+    }
+    let tz = time_tz::timezones::get_by_name(&tzid).unwrap();
+    let date_time = date_time.assume_timezone(tz).unwrap();
+    let format = time::format_description::well_known::Rfc2822;
+    date_time.format(&format).unwrap()
+}
+
+/// Try to improve input_date by wrapping a non-local date in a local one.
+fn improve_date(input_date: &str) -> anyhow::Result<String> {
+    let format = time::format_description::well_known::Rfc2822;
+    let mut date_time = time::OffsetDateTime::parse(input_date, &format)?;
+    let local_offset = time::UtcOffset::current_local_offset()?;
+    if date_time.offset() == local_offset {
+        return Err(anyhow::anyhow!("matching offset"));
+    }
+    date_time = date_time.to_offset(local_offset);
+    Ok(date_time.format(&format)?)
 }
 
 fn main() -> anyhow::Result<()> {
@@ -27,48 +70,28 @@ fn main() -> anyhow::Result<()> {
             for property in event.properties {
                 if property.name == "SUMMARY" {
                     if let Some(value) = property.value {
-                        println!("Summary: {}", decode_property_value(&value));
+                        println!("Summary: {}", decode_text(&value));
                     }
                 } else if property.name == "DESCRIPTION" {
                     if let Some(value) = property.value {
-                        println!("Description: {}", decode_property_value(&value));
+                        println!("Description: {}", decode_text(&value));
                     }
                 } else if property.name == "LOCATION" {
                     if let Some(value) = property.value {
-                        println!("Location: {}", decode_property_value(&value));
+                        println!("Location: {}", decode_text(&value));
                     }
                 } else if property.name == "ORGANIZER" {
                     if let Some(value) = property.value {
-                        println!("Organizer: {}", decode_property_value(&value));
+                        println!("Organizer: {}", decode_text(&value));
                     }
                 } else if property.name == "DTSTART" {
-                    if let Some(value) = property.value {
-                        println!("Dtstart: {}", decode_property_value(&value));
-                    }
-                    if let Some(params) = property.params {
-                        for (key, value) in params {
-                            if key == "TZID" {
-                                let time_zone = value.first();
-                                if let Some(value) = time_zone {
-                                    println!("Dtstart Tzid: {}", value);
-                                }
-                            }
-                        }
-                    }
+                    let input_date = decode_date_time(&property);
+                    let improved = improve_date(&input_date).unwrap();
+                    println!("Dtstart: {improved} ({input_date})");
                 } else if property.name == "DTEND" {
-                    if let Some(value) = property.value {
-                        println!("Dtend: {}", decode_property_value(&value));
-                    }
-                    if let Some(params) = property.params {
-                        for (key, value) in params {
-                            if key == "TZID" {
-                                let time_zone = value.first();
-                                if let Some(value) = time_zone {
-                                    println!("Dtend Tzid: {}", value);
-                                }
-                            }
-                        }
-                    }
+                    let input_date = decode_date_time(&property);
+                    let improved = improve_date(&input_date).unwrap();
+                    println!("Dtend: {improved} ({input_date})");
                 }
             }
         }
