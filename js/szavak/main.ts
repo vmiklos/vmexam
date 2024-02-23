a29b16d69c19a5bf904e3f696f32859149542671
Miklos Vajna <vmiklos@collabora.com>
2024-02-23 22:45:41 +0100

    js, szavak: add 'p'

--- js/szavak/main.ts
+++ js/szavak/main.ts
@@ -61,8 +61,9 @@ async function refreshClick()
 
     let valid = true;
     const valid_letters = [
-        'a', 'á', 'b', 'c', 'e', 'é', 'h', 'i', 'í', 'j', 'k', 'l', 'm', 'n',
-        'o', 'ó', 'ö', 'ő', 'r', 's', 't', 'u', 'ú', 'ü', 'ű', 'v', 'z', '='
+        'a', 'á', 'b', 'c', 'e', 'é', 'h', 'i', 'í', 'j',
+        'k', 'l', 'm', 'n', 'o', 'ó', 'ö', 'ő', 'p', 'r',
+        's', 't', 'u', 'ú', 'ü', 'ű', 'v', 'z', '='
     ];
     const y_prefixes: string[] = []; // could be e.g. 'g'.
     for (let i = 0; i < window.wordList.length; i++)
