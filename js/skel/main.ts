d7e03b422777a0c0782b35bf2d4995b10eecf2e3
Miklos Vajna <vmiklos@collabora.com>
2024-02-25 15:25:09 +0100

    js: add a skel project
    together with an init script to create a new project based on this

---
+++ js/skel/main.ts
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2024 Miklos Vajna
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+document.addEventListener("DOMContentLoaded", function() {
+    const body = document.querySelector('body');
+    body.appendChild(document.createTextNode('skel'));
+});
+
+// vim: shiftwidth=4 softtabstop=4 expandtab:
