09a5f279dc77d3ed0b8c9615838c877aff6254c0
Miklos Vajna <vmiklos@collabora.com>
2024-05-20 15:38:58 +0200

    js, rustwasm: remove, was just copy&paste
    from https://rustwasm.github.io/docs/book/

--- js/rustwasm/wasm-string-sum/src/lib.rs
+++
@@ -1,6 +0,0 @@
-use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-pub fn sum_as_string(a: usize, b: usize) -> String {
-    (a + b).to_string()
-}
--- js/rustwasm/wasm-string-sum/www/index.js
+++
@@ -1,3 +0,0 @@
-import * as wasm from "wasm-string-sum";
-
-console.log(JSON.stringify(wasm.sum_as_string(1, 2)));
