edc7edc7a149a1fafa5047ee63422e1659364226
Miklos Vajna <vmiklos@collabora.com>
2024-05-24 23:39:44 +0200

    avg: improve error handling
    Get rid of 2 unwrap() calls.

--- avg/README.md
+++ avg/README.md
@@ -1,4 +1,4 @@
 # avg
 
-Simple prints the average of all arguments, in case creating a new Calc sheet just for this would be
+Simply prints the average of all arguments, in case creating a new Calc sheet just for this would be
 an overkill.
--- avg/src/main.rs
+++ avg/src/main.rs
@@ -1,7 +1,23 @@
-fn main() {
+/*
+ * Copyright 2022 Miklos Vajna
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#![deny(warnings)]
+#![warn(clippy::all)]
+#![warn(missing_docs)]
+
+//! Simply prints the average of all arguments.
+
+use anyhow::Context as _;
+
+fn main() -> anyhow::Result<()> {
     let count = (std::env::args().count() - 1) as i64;
     let mut iter = std::env::args();
-    iter.next().unwrap();
-    let sum: i64 = iter.map(|arg| arg.parse::<i64>().unwrap()).sum();
-    println!("{}", sum / count);
+    iter.next().context("next failed")?;
+    let numbers: Result<Vec<_>, _> = iter.map(|arg| arg.parse::<i64>()).collect();
+    let sum: i64 = numbers?.iter().sum();
+    println!("{}", sum.checked_div(count).context("checked_div failed")?);
+    Ok(())
 }
