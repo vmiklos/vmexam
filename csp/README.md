ff5592cde22948b048c89981c77d424239270f99
Miklos Vajna <vmiklos@collabora.com>
2024-05-27 19:32:54 +0200

    csp: parameterize this a bit

--- csp/README.md
+++ csp/README.md
@@ -1,3 +1,14 @@
 # csp
 
-Simple [CSP](https://en.wikipedia.org/wiki/Constraint_satisfaction_problem) solver sample.
+Given a 2x2 table with numbers, select cells, so that the sum of the selected cells will equal to
+the provided sum.
+
+(Real-life math exercise for 2nd grade students in primary school.)
+
+Example:
+
+```console
+$ csp --sum 16 5 4 8 7
+5 4
+0 7
+```
--- csp/src/main.rs
+++ csp/src/main.rs
@@ -8,18 +8,29 @@
 #![warn(clippy::all)]
 #![warn(missing_docs)]
 
-//! Given a 2x2 table with numbers, select cells, so that the sum of the selected cells will equal to
-//! the provided sum.
+//! Simple [CSP](https://en.wikipedia.org/wiki/Constraint_satisfaction_problem) solver sample.
 
-//! (Real-life math exercise for 2nd grade students in primary school.)
+use clap::Parser as _;
+
+#[derive(clap::Parser)]
+struct Arguments {
+    #[arg(short, long)]
+    sum: i32,
+    a1: i32,
+    a2: i32,
+    b1: i32,
+    b2: i32,
+}
 
 fn main() {
+    let args = Arguments::parse();
+
     let mut problem = puzzle_solver::Puzzle::new();
-    let a1 = problem.new_var_with_candidates(&[0, 5]);
-    let a2 = problem.new_var_with_candidates(&[0, 4]);
-    let b1 = problem.new_var_with_candidates(&[0, 8]);
-    let b2 = problem.new_var_with_candidates(&[0, 7]);
-    problem.equals(a1 + a2 + b1 + b2, 16);
+    let a1 = problem.new_var_with_candidates(&[0, args.a1]);
+    let a2 = problem.new_var_with_candidates(&[0, args.a2]);
+    let b1 = problem.new_var_with_candidates(&[0, args.b1]);
+    let b2 = problem.new_var_with_candidates(&[0, args.b2]);
+    problem.equals(a1 + a2 + b1 + b2, args.sum);
     let solutions = problem.solve_all();
     for s in solutions {
         println!("{} {}\n{} {}", s[a1], s[a2], s[b1], s[b2]);
