ded6c9fc7446ddc06e6b486adca1e0c95d5ac8aa
Miklos Vajna <vmiklos@collabora.com>
2024-06-27 19:20:46 +0200

    fit2json: merge in activity name from meta json

--- fit2json/src/main.rs
+++ fit2json/src/main.rs
@@ -22,6 +22,11 @@ struct Arguments {
     json: String,
 }
 
+#[derive(serde::Deserialize)]
+struct Activity {
+    name: String,
+}
+
 fn checked_run(first: &str, rest: &[&str]) -> anyhow::Result<()> {
     let exit_status = std::process::Command::new(first).args(rest).status()?;
     match exit_status.code().context("code() failed")? {
@@ -32,9 +37,7 @@ fn checked_run(first: &str, rest: &[&str]) -> anyhow::Result<()> {
     }
 }
 
-fn main() -> anyhow::Result<()> {
-    let args = Arguments::parse();
-
+fn create_json(args: &Arguments) -> anyhow::Result<()> {
     let gpsbabel_args = [
         "-i",
         "garmin_fit",
@@ -45,8 +48,54 @@ fn main() -> anyhow::Result<()> {
         "-F",
         &args.json,
     ];
-    println!("gpsbabel {}", gpsbabel_args.join(" "));
-    checked_run("gpsbabel", &gpsbabel_args)?;
+    println!("Running: gpsbabel {}", gpsbabel_args.join(" "));
+    checked_run("gpsbabel", &gpsbabel_args)
+}
 
+fn extract_activity_name(args: &Arguments) -> anyhow::Result<String> {
+    let mut meta_json = std::path::PathBuf::from(&args.fit);
+    meta_json.set_extension("meta.json");
+    let file = std::fs::File::open(meta_json)?;
+    let activity: Activity = serde_json::from_reader(&file)?;
+    Ok(activity.name)
+}
+
+fn read_json(args: &Arguments) -> anyhow::Result<serde_json::Value> {
+    let file = std::fs::File::open(&args.json)?;
+    Ok(serde_json::from_reader(file)?)
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Arguments::parse();
+
+    // Convert to JSON.
+    create_json(&args)?;
+
+    // Try to exract activity name.
+    let activity_name = match extract_activity_name(&args) {
+        Ok(value) => value,
+        Err(_) => "".to_string(),
+    };
+
+    if activity_name.is_empty() {
+        return Ok(());
+    }
+
+    // Mutate the JSON to show the activity name.
+    println!("Injecting description into the JSON");
+    let mut json = read_json(&args)?;
+    let features = json.as_object_mut().unwrap().get_mut("features").unwrap();
+    let feature = &mut features.as_array_mut().unwrap()[0];
+    let properties = feature
+        .as_object_mut()
+        .unwrap()
+        .get_mut("properties")
+        .unwrap();
+    let description = serde_json::Value::from(format!("<b>Name</b> {activity_name}"));
+    properties
+        .as_object_mut()
+        .unwrap()
+        .insert("description".into(), description);
+    serde_json::to_writer(std::fs::File::create(&args.json)?, &json)?;
     Ok(())
 }
