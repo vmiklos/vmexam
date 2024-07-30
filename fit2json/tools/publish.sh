5fd05e7fe37ca08d0e17f4ba390ae40ea80f18e0
Miklos Vajna <vmiklos@collabora.com>
2024-07-30 08:01:07 +0200

    fit2json: initial publish script
    It's far from perfect, but does work locally with 2 servers (backup one,
    publish one).

---
+++ fit2json/tools/publish.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+#
+# Copyright 2024 Miklos Vajna
+#
+# SPDX-License-Identifier: MIT
+#
+
+#
+# This script publishes one activity from Strava to an unlisted link, visible to unregistered users
+# as well. 'plug' is where the Strava backup is invoked, 'wilson' is where the result is published.
+#
+
+BACKUP=$(ssh plug 'find .local/share/strava-backup/activities |grep fit$ |sort |tail -n 1')
+scp plug:$BACKUP plug:$(echo $BACKUP |sed 's|.fit|.meta.json|') .
+JSON=$(fit2json -- $(basename $BACKUP))
+scp -- $JSON wilson:share/pages/map/
+echo "<https://share.vmiklos.hu/pages/map/?a=$(basename -- $JSON .json)>"
+
+# vim:set shiftwidth=4 softtabstop=4 expandtab:
