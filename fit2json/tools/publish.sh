f2c7b60357fad89b36a52a104a63f68c5620d683
Miklos Vajna <vmiklos@collabora.com>
2024-10-05 14:15:00 +0200

    fit2json publish: allow Strava backup on localhost

--- fit2json/tools/publish.sh
+++ fit2json/tools/publish.sh
@@ -10,8 +10,13 @@
 # as well. 'plug' is where the Strava backup is invoked, 'wilson' is where the result is published.
 #
 
-BACKUP=$(ssh plug 'find .local/share/strava-backup/activities |grep fit$ |sort |tail -n 1')
-scp plug:$BACKUP plug:$(echo $BACKUP |sed 's|.fit|.meta.json|') .
+if [ "$HOSTNAME" == "plug" ]; then
+    BACKUP=$(find .local/share/strava-backup/activities |grep fit$ |sort |tail -n 1)
+    cp $BACKUP $(echo $BACKUP |sed 's|.fit|.meta.json|') .
+else
+    BACKUP=$(ssh plug 'find .local/share/strava-backup/activities |grep fit$ |sort |tail -n 1')
+    scp plug:$BACKUP plug:$(echo $BACKUP |sed 's|.fit|.meta.json|') .
+fi
 JSON=$(fit2json -- $(basename $BACKUP))
 scp -- $JSON wilson:share/pages/map/
 echo "<https://share.vmiklos.hu/pages/map/?a=$(basename -- $JSON .json)>"
