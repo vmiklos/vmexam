7be907055c8abb8f021bdba79f48b47d6611f385
Miklos Vajna <vmiklos@collabora.com>
2025-03-22 15:01:07 +0100

    csp: add a sudoku solver
    Implements a backtracking algorithm from a blog post.

---
+++ csp/src/bin/sudoku.rs
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2025 Miklos Vajna
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#![deny(warnings)]
+#![warn(clippy::all)]
+#![warn(missing_docs)]
+
+//! Sudoku solver, implementing the algorithm from
+//! <https://medium.com/@ev.zafeiratos/sudoku-solver-with-python-a-methodical-approach-for-algorithm-optimization-part-1-b2c99887167f>.
+
+use anyhow::Context as _;
+
+const MODEL_WIDTH: usize = 9;
+const MODEL_HEIGHT: usize = 9;
+type ModelRow = [usize; MODEL_WIDTH];
+type Model = [ModelRow; MODEL_HEIGHT];
+
+fn create_model(problem: &str) -> anyhow::Result<Model> {
+    let mut model: Model = [[0; MODEL_WIDTH]; MODEL_HEIGHT];
+    let fro_rows = problem.split('\n');
+    for (row_index, fro_row) in fro_rows.enumerate() {
+        let mut row: ModelRow = [0; MODEL_WIDTH];
+        for (cell_index, cell) in fro_row.chars().enumerate() {
+            let value: usize = cell.to_digit(10).context("to_digit() failed")?.try_into()?;
+            row[cell_index] = value;
+        }
+        model[row_index] = row;
+    }
+    Ok(model)
+}
+
+fn find_empty(model: &Model) -> Option<(usize, usize)> {
+    for (row_index, row) in model.iter().enumerate() {
+        for (cell_index, cell) in row.iter().enumerate() {
+            if *cell == 0 {
+                return Some((row_index, cell_index));
+            }
+        }
+    }
+    None
+}
+
+fn is_valid(model: &Model, num: usize, pos: (usize, usize)) -> bool {
+    let (row, cell) = pos;
+    for cell in model[row] {
+        if cell == num {
+            return false;
+        }
+    }
+    for row in model {
+        if row[cell] == num {
+            return false;
+        }
+    }
+    let row_block_start = (row / 3) * 3;
+    let cell_block_start = (cell / 3) * 3;
+    for row in model.iter().skip(row_block_start).take(3) {
+        for cell in row.iter().skip(cell_block_start).take(3) {
+            if *cell == num {
+                return false;
+            }
+        }
+    }
+    true
+}
+
+fn solve(model: &mut Model) -> bool {
+    let empty = if let Some(value) = find_empty(model) {
+        value
+    } else {
+        return true;
+    };
+    let (row, cell) = empty;
+    for num in 1..10 {
+        if is_valid(model, num, empty) {
+            model[row][cell] = num;
+            if solve(model) {
+                return true;
+            }
+            model[row][cell] = 0;
+        }
+    }
+    false
+}
+
+fn main() -> anyhow::Result<()> {
+    let problem = r#"200080300
+060070084
+030500209
+000105408
+000000000
+402706000
+301007040
+720040060
+004010003"#;
+    let mut model = create_model(problem)?;
+    assert!(solve(&mut model));
+    for row in model {
+        for cell in row {
+            print!("{cell}");
+        }
+        println!();
+    }
+
+    Ok(())
+}
