c07a4ddfd80a6ece4c354a5e43b4cf967e1f7916
Miklos Vajna <vmiklos@collabora.com>
2025-05-22 19:07:01 +0200

    darcs-git: simplify, use console::Term

--- darcs-git/src/main.rs
+++ darcs-git/src/main.rs
@@ -12,7 +12,6 @@
 
 use anyhow::Context as _;
 use std::io::BufRead as _;
-use std::io::Read as _;
 
 /// Context implementation, backed by library calls.
 struct StdContext {}
@@ -43,23 +42,8 @@ impl darcs_git::Context for StdContext {
     }
 
     fn readch(&self) -> anyhow::Result<String> {
-        let mut stdin = std::io::stdin();
-        let fd = libc::STDIN_FILENO;
-        let mut settings = termios::Termios::from_fd(fd)?;
-
-        // Set raw mode.
-        let old_settings = settings;
-        settings.c_lflag &= !(termios::ICANON | libc::ECHO);
-        termios::tcsetattr(fd, termios::TCSANOW, &settings)?;
-
-        // Read a character.
-        let mut buffer = [0; 1];
-        stdin.read_exact(&mut buffer)?;
-
-        // Restore old mode.
-        termios::tcsetattr(fd, termios::TCSANOW, &old_settings)?;
-
-        Ok(String::from_utf8(buffer.to_vec())?)
+        let term = console::Term::stdout();
+        Ok(term.read_char()?.to_string())
     }
 }
 
