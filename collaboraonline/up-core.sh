21b8c1c2ad70de44a32d08011cb999d6efe517fa
Miklos Vajna <vmiklos@collabora.com>
2026-04-06 08:35:46 +0200

    cool core: add file list & build script

---
+++ collaboraonline/up-core.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+#
+# Builds core.git from scratch.
+#
+
+BRANCH=$(git symbolic-ref HEAD|sed 's|refs/heads/||')
+time (
+    git pull -r
+    if [ $BRANCH == main -a -e Makefile ]; then
+        make distclean
+    fi
+    ./autogen.sh
+    make check gb_SUPPRESS_TESTS=y || make check gb_SUPPRESS_TESTS=y
+    make tags
+    # distro/foo/bar -> bar
+    (cd instdir && rm -rf user && ln -s $HOME/.config/collaboraofficedev/${BRANCH##*/}/user)
+    make check
+    make vim-ide-integration
+    if [ $BRANCH == main ]; then
+        style-check-files
+    fi
+) 2>&1 |tee log
+
+exit ${PIPESTATUS[0]}
+
+# vim:set shiftwidth=4 expandtab:
