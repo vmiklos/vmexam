159b3987a0dc319893c1777575755b25a73e7904
Miklos Vajna <vmiklos@collabora.com>
2026-06-29 13:47:25 +0200

    weechat-calc: update dependencies (2026)
    And check in 'Cargo.lock', since I needed a 'cargo update -p vergen
    --precise 9.0.6' to get this to build.

--- weechat-calc/src/main.rs
+++ weechat-calc/src/main.rs
@@ -16,7 +16,7 @@ fn main() -> anyhow::Result<()> {
     let args: Vec<String> = std::env::args().collect();
     let (_, tokens) = args.split_first().context("args.split_first() failed")?;
     let expr = tokens.join(" ");
-    let mut ctx = calc::Context::<f64>::default();
+    let mut ctx = calc::Context::default();
     let res = ctx.evaluate(&expr).context("evaluate() failed")?;
     println!("{expr}={res}");
     Ok(())
