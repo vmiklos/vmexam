6058ba6f71f7580893ec5ba65d9bbe25873b2534
Miklos Vajna <vmiklos@collabora.com>
2026-08-24 21:07:33 +0200

    strava-mirror: improve test_query_top_rides_by_time()

--- strava-mirror/src/stats/tests.rs
+++ strava-mirror/src/stats/tests.rs
@@ -400,29 +400,29 @@ fn test_query_top_walks_by_elevation() {
 
 #[test]
 fn test_query_top_rides_by_time() {
-    // Given three activities (1 walk, 2 rides):
+    // Given two activities (2 rides):
     let fs = vfs::VfsPath::new(vfs::MemoryFS::new());
     let activities_dir = fs
         .join(".local/share/strava-mirror/activities/2025")
         .unwrap();
     activities_dir.create_dir_all().unwrap();
 
-    // 1. Long ride
+    // 1. Short ride
     let meta_path_1 = activities_dir
         .join("2025-01-01T10-00-00Z_1.meta.json")
         .unwrap();
-    let content_1 = r#"{"id": 1, "name": "long ride", "start_time": "2025-01-01T10:00:00Z", "sport_type": "Ride", "moving_time_raw": 10000, "elapsed_time_raw": 10400, "distance_raw": 50000.0, "elevation_gain_raw": 500.0}"#;
+    let content_1 = r#"{"id": 1, "name": "short ride", "start_time": "2025-01-01T10:00:00Z", "sport_type": "Ride", "moving_time_raw": 5000, "elapsed_time_raw": 5400, "distance_raw": 25000.0, "elevation_gain_raw": 200.0}"#;
     meta_path_1
         .create_file()
         .unwrap()
         .write_all(content_1.as_bytes())
         .unwrap();
 
-    // 2. Short ride
+    // 2. Long ride
     let meta_path_2 = activities_dir
         .join("2025-01-02T10-00-00Z_2.meta.json")
         .unwrap();
-    let content_2 = r#"{"id": 2, "name": "short ride", "start_time": "2025-01-02T10:00:00Z", "sport_type": "Ride", "moving_time_raw": 5000, "elapsed_time_raw": 5400, "distance_raw": 25000.0, "elevation_gain_raw": 200.0}"#;
+    let content_2 = r#"{"id": 2, "name": "long ride", "start_time": "2025-01-02T10:00:00Z", "sport_type": "Ride", "moving_time_raw": 10000, "elapsed_time_raw": 10400, "distance_raw": 50000.0, "elevation_gain_raw": 500.0}"#;
     meta_path_2
         .create_file()
         .unwrap()
@@ -449,7 +449,23 @@ fn test_query_top_rides_by_time() {
     ];
     run(args, &mut buf, &ctx).unwrap();
 
-    // Then no failure occurs.
+    // Then the result has a table with 2 non-header rows: long ride first, short ride second.
+    let document = parse_html(buf);
+    let row_selector = scraper::Selector::parse("tbody tr").unwrap();
+    let link_selector = scraper::Selector::parse("td:nth-child(3) a").unwrap();
+    let rows = document.select(&row_selector);
+    let names: Vec<String> = rows
+        .map(|row| {
+            row.select(&link_selector)
+                .next()
+                .unwrap()
+                .text()
+                .next()
+                .unwrap()
+                .to_string()
+        })
+        .collect();
+    assert_eq!(names, ["long ride", "short ride"]);
 }
 
 #[test]
